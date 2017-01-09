1e89b62c12dccbdec97b49bf1a47b06dc7103b02
Luke <n.lukas265@gmail.com>
2017-01-10 00:50:39 +0200

    Fixed test fail on other timezones.

--- test/github-issues/179/issue-179.ts
+++ test/github-issues/179/issue-179.ts
@@ -7,6 +7,13 @@ import {expect} from "chai";
 
 
 describe("other issues > date", () => {
+    const localTimeString = "01:00:00";
+    const localDateString = "2017-01-01";
+    const localDateTimeString = localDateString + " " + localTimeString;
+    const baseDate = new Date(localDateString + " " + localTimeString);
+    const utcDateString = baseDate.toISOString().substring(0, 10);
+    const utcTimeString = baseDate.toISOString().substr(11, 8);
+    const utcDateTimeString = utcDateString + " " + utcTimeString;
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
@@ -18,22 +25,25 @@ describe("other issues > date", () => {
     after(() => closeTestingConnections(connections));
 
     it("should convert and format between UTC and local", () => {
-        const baseDate = new Date("2017-01-01 01:00:00+200"); // UTC + 2 hours
-
         // UTC
-        DataTransformationUtils.mixedDateToDateString(baseDate, false).should.be.equal("2016-12-31");
-        DataTransformationUtils.mixedDateToTimeString(baseDate, false).should.be.equal("23:00:00");
-        DataTransformationUtils.mixedDateToDatetimeString(baseDate, false).should.be.equal("2016-12-31 23:00:00");
+        DataTransformationUtils.mixedDateToDateString(baseDate, false).should.be.equal(utcDateString);
+        DataTransformationUtils.mixedDateToTimeString(baseDate, false).should.be.equal(utcTimeString);
+        DataTransformationUtils.mixedDateToDatetimeString(baseDate, false).should.be.equal(utcDateTimeString);
 
         // local
-        DataTransformationUtils.mixedDateToDateString(baseDate, true).should.be.equal("2017-01-01");
-        DataTransformationUtils.mixedDateToTimeString(baseDate, true).should.be.equal("01:00:00");
-        DataTransformationUtils.mixedDateToDatetimeString(baseDate, true).should.be.equal("2017-01-01 01:00:00");
+        if (new Date().getTimezoneOffset() === 0) { // if testing machine is +0 zone, then local time should be the same as UTC
+            DataTransformationUtils.mixedDateToDateString(baseDate, true).should.be.equal(utcDateString);
+            DataTransformationUtils.mixedDateToTimeString(baseDate, true).should.be.equal(utcTimeString);
+            DataTransformationUtils.mixedDateToDatetimeString(baseDate, true).should.be.equal(utcDateTimeString);
+        } else {
+            DataTransformationUtils.mixedDateToDateString(baseDate, true).should.be.equal(localDateString);
+            DataTransformationUtils.mixedDateToTimeString(baseDate, true).should.be.equal(localTimeString);
+            DataTransformationUtils.mixedDateToDatetimeString(baseDate, true).should.be.equal(localDateTimeString);
+        }
 
     });
 
     it("should persist and return correct persisted dates", () => Promise.all(connections.map(async function(connection) {
-        const baseDate = new Date("2017-01-01 01:00:00+200"); // UTC + 2 hours
 
         let post = new Post();
         // Local time
@@ -49,26 +59,26 @@ describe("other issues > date", () => {
 
         post = new Post();
         // Local
-        post.localTimeOnly = "01:00:00";
-        post.localDateOnly = "2017-01-01";
-        post.localDateTime = "2017-01-01 01:00:00";
+        post.localTimeOnly = localTimeString;
+        post.localDateOnly = localDateString;
+        post.localDateTime = localDateTimeString;
         // UTC
-        post.dateOnly = "2016-12-31";
-        post.timeOnly = "23:00:00";
-        post.dateTime = "2016-12-31 23:00:00";
+        post.dateOnly = utcDateString;
+        post.timeOnly = utcTimeString;
+        post.dateTime = utcDateTimeString;
 
         await connection.entityManager.persist(post);
 
         // test if accepts full date string
         post = new Post();
         // Local
-        post.localTimeOnly = "2017-01-01 01:00:00";
-        post.localDateOnly = "2017-01-01 01:00:00";
-        post.localDateTime = "2017-01-01 01:00:00";
+        post.localTimeOnly = localDateTimeString;
+        post.localDateOnly = localDateTimeString;
+        post.localDateTime = localDateTimeString;
         // UTC
-        post.dateOnly = "2016-12-31 01:00:00";
-        post.timeOnly = "2016-12-31 23:00:00";
-        post.dateTime = "2016-12-31 23:00:00";
+        post.dateOnly = utcDateTimeString;
+        post.timeOnly = utcDateTimeString;
+        post.dateTime = utcDateTimeString;
 
         await connection.entityManager.persist(post);
 
@@ -76,13 +86,20 @@ describe("other issues > date", () => {
 
         expect(loadedPosts).not.to.be.empty;
         for (let post of loadedPosts) {
-            DataTransformationUtils.mixedDateToDatetimeString(post.localDateTime, true).should.be.equal("2017-01-01 01:00:00");
-            DataTransformationUtils.mixedDateToDatetimeString(post.dateTime).should.be.equal("2016-12-31 23:00:00");
+            DataTransformationUtils.mixedDateToDatetimeString(post.dateTime).should.be.equal(utcDateTimeString);
 
-            post.timeOnly.should.be.equal("23:00:00");
-            post.dateOnly.should.be.equal("2016-12-31");
-            post.localDateOnly.should.be.equal("2017-01-01");
-            post.localTimeOnly.should.be.equal("01:00:00");
+            post.timeOnly.should.be.equal(utcTimeString);
+            post.dateOnly.should.be.equal(utcDateString);
+
+            if (new Date().getTimezoneOffset() === 0) { // if testing machine is +0 zone, then local time should be the same as UTC
+                DataTransformationUtils.mixedDateToDatetimeString(post.localDateTime, true).should.be.equal(utcDateTimeString);
+                post.localDateOnly.should.be.equal(utcDateString);
+                post.localTimeOnly.should.be.equal(utcTimeString);
+            } else {
+                DataTransformationUtils.mixedDateToDatetimeString(post.localDateTime, true).should.be.equal(localDateTimeString);
+                post.localDateOnly.should.be.equal(localDateString);
+                post.localTimeOnly.should.be.equal(localTimeString);
+            }
         }
 
     })));
