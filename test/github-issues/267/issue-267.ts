c0a375404711313e22faf02ecc775227b133382d
John Mai <maicorp@gmail.com>
2017-03-12 03:04:25 -0700

    add check condition for undefined

--- test/github-issues/267/issue-267.ts
+++ test/github-issues/267/issue-267.ts
@@ -33,19 +33,21 @@ describe("github issues > #267 Adding Fractional Seconds", () => {
         await connection.entityManager.persist(person);
 
         let retrievedPerson = await connection.getRepository(Person).findOneById(person.id);
+        
+        if (retrievedPerson) {
+            retrievedPerson.id.should.be.equal(1);
+            retrievedPerson.firstName.should.be.equal("Hello");
+            retrievedPerson.lastName.should.be.equal("World");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.birthday).should.be.equal("2017-01-01 12:01:05.000");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.aTime).should.be.equal("2017-01-01 12:01:05.100");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.bTime).should.be.equal("2017-01-01 12:01:05.120");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.cTime).should.be.equal("2017-01-01 12:01:05.123");
 
-        retrievedPerson.id.should.be.equal(1);
-        retrievedPerson.firstName.should.be.equal("Hello");
-        retrievedPerson.lastName.should.be.equal("World");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.birthday).should.be.equal("2017-01-01 12:01:05");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.aTime).should.be.equal("2017-01-01 12:01:05.100");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.bTime).should.be.equal("2017-01-01 12:01:05.120");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.cTime).should.be.equal("2017-01-01 12:01:05.123");
-
-        // JS Date clas only does milliseconds. Future might have a plan for microseconds
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.dTime).should.be.equal("2017-01-01 12:01:05.123");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.eTime).should.be.equal("2017-01-01 12:01:05.123");
-        DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.fTime).should.be.equal("2017-01-01 12:01:05.123");
+            // JS Date clas only does milliseconds. Future might have a plan for microseconds
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.dTime).should.be.equal("2017-01-01 12:01:05.123");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.eTime).should.be.equal("2017-01-01 12:01:05.123");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.fTime).should.be.equal("2017-01-01 12:01:05.123");
+        }
     })));
 
 });
