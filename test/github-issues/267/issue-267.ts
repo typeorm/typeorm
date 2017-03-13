6142c324c2216f9c08b280de5c628df151bbecf4
John Mai <maicorp@gmail.com>
2017-03-13 02:29:06 -0700

    Remove extra zero

--- test/github-issues/267/issue-267.ts
+++ test/github-issues/267/issue-267.ts
@@ -38,12 +38,12 @@ describe("github issues > #267 Adding Fractional Seconds", () => {
             retrievedPerson.id.should.be.equal(1);
             retrievedPerson.firstName.should.be.equal("Hello");
             retrievedPerson.lastName.should.be.equal("World");
-            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.birthday).should.be.equal("2017-01-01 12:01:05.000");
+            DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.birthday).should.be.equal("2017-01-01 12:01:05.00");
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.aTime).should.be.equal("2017-01-01 12:01:05.100");
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.bTime).should.be.equal("2017-01-01 12:01:05.120");
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.cTime).should.be.equal("2017-01-01 12:01:05.123");
 
-            // JS Date clas only does milliseconds. Future might have a plan for microseconds
+            // JS Date class only does up to milliseconds. Future might have a plan for microseconds
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.dTime).should.be.equal("2017-01-01 12:01:05.123");
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.eTime).should.be.equal("2017-01-01 12:01:05.123");
             DataTransformationUtils.mixedDateToDatetimeString(retrievedPerson.fTime).should.be.equal("2017-01-01 12:01:05.123");
