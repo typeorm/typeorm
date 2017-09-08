58244c5223d64850350d3deb006e5f5485246b19
Chriss Kalogeropoulos <iz.iznogood@gmail.com>
2017-09-08 23:22:39 +0300

    Add sparse option to all Index Metadata objects
    Add sparse option to all index metadata objects and push it to the MongoDB driver

--- src/entity-schema/EntitySchema.ts
+++ src/entity-schema/EntitySchema.ts
@@ -259,9 +259,16 @@ export interface EntitySchema { // todo: make it-to-date
             columns: string[];
 
             /**
-             * Index uniqueness.
+             * Indicates if this index must be unique or not.
              */
             unique: boolean;
+
+            /**
+             * If true, the index only references documents with the specified field.
+             * These indexes use less space but behave differently in some situations (particularly sorts).
+             * This option is only supported for mongodb database.
+             */
+            sparse?: boolean;
         };    
     };
 
