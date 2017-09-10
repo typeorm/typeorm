7f75758ecf959f453bc5e01fda8df6b329aeae74
Chriss Kalogeropoulos <iz.iznogood@gmail.com>
2017-09-10 12:42:17 +0300

    Change test code structure according to review comments
    Change the strructure to use 'it' blocks, makes code clearer

--- test/functional/database-schema/indices/indices-create-modify.ts
+++ test/functional/database-schema/indices/indices-create-modify.ts
@@ -20,64 +20,47 @@ describe("indices > reading index from entity schema and updating database", () 
 
     describe("create index", function() {
 
-        it("should work without errors", () => Promise.all(connections.map(async connection => {
+        it("should create a non unique index with 2 columns", () => Promise.all(connections.map(async connection => {
 
-            const checkCreateFn = async function() {
+            const queryRunner = connection.createQueryRunner();
+            const tableSchema = await queryRunner.loadTableSchema("person");
+            await queryRunner.release();
 
-                const queryRunner = connection.createQueryRunner();
-                const tableSchema = await queryRunner.loadTableSchema("person");
-                await queryRunner.release();
-    
-                expect(tableSchema!.indices.length).to.be.equal(1);
-                expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
-                expect(tableSchema!.indices[0].isUnique).to.be.false; 
-                expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
-                expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("firstname"); 
-                expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("lastname");
-    
-            };
+            expect(tableSchema!.indices.length).to.be.equal(1);
+            expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
+            expect(tableSchema!.indices[0].isUnique).to.be.false; 
+            expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
+            expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("firstname"); 
+            expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("lastname");
 
-            const checkUniquenessFn = async function () {
-
-                const queryRunner = connection.createQueryRunner();
-                const tableSchema = await queryRunner.loadTableSchema("person");
-                await queryRunner.release();
-    
-                expect(tableSchema!.indices.length).to.be.equal(1);
-                expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
-                expect(tableSchema!.indices[0].isUnique).to.be.true; 
-                expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
-                expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("firstname"); 
-                expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("lastname");
-
-            };
-
-            const checkColumnChangeFn = async function () {
-
-                const queryRunner = connection.createQueryRunner();
-                const tableSchema = await queryRunner.loadTableSchema("person");
-                await queryRunner.release();
-    
-                expect(tableSchema!.indices.length).to.be.equal(1);
-                expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
-                expect(tableSchema!.indices[0].isUnique).to.be.false; 
-                expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
-                expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("lastname");
-                expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("firstname"); 
-
-            };
-            await checkCreateFn();
-        
-            const entityMetadata1 = connection.entityMetadatas.find(x => x.name === "Person");
-            const indexMetadata1 = entityMetadata1!.indices.find(x => x.name === "IDX_TEST");
-            indexMetadata1!.isUnique = true;
+        })));
+            
+        it("should update the index to be unique", () => Promise.all(connections.map(async connection => {
+            
+            const entityMetadata = connection.entityMetadatas.find(x => x.name === "Person");
+            const indexMetadata = entityMetadata!.indices.find(x => x.name === "IDX_TEST");
+            indexMetadata!.isUnique = true;
 
             await connection.synchronize(false);
-            await checkUniquenessFn();
+
+            const queryRunner = connection.createQueryRunner();
+            const tableSchema = await queryRunner.loadTableSchema("person");
+            await queryRunner.release();
+
+            expect(tableSchema!.indices.length).to.be.equal(1);
+            expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
+            expect(tableSchema!.indices[0].isUnique).to.be.true; 
+            expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
+            expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("firstname"); 
+            expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("lastname");
+
+        })));
+
+        it("should update the index swaping the 2 columns", () => Promise.all(connections.map(async connection => {
             
-            const entityMetadata2 = connection.entityMetadatas.find(x => x.name === "Person");
-            entityMetadata2!.indices = [new IndexMetadata({
-                entityMetadata: <EntityMetadata>entityMetadata2,
+            const entityMetadata = connection.entityMetadatas.find(x => x.name === "Person");
+            entityMetadata!.indices = [new IndexMetadata({
+                entityMetadata: <EntityMetadata>entityMetadata,
                 args: {
                     target: Person,
                     name: "IDX_TEST",
@@ -85,10 +68,20 @@ describe("indices > reading index from entity schema and updating database", () 
                     unique: false
                 }
             })];
-            entityMetadata2!.indices.forEach(index => index.build(connection.namingStrategy));
+            entityMetadata!.indices.forEach(index => index.build(connection.namingStrategy));
 
             await connection.synchronize(false);
-            await checkColumnChangeFn();
+
+            const queryRunner = connection.createQueryRunner();
+            const tableSchema = await queryRunner.loadTableSchema("person");
+            await queryRunner.release();
+
+            expect(tableSchema!.indices.length).to.be.equal(1);
+            expect(tableSchema!.indices[0].name).to.be.equal("IDX_TEST"); 
+            expect(tableSchema!.indices[0].isUnique).to.be.false; 
+            expect(tableSchema!.indices[0].columnNames.length).to.be.equal(2); 
+            expect(tableSchema!.indices[0].columnNames[0]).to.be.equal("lastname");
+            expect(tableSchema!.indices[0].columnNames[1]).to.be.equal("firstname"); 
 
         })));
 
