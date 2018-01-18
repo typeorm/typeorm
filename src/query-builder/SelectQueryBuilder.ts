21cae41004b459e037227735a2ef1ccda71ddf9d
Brendan Roche <brendan@kounta.com>
2018-01-19 10:53:55 +1100

    #1469 EntityManager.find (and related methods): add support for conditions and options

--- src/query-builder/SelectQueryBuilder.ts
+++ src/query-builder/SelectQueryBuilder.ts
@@ -697,7 +697,7 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
      * Adds new AND WHERE condition in the query builder.
      * Additionally you can add parameters used in where expression.
      */
-    andWhere(where: string|Brackets|((qb: this) => string), parameters?: ObjectLiteral): this {
+    andWhere(where: string|Brackets|((qb: this) => string)|ObjectLiteral|ObjectLiteral[], parameters?: ObjectLiteral): this {
         this.expressionMap.wheres.push({ type: "and", condition: this.computeWhereParameter(where) });
         if (parameters) this.setParameters(parameters);
         return this;
