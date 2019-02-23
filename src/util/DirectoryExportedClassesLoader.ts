2666b8d7313ee9c39fc95f22bdf981d3eb8758d4
Gints Polis <gints.polis@lattelecom.lv>
2019-02-23 09:01:48 +0200

    Moves directory search to it's own function

--- src/util/DirectoryExportedClassesLoader.ts
+++ src/util/DirectoryExportedClassesLoader.ts
@@ -20,18 +20,41 @@ export function importClassesFromDirectories(directories: string[], formats = ["
         return allLoaded;
     }
 
-    const allFiles = directories.reduce((allDirs, dir) => {
-        return allDirs.concat(PlatformTools.load("glob").sync(PlatformTools.pathNormalize(dir)));
-    }, [] as string[]);
-
-    const dirs = allFiles
-        .filter(file => {
-            const dtsExtension = file.substring(file.length - 5, file.length);
-            return formats.indexOf(PlatformTools.pathExtname(file)) !== -1 && dtsExtension !== ".d.ts";
-        })
-        .map(file => PlatformTools.load(PlatformTools.pathResolve(file)));
+    const dirs = this.getAllDirs(directories, formats).map((file: string) => {
+        PlatformTools.load(PlatformTools.pathResolve(file));
+    });
 
     return loadFileClasses(dirs, []);
+}
+
+/**
+ * Gets all files for given directory mask
+ * @param directories array of directory path's
+ * @param formats file extensions to filter out
+ */
+export function getAllDirs(
+    directories: string[],
+    formats = [".js", ".ts"]
+): string[] {
+    const allFiles = directories.reduce(
+        (allDirs, dir) => {
+            return allDirs.concat(
+                PlatformTools.load("glob").sync(
+                    PlatformTools.pathNormalize(dir)
+                )
+            );
+        },
+        [] as string[]
+    );
+
+    return allFiles
+        .filter(file => {
+            const dtsExtension = file.substring(file.length - 5, file.length);
+            return (
+                formats.indexOf(PlatformTools.pathExtname(file)) !== -1 &&
+                dtsExtension !== ".d.ts"
+            );
+        });
 }
 
 /**
