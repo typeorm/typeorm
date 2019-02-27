9b04ce91d7a8a9511593dd394b29fd4d88bf497d
Chris Suich <chris@stackforce.com>
2019-02-27 17:16:51 -0500

    Fixed issue with loading realations whenn all relationIds are numbers

--- src/query-builder/RelationIdLoader.ts
+++ src/query-builder/RelationIdLoader.ts
@@ -214,7 +214,7 @@ export class RelationIdLoader {
                 const areAllNumbers = values.every(value => typeof value === "number");
 
                 if (areAllNumbers) {
-                    condition2 = `${mainAlias}.${columns[0].propertyPath} IN (${values.join(", ")})`;
+                    condition2 = `${mainAlias}.${inverseColumns[0].propertyPath} IN (${values.join(", ")})`;
                 } else {
                     qb.setParameter("values2", values);
                     condition2 = mainAlias + "." + inverseColumns[0].propertyPath + " IN (:...values2)"; // todo: use ANY for postgres
