a32afbc73b1a80396178f9ebd3fa2e5a6952ffcc
Egor <egorzaremba@gmail.com>
2019-06-18 18:37:22 +0300

    style: fix linting

--- test/github-issues/3309/issue-3309.ts
+++ test/github-issues/3309/issue-3309.ts
@@ -1,35 +1,33 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
 import {Connection} from "../../../src/connection/Connection";
-import {Photo} from "./entity/Photo"
-import {Member} from "./entity/Member"
+import {Photo} from "./entity/Photo";
+import {Member} from "./entity/Member";
 import {expect} from "chai";
 
-describe.only("github issues > #3309 findOptions uses same parameter for different where options", () => {
+describe("github issues > #3309 findOptions uses same parameter for different where options", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["postgres"],
         dropSchema: true,
-        logging: true
     }));
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
 
     it("should returns 2 object correctly", () => Promise.all(connections.map(async connection => {
-        interface objInterface {
-            [key: string]: Photo
+        interface ObjInterface {
+            [key: string]: Photo;
         }
-        const obj:objInterface = {}
-
-        const array: string[] = ['old_type', 'new_type', 'old_type', 'new_type', 'new_type']
+        const obj: ObjInterface = {};
+        const array: string[] = ["old_type", "new_type", "old_type", "new_type", "new_type"];
         array.forEach(async (type: string, i: number) => {
             const index = i + 1;
             obj[`photo${index}`] = new Photo();
             obj[`photo${index}`].type = type;
             await connection.manager.save(obj[`photo${index}`]);
-        })
+        });
 
         await connection.synchronize();
 
@@ -52,10 +50,10 @@ describe.only("github issues > #3309 findOptions uses same parameter for differe
                 member: {
                     id: 2
                 },
-                type: 'new_type'
+                type: "new_type"
             }
         });
-        expect([result[0].type, result[1].type]).to.be.eql(['new_type', 'new_type']);
+        expect([result[0].type, result[1].type]).to.be.eql(["new_type", "new_type"]);
 
     })));
 
