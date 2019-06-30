a308ee27daa16cc4d7f7f5cfca4248e67cc05713
Liau Jian Jie <liaujianjie@gmail.com>
2019-06-30 20:58:06 +0800

    Add more tests

--- test/functional/cube/postgres/cube-postgres.ts
+++ test/functional/cube/postgres/cube-postgres.ts
@@ -6,6 +6,7 @@ import {
     createTestingConnections,
     reloadTestingDatabases
 } from "../../../utils/test-utils";
+import { Post } from "./entity/Post";
 
 describe("cube-postgres", () => {
     let connections: Connection[];
@@ -31,6 +32,85 @@ describe("cube-postgres", () => {
                         tableColumn.type === "cube"
                 );
                 expect(cubeColumn).to.not.be.undefined;
+            })
+        ));
+
+    it("should persist cube correctly", () =>
+        Promise.all(
+            connections.map(async connection => {
+                const color = [255, 0, 0];
+                const postRepo = connection.getRepository(Post);
+                const post = new Post();
+                post.color = color;
+                const persistedPost = await postRepo.save(post);
+                const foundPost = await postRepo.findOne(persistedPost.id);
+                expect(foundPost).to.exist;
+                expect(foundPost!.color).to.deep.equal(color);
+            })
+        ));
+
+    it("should update cube correctly", () =>
+        Promise.all(
+            connections.map(async connection => {
+                const color = [255, 0, 0];
+                const color2 = [0, 255, 0];
+                const postRepo = connection.getRepository(Post);
+                const post = new Post();
+                post.color = color;
+                const persistedPost = await postRepo.save(post);
+
+                await postRepo.update(
+                    { id: persistedPost.id },
+                    { color: color2 }
+                );
+
+                const foundPost = await postRepo.findOne(persistedPost.id);
+                expect(foundPost).to.exist;
+                expect(foundPost!.color).to.deep.equal(color2);
+            })
+        ));
+
+    it("should re-save cube correctly", () =>
+        Promise.all(
+            connections.map(async connection => {
+                const color = [255, 0, 0];
+                const color2 = [0, 255, 0];
+                const postRepo = connection.getRepository(Post);
+                const post = new Post();
+                post.color = color;
+                const persistedPost = await postRepo.save(post);
+
+                persistedPost.color = color2;
+                await postRepo.save(persistedPost);
+
+                const foundPost = await postRepo.findOne(persistedPost.id);
+                expect(foundPost).to.exist;
+                expect(foundPost!.color).to.deep.equal(color2);
+            })
+        ));
+
+    it("should be able to order cube by euclidean distance", () =>
+        Promise.all(
+            connections.map(async connection => {
+                const color1 = [255, 0, 0];
+                const color2 = [255, 255, 0];
+                const color3 = [255, 255, 255];
+
+                const post1 = new Post();
+                post1.color = color1;
+                const post2 = new Post();
+                post2.color = color2;
+                const post3 = new Post();
+                post3.color = color3;
+                await connection.manager.save([post1, post2, post3]);
+
+                const posts = await connection.manager
+                    .createQueryBuilder(Post, "post")
+                    .orderBy("color <-> '(0, 255, 0)'", "DESC")
+                    .getMany();
+
+                const postIds = posts.map(post => post.id);
+                expect(postIds).to.deep.equal([post1.id, post3.id, post2.id]);
             })
         ));
 });
