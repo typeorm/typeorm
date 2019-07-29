bea9048a5f5491f658536f0773b4566b524d81cf
Logan Yott <loganyott@gmail.com>
2019-07-29 16:25:48 -0600

    test: fixing tests and removing .only

--- test/github-issues/2418/issue-2418.ts
+++ test/github-issues/2418/issue-2418.ts
@@ -1,9 +1,9 @@
 import {Connection} from "../../../src";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
 import {Category} from "./entity/Category";
-import { expect } from "chai";
+import {expect} from "chai";
 
-describe.only("github issues > #2418 mpath is not updated on parent update", () => {
+describe("github issues > #2418 mpath is not updated on parent update", () => {
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
@@ -13,79 +13,82 @@ describe.only("github issues > #2418 mpath is not updated on parent update", () 
     after(() => closeTestingConnections(connections));
 
     it("Should update ancestors on parent change", () => Promise.all(connections.map(async connection => {
-        const grandparent = new Category();
-        grandparent.title = "Grandparent";
-        await connection.manager.save(grandparent);
+        const {manager} = connection;
 
-        const parent = new Category();
-        parent.title = "Parent";
-        parent.parent = Promise.resolve(grandparent);
-        await connection.manager.save(parent);
+        const grandparent = await manager.save(Object.assign(new Category(), {
+            title: "Grandparent"
+        }));
 
-        const child = new Category();
-        child.title = "Child";
-        child.parent = Promise.resolve(parent);
-        await connection.manager.save(child);
+        const parent = await manager.save(Object.assign(new Category(), {
+            title: "Parent",
+            parent: Promise.resolve(grandparent),
+        }));
+
+        const child = await manager.save(Object.assign(new Category(), {
+            title: "Child",
+            parent: Promise.resolve(parent),
+        }));
 
         let [ancestors, ancestorCount] = await Promise.all([
-            connection.manager.getTreeRepository(Category).findAncestors(child),
-            connection.manager.getTreeRepository(Category).countAncestors(child),
+            manager.getTreeRepository(Category).findAncestors(child),
+            manager.getTreeRepository(Category).countAncestors(child),
         ]);
-        expect(ancestorCount).to.be.equal(3, "Ancestor relationships not created as expected");
-        expect(ancestors).to.deep.include(grandparent, "Grandparent not included in first ancestors");
-        expect(ancestors).to.deep.include(parent, "Parent not included in first ancestors");
+        expect(ancestors).to.deep.include.members([grandparent, parent].map(transformCategory));
+        expect(ancestorCount).to.be.equal(3, "Ancestors updated as expected");
 
         child.parent = Promise.resolve(grandparent);
-        await connection.manager.save(child);
+        await manager.save(child);
 
         [ancestors, ancestorCount] = await Promise.all([
-            connection.manager.getTreeRepository(Category).findAncestors(child),
-            connection.manager.getTreeRepository(Category).countAncestors(child),
+            manager.getTreeRepository(Category).findAncestors(child),
+            manager.getTreeRepository(Category).countAncestors(child),
         ]);
-        expect(ancestors).to.deep.include(grandparent, "Grandparent not included in ancestors after change");
-        expect(ancestors).to.not.deep.include(parent, "Parent still included in ancestors after change");
+        expect(ancestors).to.deep.include(transformCategory(grandparent), "Grandparent still included in ancestors after change");
+        expect(ancestors).to.not.deep.include(transformCategory(parent), "Parent not included in ancestors after change");
         expect(ancestorCount).to.be.equal(2, "Ancestors not updated correctly");
     })));
 
     it("Should update children's ancestors on parent change", () => Promise.all(connections.map(async connection => {
-        const grandparent = new Category();
-        grandparent.title = "Grandparent";
-        await connection.manager.save(grandparent);
+        const {manager} = connection;
 
-        const parent = new Category();
-        parent.title = "Parent";
-        parent.parent = Promise.resolve(grandparent);
-        await connection.manager.save(parent);
+        const grandparent = await manager.save(Object.assign(new Category(), {
+            title: "Grandparent"
+        }));
 
-        const child = new Category();
-        child.title = "Child";
-        child.parent = Promise.resolve(parent);
-        await connection.manager.save(child);
+        const parent = await manager.save(Object.assign(new Category(), {
+            title: "Parent",
+            parent: Promise.resolve(grandparent),
+        }));
 
-        const grandchild = new Category();
-        grandchild.title = "Grandchild";
-        grandchild.parent = Promise.resolve(child);
-        await connection.manager.save(grandchild);
+        const child = await manager.save(Object.assign(new Category(), {
+            title: "Child",
+            parent: Promise.resolve(parent),
+        }));
+
+        const grandchild = await manager.save(Object.assign(new Category(), {
+            title: "Grandchild",
+            parent: Promise.resolve(child),
+        }));
 
         let [ancestors, ancestorCount] = await Promise.all([
-            connection.manager.getTreeRepository(Category).findAncestors(grandchild),
-            connection.manager.getTreeRepository(Category).countAncestors(grandchild),
+            manager.getTreeRepository(Category).findAncestors(grandchild),
+            manager.getTreeRepository(Category).countAncestors(grandchild),
         ]);
-        expect(ancestorCount).to.be.equal(4, "Ancestor relationships not created as expected");
-        expect(ancestors).to.deep.include(grandparent, "Grandparent not included in first ancestors");
-        expect(ancestors).to.deep.include(parent, "Parent not included in first ancestors");
-        expect(ancestors).to.deep.include(child, "Child not included in first ancestors");
+        expect(ancestorCount).to.be.equal(4, "Ancestor relationships created as expected");
+        expect(ancestors).to.deep.include.members([grandparent, parent, child].map(transformCategory));
 
         child.parent = Promise.resolve(grandparent);
-        await connection.manager.save(child);
+        await manager.save(child);
 
         [ancestors, ancestorCount] = await Promise.all([
-            connection.manager.getTreeRepository(Category).findAncestors(grandchild),
-            connection.manager.getTreeRepository(Category).countAncestors(grandchild),
+            manager.getTreeRepository(Category).findAncestors(grandchild),
+            manager.getTreeRepository(Category).countAncestors(grandchild),
         ]);
-        expect(ancestors).to.deep.include(grandparent, "Grandparent not included in ancestors after change");
-        expect(ancestors).to.not.deep.include(parent, "Parent still included in ancestors after change");
-        expect(ancestors).to.deep.include(grandparent, "Child not included in ancestors after change");
-        expect(ancestorCount).to.be.equal(3, "Ancestors not updated correctly");
+        expect(ancestors).to.deep.include(transformCategory(grandparent), "Grandparent still included in ancestors after change");
+        expect(ancestors).to.not.deep.include(transformCategory(parent), "Parent not included in ancestors after change");
+        expect(ancestors).to.deep.include(transformCategory(child), "Child still included in ancestors after change");
+        expect(ancestorCount).to.be.equal(3, "Ancestor count updated correctly");
     })));
 });
+
+const transformCategory = ({id, title}: Category) => ({id, title});
