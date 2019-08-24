1454128f8dcdf08d307e7735bc95beb08bea6641
Duckies <gilbert.johnt@gmail.com>
2019-08-24 00:45:22 -0400

    Patch to allow apostrophes in postgres enum string.

---
+++ test/github-issues/4630/issue-4630.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import { Realm } from './entity/User';
+import {User} from "./entity/User";
+
+describe("github issues > #4360 Enum string not escaping resulting in broken migrations.", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("should should allow postgres enums to have apostrophes in the values", () => Promise.all(connections.map(async connection => {
+        const user = new User()
+        user.realm = Realm.KelThuzad;
+
+        await connection.manager.save(user);
+
+        const users = await connection.manager.find(User);
+
+        users.should.eql([{
+            id: 1,
+            realm: "Kel'Thuzad"
+        }])
+    })));
+});
