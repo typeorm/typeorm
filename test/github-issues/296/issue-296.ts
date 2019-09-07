0e402387fdde4ddf7921dfad61bfa64aeeb4778f
Asare Worae <asare.w@gmail.com>
2019-09-07 09:27:49 +0200

    ensure proper select operator based on connection driver

--- test/github-issues/296/issue-296.ts
+++ test/github-issues/296/issue-296.ts
@@ -14,25 +14,28 @@ describe("github issues > #296 select additional computed columns", () => {
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
 
+    it("should correctly select computed columns and populate given entity properties", () => Promise.all(connections.map(async connection => {
 
-    it("should correctly select computed size of text column and populate given entity property", () => Promise.all(connections.map(async connection => {
         const post = new Post();
         post.title = "hello post";
         post.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu";
         await connection.manager.save(post);
 
-        const query = connection.manager
-            .createQueryBuilder(Post, "post");
+        const lengthOperator = connection.driver.options.type === "mssql" ? "LEN" : "LENGTH";
+        const subStrOperation = ["sqljs", "sqlite"].indexOf(connection.driver.options.type) > -1 ? "SUBSTR" : "SUBSTRING";
+
+        const query = connection.manager.createQueryBuilder(Post, "post");
         const loadedPost = await query
             .select("post.id")
-            .addSelect("post.text")
-            .addSelect("LENGTH(post.text)", `${query.alias}_textSize`)
+            .addSelect(`${subStrOperation}(post.text, 1, 20)`, "post_text")
+            .addSelect(`${lengthOperator}(post.text)`, `${query.alias}_textSize`)
             .getOne();
 
         expect(loadedPost).not.to.be.undefined;
         expect(loadedPost!.text).not.to.be.undefined;
-        // Because some drivers return int's as strings's, so just to be sure we
-        // cast both to string
+        expect(loadedPost!.text!.length).to.be.equal(20);
+        // Some drivers return function int's as strings's, so just to be sure
+        // we cast both to string
         expect("" + loadedPost!.textSize).to.be.equal("" + post.text.length);
     })));
 });
