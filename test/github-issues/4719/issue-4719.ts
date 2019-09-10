7b5c1f3012c9906917dfa48f80829b9c8a1dafc6
Toby Hinloopen <toby@bonaroo.nl>
2019-09-10 17:10:47 +0200

    Add hstore-injection test

--- test/github-issues/4719/issue-4719.ts
+++ test/github-issues/4719/issue-4719.ts
@@ -4,7 +4,6 @@ import {Connection} from "../../../src/connection/Connection";
 import {Post} from "./entity/Post";
 
 describe("github issues > #4719 HStore with empty string values", () => {
-
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
@@ -24,6 +23,19 @@ describe("github issues > #4719 HStore with empty string values", () => {
       const loadedPost = await postRepository.findOneOrFail(id);
       loadedPost.hstoreObj.should.be.deep.equal(
         { name: "Alice", surname: "A", age: "25", blank: "", "": "blank-key", "\"": "\"", foo: null });
+      await queryRunner.release();
+    })));
+
+    it("should not allow 'hstore injection'", () => Promise.all(connections.map(async connection => {
+      const queryRunner = connection.createQueryRunner();
+      const postRepository = connection.getRepository(Post);
+
+      const post = new Post();
+      post.hstoreObj = { username: `", admin=>"1`, admin: "0" };
+      const {id} = await postRepository.save(post);
+
+      const loadedPost = await postRepository.findOneOrFail(id);
+      loadedPost.hstoreObj.should.be.deep.equal({ username: `", admin=>"1`, admin: "0" });
       await queryRunner.release();
     })));
 });
