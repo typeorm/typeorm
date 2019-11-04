b74a64ab0cf45b0d8f74fa3111a7fd3bb6241677
bthompson <bthompson@fortherecord.com>
2019-11-04 13:36:38 +1000

    FTRX-5492 Update the deep partial type to work with typescript 3.6, because typescript haven't released a fix yet

--- src/common/DeepPartial.ts
+++ src/common/DeepPartial.ts
@@ -1,6 +1,9 @@
 /**
  * Same as Partial<T> but goes deeper and makes Partial<T> all its properties and sub-properties.
+ * Implemented in FTRX-5492 to support upgrading typescript to 3.6.
+ * This is not the latest version of the code provided by typeorm, but it fixes beast.
+ * @see https://github.com/Microsoft/TypeScript/issues/21592#issuecomment-496723647
  */
 export type DeepPartial<T> = {
-    [P in keyof T]?: DeepPartial<T[P]>;
+    [P in keyof T]?: T[P] extends never ? DeepPartial<T[P]> : DeepPartial<T[P]>
 };
