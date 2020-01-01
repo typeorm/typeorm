99ddb968dc9ab5c9d0801b33c7d1cb6ce7883f58
duckies <gilbert.johnt@gmail.com>
2020-01-01 18:18:30 -0500

    Convert enum numbers after spaced strings

--- src/driver/postgres/PostgresDriver.ts
+++ src/driver/postgres/PostgresDriver.ts
@@ -537,10 +537,6 @@ export class PostgresDriver implements Driver {
             if (columnMetadata.isArray) {
                 // manually convert enum array to array of values (pg does not support, see https://github.com/brianc/node-pg-types/issues/56)
                 value = value !== "{}" ? (value as string).substr(1, (value as string).length - 2).split(",") : [];
-                // convert to number if that exists in possible enum options
-                value = value.map((val: string) => {
-                    return !isNaN(+val) && columnMetadata.enum!.indexOf(parseInt(val)) >= 0 ? parseInt(val) : val;
-                });
                 // Remove extraneous delimiting quotes around enum strings with spaces.
                 value = value.map((val: string) => {
                     // Returns an empty string if the length is negative.
@@ -548,6 +544,11 @@ export class PostgresDriver implements Driver {
                     
                     return columnMetadata.enum!.indexOf(stripped) >= 0 ? stripped : val;
                 });
+                // convert to number if that exists in possible enum options
+                value = value.map((val: string) => {
+                    return !isNaN(+val) && columnMetadata.enum!.indexOf(parseInt(val)) >= 0 ? parseInt(val) : val;
+                });
+
             } else {
                 // convert to number if that exists in possible enum options
                 value = !isNaN(+value) && columnMetadata.enum!.indexOf(parseInt(value)) >= 0 ? parseInt(value) : value;
