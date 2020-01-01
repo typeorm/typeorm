7f1e9ada479de54ae8fd8e49c8733c4f2c9df911
duckies <gilbert.johnt@gmail.com>
2020-01-01 18:37:19 -0500

    Remove logging and test enum strings without arrays.

--- test/github-issues/RENAME/issue-5275.ts
+++ test/github-issues/RENAME/issue-5275.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata";
 import { Connection } from "../../../src";
 import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
-import { User } from "./entity/User";
+import { User, Role } from "./entity/User";
+import { UserTwo } from "./entity/UserTwo";
 
-describe("github issues > #5275 Enums with spaces are not converted properly.", () => {
+describe.only("github issues > #5275 Enums with spaces are not converted properly.", () => {
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         schemaCreate: true,
         dropSchema: true,
-        logging: true,
         enabledDrivers: ["postgres"]
     }));
 
@@ -19,13 +19,26 @@ describe("github issues > #5275 Enums with spaces are not converted properly.", 
     it("should correctly parse enums of strings with spaces", () => Promise.all(connections.map(async connection => {
         const userRepository = connection.getRepository(User);
 
-        await userRepository.save({ id: 1 });
+        await userRepository.save({ id: 1, roles: [Role.GuildMaster, Role.Officer, Role.Number, Role.PlayerAlt] });
 
         const user = await userRepository.findOneOrFail(1);
 
         user.should.eql({
             id: 1,
-            roles: ["Guild Master"]
+            roles: ["Guild Master", "Officer", 1, "Player Alt"]
+        });
+    })));
+
+    it("should correctly parse non-array enums with spaces", () => Promise.all(connections.map(async connection => {
+        const userRepository = connection.getRepository(UserTwo);
+
+        await userRepository.save({ id: 1, role: Role.GuildMaster });
+
+        const user = await userRepository.findOneOrFail(1);
+
+        user.should.eql({
+            id: 1,
+            role: "Guild Master"
         });
     })));
 });
