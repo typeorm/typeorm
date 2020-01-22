49e59988d0df18e9d085bbba6a610e5c7284ad35
Ju Hyuk Park <jpark@roo.kr>
2020-01-22 19:43:54 +0900

    typeorm/typeorm#3636 synchronize drops json column on mariadb

---
+++ test/github-issues/3636/entity/Post.ts
@@ -0,0 +1,17 @@
+import {Column} from "../../../../src/decorator/columns/Column";
+import {PrimaryColumn} from "../../../../src/decorator/columns/PrimaryColumn";
+import {Entity} from "../../../../src/decorator/entity/Entity";
+
+@Entity()
+export class Post {
+
+    @PrimaryColumn()
+    id: number;
+
+    @Column({
+        type: "json",
+        default: "{}"
+    })
+    data: any;
+
+}
