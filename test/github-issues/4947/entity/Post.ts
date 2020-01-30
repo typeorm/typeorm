a7585af8f66c86c09cc766abb07fac65c5202b95
simpert <tommysimper@hotmail.com>
2020-01-29 20:14:03 -0700

    fix: issue entity object not passed to subscriber
    Closes: #4947

---
+++ test/github-issues/4947/entity/Post.ts
@@ -0,0 +1,20 @@
+import {Entity} from "../../../../src/decorator/entity/Entity";
+import {PrimaryGeneratedColumn} from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
+import {Column} from "../../../../src/decorator/columns/Column";
+import {UpdateDateColumn} from "../../../../src/decorator/columns/UpdateDateColumn";
+
+@Entity()
+export class Post {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({nullable: true})
+    title?: string;
+
+    @UpdateDateColumn()
+    updateDate: Date;
+
+    @Column()
+    dateModified: Date;
+}
