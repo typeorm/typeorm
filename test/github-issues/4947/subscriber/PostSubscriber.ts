b9e0df1fbcc793ffacb42272d1c98a1a7fdb9f39
simpert <tommysimper@hotmail.com>
2020-01-29 20:47:03 -0700

    fix: formatting

--- test/github-issues/4947/subscriber/PostSubscriber.ts
+++ test/github-issues/4947/subscriber/PostSubscriber.ts
@@ -1,9 +1,5 @@
 import {Post} from "../entity/Post";
-<<<<<<< HEAD
 import {EntitySubscriberInterface, EventSubscriber, UpdateEvent, InsertEvent} from "../../../../src";
-=======
-import {EntitySubscriberInterface,EventSubscriber,UpdateEvent,InsertEvent} from "../../../../src";
->>>>>>> d50acad246920ce5e7fcca6b119f14039861e98c
 
 @EventSubscriber()
 export class PostSubscriber implements EntitySubscriberInterface<Post> {
@@ -12,28 +8,16 @@ export class PostSubscriber implements EntitySubscriberInterface<Post> {
     }
 
     beforeUpdate(event: UpdateEvent<Post>) {
-<<<<<<< HEAD
         if (event.entity) {
             event.entity.dateModified = new Date();
             event.entity.title = 'set in subscriber when updated';
-=======
-        if(event.entity) {
-            event.entity.dateModified=new Date();
-            event.entity.title='set in subscriber when updated';
->>>>>>> d50acad246920ce5e7fcca6b119f14039861e98c
         }
     }
 
     beforeInsert(event: InsertEvent<Post>) {
-<<<<<<< HEAD
         if (event.entity) {
             event.entity.dateModified = new Date();
             event.entity.title = 'set in subscriber when created';
-=======
-        if(event.entity) {
-            event.entity.dateModified=new Date();
-            event.entity.title='set in subscriber when created';
->>>>>>> d50acad246920ce5e7fcca6b119f14039861e98c
         }
     }
 }
