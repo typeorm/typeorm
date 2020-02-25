f441fb9ad3ac8c9f620bf27d28d0d89bf0ef1578
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:13:40 +1100

    refactor: equal findoperator extends base class

--- src/find-options/operator/Equal.ts
+++ src/find-options/operator/Equal.ts
@@ -1,9 +1,18 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: Equal("value") }
  */
-export function Equal<T>(value: T|FindOperator<T>) {
-    return new FindOperator("equal", value);
+class EqualOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} = ${parameters[0]}`;
+    }
 }
+
+export const Equal = <T>(value: T|FindOperator<T>): EqualOperator<T> => new EqualOperator<T>(value);
