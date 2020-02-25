9d996bb18004994b26a235657ba382adc588fd56
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:18:56 +1100

    refactor: in findoperator extends base class

--- src/find-options/operator/In.ts
+++ src/find-options/operator/In.ts
@@ -1,9 +1,18 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: In([...]) }
  */
-export function In<T>(value: T[]|FindOperator<T>) {
-    return new FindOperator("in", value as any, true, true);
+class InOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value, true, true);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} IN (${parameters.join(", ")})`;
+    }
 }
+
+export const In = <T>(value: T[] | FindOperator<T>) => new InOperator<T>(value as any);
