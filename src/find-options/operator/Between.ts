a53b5d500335a24c863d63b2fd76052c2719a043
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:12:01 +1100

    refactor: between operator extends base class

--- src/find-options/operator/Between.ts
+++ src/find-options/operator/Between.ts
@@ -1,9 +1,20 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: Between(x, y) }
  */
-export function Between<T>(from: T|FindOperator<T>, to: T|FindOperator<T>) {
-    return new FindOperator("between", [from, to] as any, true, true);
+
+class BetweenOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value, true, true);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} BETWEEN ${parameters[0]} AND ${parameters[1]}`;
+    }
+
 }
+
+export const Between = <T>(from: T | FindOperator<T>, to: T | FindOperator<T>) => new BetweenOperator<T>([from, to] as any);
