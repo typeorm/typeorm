245558404a53173dc7f31a8dbf97078a3ce41a4d
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:25:15 +1100

    refactor: lessthan operator extends base class

--- src/find-options/operator/LessThan.ts
+++ src/find-options/operator/LessThan.ts
@@ -1,9 +1,19 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: LessThan(10) }
  */
-export function LessThan<T>(value: T|FindOperator<T>) {
-    return new FindOperator("lessThan", value);
+class LessThanOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} < ${parameters[0]}`;
+
+    }
 }
+
+export const LessThan = <T>(value: T | FindOperator<T>) => new LessThanOperator<T>(value);
