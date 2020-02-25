7ea837cc80c7b28b1ea6987b965be30215958618
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:53:57 +1100

    refactor: not operator extends base class

--- src/find-options/operator/Not.ts
+++ src/find-options/operator/Not.ts
@@ -1,10 +1,24 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Used to negotiate expression.
  * Example: { title: not("hello") } will return entities where title not equal to "hello".
  */
-export function Not<T>(value: T|FindOperator<T>) {
-    return new FindOperator("not", value);
+class NotOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        if (this._value instanceof FindOperator) {
+            return `NOT(${this._value.toSql(connection, aliasPath, parameters)})`;
+        } else {
+            return `${aliasPath} != ${parameters[0]}`;
+        }
+    }
 }
+
+
+export const Not = <T>(value: T | FindOperator<T>) => new NotOperator<T>(value);
