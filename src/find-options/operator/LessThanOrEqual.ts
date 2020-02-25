6092333bde6210c5307568537c4ec42c3bbb4443
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:27:25 +1100

    refactor: lessthanorequal operator extends base class

--- src/find-options/operator/LessThanOrEqual.ts
+++ src/find-options/operator/LessThanOrEqual.ts
@@ -1,9 +1,19 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: LessThanOrEqual(10) }
  */
-export function LessThanOrEqual<T>(value: T|FindOperator<T>) {
-    return new FindOperator("lessThanOrEqual", value);
+class LessThanOrEqualOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} <= ${parameters[0]}`;
+    }
 }
+
+
+export const LessThanOrEqual = <T>(value: T | FindOperator<T>) => new LessThanOrEqualOperator<T>(value);
