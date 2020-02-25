ec690fc025d245b2124c0d0b828c403c2433a478
Wafik Salim <wafiks@gmail.com>
2020-02-25 23:34:21 +1100

    refactor: morethan operator extends base class

--- src/find-options/operator/MoreThan.ts
+++ src/find-options/operator/MoreThan.ts
@@ -1,9 +1,19 @@
 import {FindOperator} from "../FindOperator";
+import {Connection} from "../..";
 
 /**
  * Find Options Operator.
  * Example: { someField: MoreThan(10) }
  */
-export function MoreThan<T>(value: T|FindOperator<T>) {
-    return new FindOperator("moreThan", value);
+class MoreThanOperator<T> extends FindOperator<T> {
+    constructor(value: FindOperator<T> | T) {
+        super(value);
+    }
+
+    toSql(connection: Connection, aliasPath: string, parameters: string[]): string {
+        return `${aliasPath} > ${parameters[0]}`;
+
+    }
 }
+
+export const MoreThan = <T>(value: T | FindOperator<T>) => new MoreThanOperator<T>(value);
