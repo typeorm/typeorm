34e7f0a49d87a4fc10b4800819ec0d132f0c59ef
julius <julius.friedrich@shift-f3.com>
2020-03-08 00:23:09 +0100

    only disconnect connection if connected.

--- test/github-issues/2096/issue-2096.ts
+++ test/github-issues/2096/issue-2096.ts
@@ -1,13 +1,13 @@
 import "reflect-metadata";
-import {expect} from "chai";
-import {createConnection, getConnectionManager} from "../../../src";
+import { expect } from "chai";
+import { createConnection, getConnectionManager } from "../../../src";
 
 describe("github issues > #2096 [mysql] Database name isn't read from url", () => {
     it("should be possible to define a database by connection url for mysql", async () => {
         const connection = getConnectionManager().connections.find(
             c => c.name.indexOf("mysql") > -1
         );
-        if (connection) {
+        if (connection && connection.isConnected) {
             await connection.close();
         }
         // it is important to synchronize here, to trigger EntityMetadataValidator.validate
