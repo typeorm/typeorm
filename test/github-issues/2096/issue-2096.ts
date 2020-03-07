f80be4177cf613c6ae37396d0174e7d2cb593d49
julius <julius.friedrich@shift-f3.com>
2020-03-08 00:03:21 +0100

    fix: respect database from connection urls
    database names can be defined in the options object. Now also connection urls that contain a database can be used to have the database set in the drivers object.
    
    Closes: #2096

---
+++ test/github-issues/2096/issue-2096.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import {expect} from "chai";
+import {createConnection, getConnectionManager} from "../../../src";
+
+describe("github issues > #2096 [mysql] Database name isn't read from url", () => {
+    it("should be possible to define a database by connection url for mysql", async () => {
+        const connection = getConnectionManager().connections.find(
+            c => c.name.indexOf("mysql") > -1
+        );
+        if (connection) {
+            await connection.close();
+        }
+        // it is important to synchronize here, to trigger EntityMetadataValidator.validate
+        // that previously threw the error where the database on the driver object was undefined
+        const newConnection = await createConnection({
+            url: "mysql://test:test@localhost:3306/test",
+            entities: [__dirname + "/entity/*{.js,.ts}"],
+            synchronize: true,
+            type: "mysql"
+        });
+        expect(newConnection.isConnected).to.eq(true);
+    });
+});
