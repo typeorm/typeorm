0fa78a264f819bd7b7251af50b40e0b44b3ded1c
julius <julius.friedrich@shift-f3.com>
2020-03-08 01:34:36 +0100

    fix credentials for testing

--- test/github-issues/2096/issue-2096.ts
+++ test/github-issues/2096/issue-2096.ts
@@ -1,23 +1,19 @@
 import "reflect-metadata";
-import { expect } from "chai";
-import { createConnection, getConnectionManager } from "../../../src";
+import {expect} from "chai";
+import {Connection} from "../../../src";
 
 describe("github issues > #2096 [mysql] Database name isn't read from url", () => {
     it("should be possible to define a database by connection url for mysql", async () => {
-        const connection = getConnectionManager().connections.find(
-            c => c.name.indexOf("mysql") > -1
-        );
-        if (connection && connection.isConnected) {
-            await connection.close();
-        }
         // it is important to synchronize here, to trigger EntityMetadataValidator.validate
         // that previously threw the error where the database on the driver object was undefined
-        const newConnection = await createConnection({
-            url: "mysql://test:test@localhost:3306/test",
+        const connection = new Connection({
+            name: "#2096",
+            url: "mysql://root:admin@localhost:3306/test",
             entities: [__dirname + "/entity/*{.js,.ts}"],
             synchronize: true,
             type: "mysql"
         });
-        expect(newConnection.isConnected).to.eq(true);
+        expect(connection.isConnected).to.eq(true);
+        await connection.close();
     });
 });
