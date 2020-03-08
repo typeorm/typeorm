d2b687021710c7256af53b55cfecdef0d502acd8
julius <julius.friedrich@shift-f3.com>
2020-03-08 01:42:10 +0100

    create connection by lib function

--- test/github-issues/2096/issue-2096.ts
+++ test/github-issues/2096/issue-2096.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import {expect} from "chai";
-import {Connection} from "../../../src";
+import {createConnection} from "../../../src";
 
 describe("github issues > #2096 [mysql] Database name isn't read from url", () => {
     it("should be possible to define a database by connection url for mysql", async () => {
         // it is important to synchronize here, to trigger EntityMetadataValidator.validate
         // that previously threw the error where the database on the driver object was undefined
-        const connection = new Connection({
+        const connection = await createConnection({
             name: "#2096",
             url: "mysql://root:admin@localhost:3306/test",
             entities: [__dirname + "/entity/*{.js,.ts}"],
