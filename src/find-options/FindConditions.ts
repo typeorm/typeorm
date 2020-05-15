d561899445972828dd3471e1cc83d163d8d048da
Bogdan Gusiev <agresso@gmail.com>
2020-05-15 16:46:30 +0300

    Fix BaseEntity finder methods to properly typecheck  lazy relations conditions

--- src/find-options/FindConditions.ts
+++ src/find-options/FindConditions.ts
@@ -3,6 +3,9 @@ import {FindOperator} from "./FindOperator";
 /**
  * Used for find operations.
  */
-export type FindConditions<T> = {
-    [P in keyof T]?: T[P] extends never ? FindConditions<T[P]>|FindOperator<FindConditions<T[P]>> : FindConditions<T[P]>|FindOperator<FindConditions<T[P]>>;
+type FindCondition<T> = FindConditions<T> | FindOperator<FindConditions<T>>;
+export type FindConditions<T> = T | {
+    [P in keyof T]?: T[P] extends Promise<infer U>
+        ? FindCondition<U>
+        : FindCondition<T[P]>;
 };
