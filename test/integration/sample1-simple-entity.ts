0d0f163b68add33fa50820efe842f60a6b2d0d31
pleerock <pleerock.me@gmail.com>
2020-05-18 16:07:11 +0300

    enabling hana

--- test/integration/sample1-simple-entity.ts
+++ test/integration/sample1-simple-entity.ts
@@ -4,7 +4,7 @@ import {Connection} from "../../src/connection/Connection";
 import {Post} from "../../sample/sample1-simple-entity/entity/Post";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../utils/test-utils";
 
-describe("insertion", function() {
+describe.only("insertion", function() {
 
     // -------------------------------------------------------------------------
     // Setup
