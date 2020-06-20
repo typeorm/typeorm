5f0be7eca60a643c78143cbf72ad5e5fac1554c0
Dan Pickett <dan.pickett@launchacademy.com>
2020-06-20 12:22:46 -0400

    fix linting errors

--- test/github-issues/6284/issue-6284.ts
+++ test/github-issues/6284/issue-6284.ts
@@ -6,8 +6,8 @@ describe("cli support for cjs extension", () => {
     it("will load a cjs file", async  () => {
         const databaseType = "postgres";
         const config = `module.exports = {"type": "${databaseType}"};`;
-        const cjsConfigPath = [__dirname, "ormconfig.cjs"].join("/")
-        writeFileSync(cjsConfigPath, config)
+        const cjsConfigPath = [__dirname, "ormconfig.cjs"].join("/");
+        writeFileSync(cjsConfigPath, config);
 
         const reader = new ConnectionOptionsReader({root: __dirname });
 
