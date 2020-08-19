34cc5a65cd3f33d22f47e1dab9c8e6db5c7fadf8
Murat Gundes <guendes.murat@indivumed.com>
2020-08-19 15:46:46 +0200

    fix: ensure that this changes applies just for Oracle Driver
    Closes: #5067

--- test/github-issues/5067/issue-5067.ts
+++ test/github-issues/5067/issue-5067.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import {expect} from "chai";
 import {Connection} from "../../../src";
 import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
+import {OracleDriver} from "../../../src/driver/oracle/OracleDriver";
 
 describe.only("github issues > #5067 ORA-00972: identifier is too long", () => {
     let connections: Connection[];
@@ -11,14 +12,17 @@ describe.only("github issues > #5067 ORA-00972: identifier is too long", () => {
     after(() => closeTestingConnections(connections));
 
     it("generated parameter name returns index instead of parameter name", () => Promise.all(connections.map(async connection => {
-        const paramName = "output_";
-        const parametersCount = 0;
-        const createdParameter = await connection.driver.createParameter(paramName, parametersCount);
+        if (connection.driver instanceof OracleDriver)
+        {
+            const paramName = "output_";
+            const parametersCount = 0;
+            const createdParameter = await connection.driver.createParameter(paramName, parametersCount);
 
-        expect(createdParameter).to.be.not.undefined;
-        expect(createdParameter).to.be.not.null;
-        expect(createdParameter).to.be.an("String");
-        expect(createdParameter).to.not.match(/(?:output_)/);
-        expect(await connection.driver.createParameter(paramName, 2)).to.equal(":" + 3);
+            expect(createdParameter).to.be.not.undefined;
+            expect(createdParameter).to.be.not.null;
+            expect(createdParameter).to.be.an("String");
+            expect(createdParameter).to.not.match(/(?:output_)/);
+            expect(await connection.driver.createParameter(paramName, 2)).to.equal(":" + 3);
+        }
     })));
 });
