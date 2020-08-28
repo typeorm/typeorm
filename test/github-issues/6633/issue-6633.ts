476209e169edf8918a603a0035cf3ee54bd81526
Michael Bromley <michael@michaelbromley.co.uk>
2020-08-28 21:47:41 +0200

    test: Enable all tests

--- test/github-issues/6633/issue-6633.ts
+++ test/github-issues/6633/issue-6633.ts
@@ -4,7 +4,7 @@ import { Connection } from "../../../src";
 import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
 import { Post } from "../4440/entity/Post";
 
-describe.only("github issues > #6633 Fulltext indices continually dropped & re-created", () => {
+describe("github issues > #6633 Fulltext indices continually dropped & re-created", () => {
 
     let connections: Connection[];
     before(async () => {
