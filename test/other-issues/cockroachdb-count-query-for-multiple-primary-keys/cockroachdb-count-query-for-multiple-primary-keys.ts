4e31ba04ba6f79157f24a333723aa37d811fc7b1
temnov98 <temnov_aleksei@mail.ru>
2020-09-19 15:51:52 +0300

    fix: count() method for multiple primary keys for cockroachdb
    Cockroachdb does not support concat() for different types at the moment.
    To fix this problem, each primary key is cast to the text type.

---
+++ test/other-issues/cockroachdb-count-query-for-multiple-primary-keys/cockroachdb-count-query-for-multiple-primary-keys.ts
@@ -0,0 +1,23 @@
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {expect} from 'chai';
+import {Test} from './entity/Test';
+
+describe("other issues > cockroachdb count query for multiple primary keys", () => {
+
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [Test],
+        schemaCreate: true,
+        dropSchema: true,
+        enabledDrivers: ["cockroachdb"]
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("Count query should be completed successfully", () => Promise.all(connections.map(async connection => {
+        const count = await connection.getRepository(Test).count();
+        expect(count).to.be.equal(0);
+    })));
+
+});
