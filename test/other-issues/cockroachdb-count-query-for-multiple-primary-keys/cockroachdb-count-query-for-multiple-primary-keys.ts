758d858fb1658c303c601e12a6d7af2b0cb42b71
temnov98 <temnov_aleksei@mail.ru>
2020-09-19 16:45:51 +0300

    fix: add doublequote

--- test/other-issues/cockroachdb-count-query-for-multiple-primary-keys/cockroachdb-count-query-for-multiple-primary-keys.ts
+++ test/other-issues/cockroachdb-count-query-for-multiple-primary-keys/cockroachdb-count-query-for-multiple-primary-keys.ts
@@ -1,7 +1,7 @@
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
 import {Connection} from "../../../src/connection/Connection";
-import {expect} from 'chai';
-import {Test} from './entity/Test';
+import {expect} from "chai";
+import {Test} from "./entity/Test";
 
 describe("other issues > cockroachdb count query for multiple primary keys", () => {
 
