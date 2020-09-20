e3df5d9dd398473020c37574dab9d8bd54927f1c
temnov98 <temnov_aleksei@mail.ru>
2020-09-21 01:53:00 +0300

    test: update and move tests for count() method for multiple primary keys

--- test/functional/query-builder/count/query-builder-count.ts
+++ test/functional/query-builder/count/query-builder-count.ts
@@ -1,16 +1,15 @@
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
+import {Connection} from "../../../../src/connection/Connection";
 import {expect} from "chai";
 import {Test} from "./entity/Test";
 
-describe("other issues > cockroachdb count query for multiple primary keys", () => {
+describe("query builder > count", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
         entities: [Test],
         schemaCreate: true,
         dropSchema: true,
-        enabledDrivers: ["cockroachdb"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
