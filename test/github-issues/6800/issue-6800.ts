c42fd8c4d4402c4765b27b22ff0c433cb12c0444
ronits-cx <ronit.steinberg@checkmarx.com>
2020-10-01 09:13:58 +0300

    change test

--- test/github-issues/6800/issue-6800.ts
+++ test/github-issues/6800/issue-6800.ts
@@ -1,26 +1,88 @@
 import "reflect-metadata";
-import {expect} from "chai";
-import {Connection} from "../../../src";
-import {User} from "./entity/User";
-import {Role} from "./entity/Role";
-import {createTestingConnections, reloadTestingDatabases, closeTestingConnections} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Table} from "../../../src/schema-builder/table/Table";
+import { QueryRunner } from "../../../src";
 
-describe("github issues > #6800 fix performance and wrong foreign key in mysql", () => {
+const createDB = async (queryRunner: QueryRunner, dbName: string) => {
+    await queryRunner.createDatabase(dbName, true);
+    const questionTableName = `${dbName}.question`;
+    const categoryTableName = `${dbName}.category`;
+
+    await queryRunner.createTable(new Table({
+        name: questionTableName,
+        columns: [
+            {
+                name: "id",
+                type: "int",
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: "increment"
+            },
+            {
+                name: "name",
+                type: "varchar",
+            }
+        ],
+        indices: [
+            {
+                columnNames: ["name"],
+                name: "IDX_QUESTION_NAME"
+            }
+        ]
+    }), true);
+
+    await queryRunner.createTable(new Table({
+        name: categoryTableName,
+        columns: [
+            {
+                name: "id",
+                type: "int",
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: "increment"
+            },
+            {
+                name: "questionId",
+                type: "int",
+                isUnique: true
+            }
+        ],
+        foreignKeys: [
+            {
+                columnNames: ["questionId"],
+                referencedTableName: questionTableName,
+                referencedColumnNames: ["id"]
+            }
+        ]
+    }), true);
+};
+
+describe("github issues > #6800 fix performance and wrong foreign key in mysql multi-tenanted DB", () => {
+
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
-                entities: [User, Role],
-                enabledDrivers: ["mysql", "mariadb"],
-                dropSchema: true,
-            })
-    );
+    before(async () => {
+        connections = await createTestingConnections({
+            entities: [__dirname + "/entity/*{.js,.ts}"],
+            enabledDrivers: ["mysql"],
+            schemaCreate: true,
+            dropSchema: true,
+        });
+    });
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
 
-    it("should have equal foreign keys from schema and entities", () => Promise.all(connections.map(async connection => {
-        console.log("heeeeere");
-        const queryRunner = connection.createQueryRunner("master");
-        const metadataFromSchema = await queryRunner.connection.getMetadata(User);
-        const metadataFromEntities = connection.getMetadata(User);
-        return expect(metadataFromSchema).to.be.deep.equal(metadataFromEntities);
+    it("should correctly load foreign keys and indices", () => Promise.all(connections.map(async connection => {
+
+        console.log("here");
+        const queryRunner = connection.createQueryRunner();
+        await createDB(queryRunner, "test1");
+        await createDB(queryRunner, "test2");
+
+        await queryRunner.release();
+
+        await queryRunner.dropDatabase("test1");
+        await queryRunner.dropDatabase("test2");
     })));
+
 });
