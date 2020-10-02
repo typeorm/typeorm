af7d7fab52c99fc80bd32a90e656dfcfad1079b6
ronits-cx <ronit.steinberg@checkmarx.com>
2020-10-02 11:35:14 +0300

    update test to only include indices

--- test/github-issues/6800/issue-6800.ts
+++ test/github-issues/6800/issue-6800.ts
@@ -48,22 +48,20 @@ const createDB = async (queryRunner: QueryRunner, dbName: string) => {
                 generationStrategy: "increment"
             },
             {
-                name: "questionId",
+                name: "name",
                 type: "int",
             }
         ],
-        foreignKeys: [
-            {
-                columnNames: ["questionId"],
-                referencedTableName: questionTableName,
-                referencedColumnNames: ["id"],
-                name: "FK_CATEGORY_QUESTION"
-            }
-        ]
+        indices: [
+          {
+              columnNames: ["name"],
+              name: "IDX_CATEGORY_NAME"
+          }
+      ]
     }), true);
 };
 
-describe("github issues > #6800 fix performance and wrong foreign key in mysql multi-tenanted DB", () => {
+describe("github issues > #6800 fix indices performance in mysql multi-tenanted DB", () => {
 
     let connections: Connection[];
     let testConnections = [] as Connection[];
@@ -99,7 +97,7 @@ describe("github issues > #6800 fix performance and wrong foreign key in mysql m
         await closeTestingConnections(testConnections);
     });
 
-    it("should correctly load foreign keys and indices", () => Promise.all(connections.map(async connection => {
+    it("should load indices correctly just as it did before the change, the performance improvement shouldn't affect the result", () => Promise.all(connections.map(async connection => {
         const options = connection.options as MysqlConnectionOptions;
 
         const connectionTest1 = await createConnection({ ...options, name: "test1", database: "test1" });
@@ -108,13 +106,11 @@ describe("github issues > #6800 fix performance and wrong foreign key in mysql m
         testQueryRunners.push(queryRunnerTest1);
         const [questionTable1, categoryTable1] = await queryRunnerTest1.getTables([questionName, categoryName]);
 
-        expect(questionTable1.foreignKeys.length).to.eq(1);
-        expect(questionTable1.foreignKeys[0].name).to.eq("FK_CATEGORY_QUESTION");
-        expect(questionTable1.foreignKeys[0].columnNames.length).to.eq(1);  // before the fix this was 2, one for each schema
-        expect(questionTable1.foreignKeys[0].columnNames[0]).to.eq("questionId");
-        expect(questionTable1.indices.length).to.eq(0);
+        expect(questionTable1.indices.length).to.eq(1);
+        expect(questionTable1.indices[0].name).to.eq("IDX_CATEGORY_NAME");
+        expect(questionTable1.indices[0].columnNames.length).to.eq(1);
+        expect(questionTable1.indices[0].columnNames[0]).to.eq("name");
 
-        expect(categoryTable1.foreignKeys.length).to.eq(0);
         expect(categoryTable1.indices.length).to.eq(1);
         expect(categoryTable1.indices[0].name).to.eq("IDX_QUESTION_NAME");
         expect(categoryTable1.indices[0].columnNames.length).to.eq(1);
