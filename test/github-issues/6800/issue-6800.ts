30ac834d908ce709e618a95948f117479eb6ff02
ronits-cx <ronit.steinberg@checkmarx.com>
2020-10-04 08:55:27 +0300

    code review comments

--- test/github-issues/6800/issue-6800.ts
+++ test/github-issues/6800/issue-6800.ts
@@ -1,16 +1,14 @@
 import "reflect-metadata";
 import {Connection} from "../../../src/connection/Connection";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
 import {Table} from "../../../src/schema-builder/table/Table";
-import { QueryRunner, createConnection, TableIndex } from "../../../src";
-import { MysqlConnectionOptions } from "../../../src/driver/mysql/MysqlConnectionOptions";
+import { QueryRunner, TableIndex } from "../../../src";
 import { expect } from "chai";
 
 const questionName = "question";
 const categoryName = "category";
 
-const createDB = async (queryRunner: QueryRunner, dbName: string) => {
-    await queryRunner.createDatabase(dbName, true);
+const createTables = async (queryRunner: QueryRunner, dbName: string) => {
     const questionTableName = `${dbName}.${questionName}`;
     const categoryTableName = `${dbName}.${categoryName}`;
 
@@ -84,8 +82,6 @@ const createDB = async (queryRunner: QueryRunner, dbName: string) => {
 describe("github issues > #6800 fix foreign key and indices query performance in mysql multi-tenanted DB", () => {
 
     let connections: Connection[];
-    let testConnections = [] as Connection[];
-    let testQueryRunners = [] as QueryRunner[];
     before(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
@@ -94,62 +90,55 @@ describe("github issues > #6800 fix foreign key and indices query performance in
             dropSchema: false,
         });
 
-        if (connections.length > 0) {
-            const queryRunner = connections[0].createQueryRunner();
-            await createDB(queryRunner, "test1");
-            await createDB(queryRunner, "test2");
+        for (const connection of connections) {
+            const queryRunner = connection.createQueryRunner();
+            await createTables(queryRunner, String(connection.driver.database));
+            await queryRunner.createDatabase("test2", true);
+            await createTables(queryRunner, "test2");
             await queryRunner.release();
         };
     });
 
-    beforeEach(() => reloadTestingDatabases(connections));
-
     after(async () => {
-        if (connections.length > 0) {
-            const queryRunner = connections[0].createQueryRunner();
-            await queryRunner.dropDatabase("test1");
+        for (const connection of connections) {
+            const queryRunner = connection.createQueryRunner();
             await queryRunner.dropDatabase("test2");
             await queryRunner.release();
         };
 
         await closeTestingConnections(connections);
-        await Promise.all(testQueryRunners.map(queryRunner => queryRunner.release()));
-        await closeTestingConnections(testConnections);
     });
 
     it("should load foreign keys and indices correctly just as it did before the change, the performance improvement shouldn't affect the result",
       () => Promise.all(connections.map(async connection => {
-        const options = connection.options as MysqlConnectionOptions;
+        const queryRunner = connection.createQueryRunner();
+        const tables = await queryRunner.getTables([questionName, categoryName]);
 
-        const connectionTest1 = await createConnection({ ...options, name: "test1", database: "test1" });
-        testConnections.push(connectionTest1);
-        const queryRunnerTest1 = connectionTest1.createQueryRunner();
-        testQueryRunners.push(queryRunnerTest1);
-        const tables = await queryRunnerTest1.getTables([questionName, categoryName]);
+        const questionTable = tables.find(table => table.name === questionName) as Table;
+        const categoryTable = tables.find(table => table.name === categoryName) as Table;
 
-        const questionTable1 = tables.find(table => table.name === questionName) as Table;
-        const categoryTable1 = tables.find(table => table.name === categoryName) as Table;
+        queryRunner.release();
 
-        expect(questionTable1.indices.length).to.eq(2);
+        expect(questionTable.indices.length).to.eq(2);
 
-        const questionNameIndex = questionTable1.indices.find(index => index.name === "IDX_QUESTION_NAME") as TableIndex;
-        const questionTypeIndex = questionTable1.indices.find(index => index.name === "IDX_QUESTION_TYPE") as TableIndex;
+        const questionNameIndex = questionTable.indices.find(index => index.name === "IDX_QUESTION_NAME") as TableIndex;
+        const questionTypeIndex = questionTable.indices.find(index => index.name === "IDX_QUESTION_TYPE") as TableIndex;
 
         expect(questionNameIndex.columnNames.length).to.eq(1);
         expect(questionNameIndex.columnNames[0]).to.eq("name");
         expect(questionTypeIndex.columnNames.length).to.eq(1);
         expect(questionTypeIndex.columnNames[0]).to.eq("type");
-        
-        expect(questionTable1.foreignKeys.length).to.eq(0);
 
-        expect(categoryTable1.indices.length).to.eq(1);
-        expect(categoryTable1.indices[0].name).to.eq("IDX_CATEGORY_NAME");
-        expect(categoryTable1.indices[0].columnNames.length).to.eq(1);
-        expect(categoryTable1.indices[0].columnNames[0]).to.eq("name");
+        expect(questionTable.foreignKeys.length).to.eq(0);
 
-        expect(categoryTable1.foreignKeys.length).to.eq(1);
-        expect(categoryTable1.foreignKeys[0].name).to.eq("FK_CATEGORY_QUESTION");
-        expect(categoryTable1.foreignKeys[0].columnNames.length).to.eq(1);
-        expect(categoryTable1.foreignKeys[0].columnNames[0]).to.eq("questionId");
+        expect(categoryTable.indices.length).to.eq(1);
+        expect(categoryTable.indices[0].name).to.eq("IDX_CATEGORY_NAME");
+        expect(categoryTable.indices[0].columnNames.length).to.eq(1);
+        expect(categoryTable.indices[0].columnNames[0]).to.eq("name");
+
+        expect(categoryTable.foreignKeys.length).to.eq(1);
+        expect(categoryTable.foreignKeys[0].name).to.eq("FK_CATEGORY_QUESTION");
+        expect(categoryTable.foreignKeys[0].columnNames.length).to.eq(1);
+        expect(categoryTable.foreignKeys[0].columnNames[0]).to.eq("questionId");
     })));
 });
