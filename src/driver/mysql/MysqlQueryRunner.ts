16514c5d8bfea10f4d959a4f4c9a0dbbf0426e1b
Maczuga <544349+Maczuga@users.noreply.github.com>
2020-11-05 17:46:14 +0100

    Fix MariaDB VIRTUAL [NOT NULL|NULL] error
    Fixes #2691
    
    query failed: ALTER TABLE `customer` CHANGE `fullName` `fullName` varchar(255) AS (CONCAT(`firstName`, ' ', `lastName`)) VIRTUAL NULL
    error: Error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'NULL' at line 1

--- src/driver/mysql/MysqlQueryRunner.ts
+++ src/driver/mysql/MysqlQueryRunner.ts
@@ -1857,10 +1857,16 @@ export class MysqlQueryRunner extends BaseQueryRunner implements QueryRunner {
             c += ` CHARACTER SET "${column.charset}"`;
         if (column.collation)
             c += ` COLLATE "${column.collation}"`;
-        if (!column.isNullable)
-            c += " NOT NULL";
-        if (column.isNullable)
-            c += " NULL";
+
+        const isMariaDb = this.driver.options.type === "mariadb";
+        // MariaDB does not support NULL/NOT NULL expressions for VIRTUAL columns
+        if (!(isMariaDb && column.asExpression && (column.generatedType || "VIRTUAL") === "VIRTUAL")) {
+            if (!column.isNullable)
+                c += " NOT NULL";
+            if (column.isNullable)
+                c += " NULL";
+        }
+
         if (column.isPrimary && !skipPrimary)
             c += " PRIMARY KEY";
         if (column.isGenerated && column.generationStrategy === "increment") // don't use skipPrimary here since updates can update already exist primary without auto inc.
