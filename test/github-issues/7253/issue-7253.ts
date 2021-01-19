49ae07f05f533624f4dadfc8a4c33f0b020668d0
Jorge Vargas <Jorge.Vargas@albelli.com>
2021-01-19 10:25:38 +0100

    docs / test: Added tests and documentation for Feature 7253 - Migrations Javascript output

---
+++ test/github-issues/7253/issue-7253.ts
@@ -0,0 +1,140 @@
+import sinon from "sinon";
+import { ConnectionOptions, ConnectionOptionsReader, DatabaseType } from "../../../src";
+import { setupTestingConnections, createTestingConnections, closeTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
+import { CommandUtils } from "../../../src/commands/CommandUtils";
+import { MigrationGenerateCommand } from "../../../src/commands/MigrationGenerateCommand";
+import { MigrationCreateCommand } from "../../../src/commands/MigrationCreateCommand";
+import { Post } from "./entity/Post";
+import { resultsTemplates } from "./results-templates";
+
+describe("github issues > #7253 Allow migration files to be output in Javascript", () => {
+    let connectionOptions: ConnectionOptions[];
+    let createFileStub: sinon.SinonStub;
+    let timerStub: sinon.SinonFakeTimers;
+    let getConnectionOptionsStub: sinon.SinonStub;
+    let migrationGenerateCommand: MigrationGenerateCommand;
+    let migrationCreateCommand: MigrationCreateCommand;
+    let connectionOptionsReader: ConnectionOptionsReader;
+    let baseConnectionOptions: ConnectionOptions;
+
+    const enabledDrivers = [
+        "mysql",
+    ] as DatabaseType[];
+
+    // simulate args: `npm run typeorm migration:run -- -n test-migration -d test-directory`
+    const testHandlerArgs = (options: Record<string, any>) => ({
+        "$0": "test",
+        "_": ["test"],
+        "name": "test-migration",
+        "dir": "test-directory",
+        ...options
+    });
+
+    before(async () => {
+        // clean out db from any prior tests in case previous state impacts the generated migrations
+        const connections = await createTestingConnections({
+            entities: [],
+            enabledDrivers
+        });
+        await reloadTestingDatabases(connections);
+        await closeTestingConnections(connections);
+
+        connectionOptions = setupTestingConnections({
+            entities: [Post],
+            enabledDrivers
+        });
+        connectionOptionsReader = new ConnectionOptionsReader();
+
+        migrationGenerateCommand = new MigrationGenerateCommand();
+        migrationCreateCommand = new MigrationCreateCommand();
+        createFileStub = sinon.stub(CommandUtils, "createFile");
+
+        timerStub = sinon.useFakeTimers(1610975184784);
+    });
+
+    after(async () => {
+        timerStub.restore();
+        createFileStub.restore();
+    });
+
+    beforeEach(async () => {
+        baseConnectionOptions = await connectionOptionsReader.get(connectionOptions[0].name as string);
+        getConnectionOptionsStub = sinon.stub(ConnectionOptionsReader.prototype, "get").resolves({
+            ...baseConnectionOptions,
+            entities: [Post]
+        });
+    });
+
+    afterEach(async () => {
+        getConnectionOptionsStub.restore();
+    });
+
+    it("writes regular empty migration file when no option is passed", async () => {
+        createFileStub.resetHistory();
+
+        await migrationCreateCommand.handler(testHandlerArgs({
+            "connection": connectionOptions[0].name
+        }));
+
+        // compare against control test strings in results-templates.ts
+        sinon.assert.calledWith(
+            createFileStub,
+            sinon.match(/test-directory.*test-migration.ts/),
+            sinon.match(resultsTemplates.create.control)
+        );
+
+        getConnectionOptionsStub.restore();
+    });
+
+    it("writes Javascript empty migration file when option is passed", async () => {
+        createFileStub.resetHistory();
+
+        await migrationCreateCommand.handler(testHandlerArgs({
+            "connection": connectionOptions[0].name,
+            "outputJs": true
+        }));
+
+        // compare against control test strings in results-templates.ts
+        sinon.assert.calledWith(
+            createFileStub,
+            sinon.match(/test-directory.*test-migration.js/),
+            sinon.match(resultsTemplates.create.javascript)
+        );
+
+        getConnectionOptionsStub.restore();
+    });
+
+    it("writes regular migration file when no option is passed", async () => {
+        createFileStub.resetHistory();
+
+        await migrationGenerateCommand.handler(testHandlerArgs({
+            "connection": connectionOptions[0].name
+        }));
+
+        // compare against control test strings in results-templates.ts
+        sinon.assert.calledWith(
+            createFileStub,
+            sinon.match(/test-directory.*test-migration.ts/),
+            sinon.match(resultsTemplates.generate.control)
+        );
+
+        getConnectionOptionsStub.restore();
+    });
+
+    it("writes Javascript printed file when option is passed", async () => {
+        createFileStub.resetHistory();
+
+        await migrationGenerateCommand.handler(testHandlerArgs({
+            "connection": connectionOptions[0].name,
+            "outputJs": true
+        }));
+        
+        // compare against "pretty" test strings in results-templates.ts
+        sinon.assert.calledWith(
+            createFileStub,
+            sinon.match(/test-directory.*test-migration.js/),
+            sinon.match(resultsTemplates.generate.javascript)
+        );
+        getConnectionOptionsStub.restore();
+    });
+});
