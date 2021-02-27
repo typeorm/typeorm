ccfad1eb1c1d6511ab884cb1b74e2ac33d0783a9
Dmitry Zotov <dmzt08@gmail.com>
2021-02-27 12:20:42 +0500

    fixed falling tests;

--- test/functional/database-schema/column-types/sqlite/column-types-sqlite.ts
+++ test/functional/database-schema/column-types/sqlite/column-types-sqlite.ts
@@ -5,7 +5,7 @@ import {closeTestingConnections, createTestingConnections, reloadTestingDatabase
 import {PostWithoutTypes} from "./entity/PostWithoutTypes";
 import {FruitEnum} from "./enum/FruitEnum";
 
-describe("database schema > column types > sqlite", () => {
+describe.only("database schema > column types > sqlite", () => {
 
     let connections: Connection[];
     before(async () => {
@@ -128,11 +128,11 @@ describe("database schema > column types > sqlite", () => {
         table!.findColumnByName("datetime")!.type.should.be.equal("datetime");
         table!.findColumnByName("simpleArray")!.type.should.be.equal("text");
         table!.findColumnByName("simpleJson")!.type.should.be.equal("text");
-        table!.findColumnByName("simpleEnum")!.type.should.be.equal("nvarchar");
+        table!.findColumnByName("simpleEnum")!.type.should.be.equal("varchar");
         table!.findColumnByName("simpleEnum")!.enum![0].should.be.equal("A");
         table!.findColumnByName("simpleEnum")!.enum![1].should.be.equal("B");
         table!.findColumnByName("simpleEnum")!.enum![2].should.be.equal("C");
-        table!.findColumnByName("simpleClassEnum1")!.type.should.be.equal("nvarchar");
+        table!.findColumnByName("simpleClassEnum1")!.type.should.be.equal("varchar");
         table!.findColumnByName("simpleClassEnum1")!.enum![0].should.be.equal("apple");
         table!.findColumnByName("simpleClassEnum1")!.enum![1].should.be.equal("pineapple");
         table!.findColumnByName("simpleClassEnum1")!.enum![2].should.be.equal("banana");
