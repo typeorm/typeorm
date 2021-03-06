14982228fab2404b585c8c3b44861aabcb5b0332
Filip Wachowiak <filipw01@interia.eu>
2021-03-06 14:22:40 +0100

    fix: empty entity when query with nested relations
    Closes: #7041

--- src/query-builder/transformer/RawSqlResultsToEntityTransformer.ts
+++ src/query-builder/transformer/RawSqlResultsToEntityTransformer.ts
@@ -187,7 +187,10 @@ export class RawSqlResultsToEntityTransformer {
             // transform joined data into entities
             let result: any = this.transform(rawResults, join.alias);
             result = !join.isMany ? result[0] : result;
-            result = !join.isMany && result === undefined ? null : result; // this is needed to make relations to return null when its joined but nothing was found in the database
+            // this is needed to make relations to return null when its joined but nothing was found in the database
+            result = !join.isMany && (result === undefined || Object.values(result).every((value) => value === null)) ? null : result;
+            // this is needed to make many relations to return [] when nothing found on nested joins
+            result = join.isMany && result.length === 1 && Object.values(result[0]).every((value) => value === null) ? [] : result;
             if (result === undefined) // if nothing was joined then simply return
                 return;
 
