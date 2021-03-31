2e77086c1076e9b4d6d6e128e07907b39ce6b4d7
Dmitry Zotov <dmzt08@gmail.com>
2021-03-31 16:56:22 +0500

    improved #3991 test for Sqlite;

--- test/github-issues/3991/entity/sqlite/Post.ts
+++ test/github-issues/3991/entity/sqlite/Post.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "../../../../../src";
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "../../../../../src";
 
 @Entity()
 export class Post {
@@ -18,6 +18,38 @@ export class Post {
     @Column({ precision: 3,  default: () => "CURRENT_TIMESTAMP" })
     col4: Date;
 
-    @Column({ precision: null, default: () => "NOW()" })
+    @Column({ default: () => "NOW()" })
     col5: Date;
+
+
+    @CreateDateColumn({ default: () => "CURRENT_DATE" })
+    col6: Date;
+
+    @CreateDateColumn({ default: () => "CURRENT_TIME" })
+    col7: Date;
+
+    @CreateDateColumn({ default: () => "CURRENT_TIMESTAMP" })
+    col8: Date;
+
+    @CreateDateColumn({ precision: 3,  default: () => "CURRENT_TIMESTAMP" })
+    col9: Date;
+
+    @CreateDateColumn({ default: () => "NOW()" })
+    col10: Date;
+
+
+    @UpdateDateColumn({ default: () => "CURRENT_DATE" })
+    col11: Date;
+
+    @UpdateDateColumn({ default: () => "CURRENT_TIME" })
+    col12: Date;
+
+    @UpdateDateColumn({ default: () => "CURRENT_TIMESTAMP" })
+    col13: Date;
+
+    @UpdateDateColumn({ precision: 3,  default: () => "CURRENT_TIMESTAMP" })
+    col14: Date;
+
+    @UpdateDateColumn({ default: () => "NOW()" })
+    col15: Date;
 }
