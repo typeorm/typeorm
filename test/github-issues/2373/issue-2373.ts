fa3cd7d0d17bb336c7ba28f33e1c17612ca1a4a2
James Ward <james@notjam.es>
2021-07-09 00:54:58 -0400

    fix the test case

--- test/github-issues/2373/issue-2373.ts
+++ test/github-issues/2373/issue-2373.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata";
 import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
-import { getConnection } from "../../../src";
+import { expect } from "chai";
+import { Connection } from "../../../src";
 import { Project } from "./entity/Project";
 import { ProjectSettings } from "./entity/ProjectSettings";
 
-describe("github issues > #2373 Deletion of a cascaded entity fails because insert statement is created for cascaded entity", () => {
+describe.only("github issues > #2373 Deletion of a cascaded entity fails because insert statement is created for cascaded entity", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
-        entities: [__dirname + "/entity/*{.js,.ts}"],
+        entities: [Project, ProjectSettings],
         schemaCreate: true,
         dropSchema: true,
     }));
@@ -18,19 +18,17 @@ describe("github issues > #2373 Deletion of a cascaded entity fails because inse
 
     it("should delete entity and cascaded entity", () => Promise.all(connections.map(async connection => {
 
-       let projectRepo = getConnection("sqlite").getRepository(Project);
-       let settingsRepo = getConnection("sqlite").getRepository(ProjectSettings);
+        let projectRepo = connection.getRepository(Project);
+        let settingsRepo = connection.getRepository(ProjectSettings);
 
-       let project = new Project();
-       let projectSettings = new ProjectSettings();
+        let project = new Project();
 
-       project.settings = projectSettings;
-       project = await projectRepo.save(project);
+        project.settings = new ProjectSettings();;
+        project = await projectRepo.save(project);
 
-       projectRepo.remove(project);
+        await projectRepo.remove(project);
 
-       projectRepo.count().should.be.eql(0);
-       settingsRepo.count().should.be.eql(0);
-
+        expect(await projectRepo.count()).to.be.eql(0);
+        expect(await settingsRepo.count()).to.be.eql(0);
     })));
 });
