cfba7fed28eb8d20ae39dcf94fda09a3cea11c31
kumar.k <kumar.k@talview.com>
2021-07-16 19:34:10 +0530

    Removed postgres driver to run the test cases for all drivers

--- test/github-issues/7849/issue-7849.ts
+++ test/github-issues/7849/issue-7849.ts
@@ -10,7 +10,6 @@ describe("github issues > #7849 Handle query builder to accomodate zero limit an
     
     before(async () => {
         connections = await createTestingConnections({
-            enabledDrivers: ["postgres"],
             entities: [TestEntity],
             schemaCreate: true,
             dropSchema: true
