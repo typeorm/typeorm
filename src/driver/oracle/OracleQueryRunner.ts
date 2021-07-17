43c06903705380e0afc40ce24e9c2ea445b421b4
James Ward <james@notjam.es>
2021-07-17 11:48:41 -0400

    missed a spot with oracle

--- src/driver/oracle/OracleQueryRunner.ts
+++ src/driver/oracle/OracleQueryRunner.ts
@@ -1327,7 +1327,12 @@ export class OracleQueryRunner extends BaseQueryRunner implements QueryRunner {
 
             // create columns from the loaded columns
             table.columns = dbColumns
-                .filter(dbColumn => dbColumn["TABLE_NAME"] === table.name)
+                .filter(
+                    dbColumn => (
+                        dbColumn["OWNER"] === dbTable["OWNER"] &&
+                        dbColumn["TABLE_NAME"] === dbTable["TABLE_NAME"]
+                    )
+                )
                 .map(dbColumn => {
                     const columnConstraints = dbConstraints.filter(
                         dbConstraint => (
@@ -1418,7 +1423,13 @@ export class OracleQueryRunner extends BaseQueryRunner implements QueryRunner {
             }), dbConstraint => dbConstraint["CONSTRAINT_NAME"]);
 
             table.checks = tableCheckConstraints.map(constraint => {
-                const checks = dbConstraints.filter(dbC => dbC["CONSTRAINT_NAME"] === constraint["CONSTRAINT_NAME"]);
+                const checks = dbConstraints.filter(
+                    dbC => (
+                        dbC["TABLE_NAME"] === constraint["TABLE_NAME"] &&
+                        dbC["OWNER"] === constraint["OWNER"] &&
+                        dbC["CONSTRAINT_NAME"] === constraint["CONSTRAINT_NAME"]
+                    )
+                );
                 return new TableCheck({
                     name: constraint["CONSTRAINT_NAME"],
                     columnNames: checks.map(c => c["COLUMN_NAME"]),
@@ -1427,9 +1438,10 @@ export class OracleQueryRunner extends BaseQueryRunner implements QueryRunner {
             });
 
             // find foreign key constraints of table, group them by constraint name and build TableForeignKey.
-            const tableForeignKeyConstraints = OrmUtils.uniq(dbForeignKeys.filter(dbForeignKey => {
-                return dbForeignKey["TABLE_NAME"] === table.name;
-            }), dbForeignKey => dbForeignKey["CONSTRAINT_NAME"]);
+            const tableForeignKeyConstraints = OrmUtils.uniq(dbForeignKeys.filter(dbForeignKey => (
+                dbForeignKey["OWNER"] === dbTable["OWNER"] &&
+                dbForeignKey["TABLE_NAME"] === dbTable["TABLE_NAME"]
+            )), dbForeignKey => dbForeignKey["CONSTRAINT_NAME"]);
 
             table.foreignKeys = tableForeignKeyConstraints.map(dbForeignKey => {
                 const foreignKeys = dbForeignKeys.filter(dbFk => (
