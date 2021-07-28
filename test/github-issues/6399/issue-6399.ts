03d23c6acb689dfdf402325e1e4b197ed20f9c92
James Ward <james@notjam.es>
2021-07-28 09:39:44 -0400

    test: handle native classes not hoisting

--- test/github-issues/6399/issue-6399.ts
+++ test/github-issues/6399/issue-6399.ts
@@ -1,7 +1,8 @@
 import {Connection} from "../../../src";
 import {expect} from "chai";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Post, TargetPost, Comment} from "./entities";
+import { Post, TargetPost } from "./entities/Post";
+import { Comment } from "./entities/Comment";
 
 describe("github issues > #6399 Process extraAppendedAndWhereCondition for inherited entity", () => {
     let connections: Connection[];
