9357058911cece8e54e44263e90d6c8817d60cfd
Lukas Windisch <52009814+TheProgrammer21@users.noreply.github.com>
2021-07-30 01:06:46 +0200

    style: remove unused declaration
    remove unused declaration to satisfy linting.

--- src/find-options/FindTreeOptions.ts
+++ src/find-options/FindTreeOptions.ts
@@ -1,7 +1,7 @@
 /**
  * Defines a special criteria to find specific entities.
  */
-export interface FindTreeOptions<Entity = any> {
+export interface FindTreeOptions {
 
   /**
     * Indicates what relations of entity should be loaded (simplified left join form).
