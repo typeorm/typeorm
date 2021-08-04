584e5a57ea7818fe9f671d39575aaca076deea86
James Ward <james@notjam.es>
2021-08-04 18:37:57 -0400

    test: use connection from test instead of global

--- test/github-issues/8016/issue-8016.ts
+++ test/github-issues/8016/issue-8016.ts
@@ -1,10 +1,9 @@
 import "reflect-metadata";
-import {getRepository} from "../../../src";
+import {Connection} from "../../../src";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
 import UserEntity from "./entity/UserEntity";
 import WorkspaceEntity from "./entity/WorkspaceEntity";
 import WorkspaceMemberEntity, {WorkspaceMemberRole} from "./entity/WorkspaceMemberEntity";
-import {Connection} from "../../../src/connection/Connection";
 import {expect} from "chai";
 
 describe.only("github issues > #8016 Cannot find alias for relation at user", () => {
@@ -19,21 +18,21 @@ describe.only("github issues > #8016 Cannot find alias for relation at user", ()
     after(() => closeTestingConnections(connections));
 
     it("should be able to perform the update", async () => await Promise.all(connections.map(async connection => {
-        const user = await getRepository(UserEntity).save({
+        const user = await connection.getRepository(UserEntity).save({
             id: "user-id"
         });
         expect(user.id).equal("user-id");
-        const workspace = await getRepository(WorkspaceEntity).save({
+        const workspace = await connection.getRepository(WorkspaceEntity).save({
             id: "workspace-id"
         });
         expect(workspace.id).equal("workspace-id");
-        let membership: WorkspaceMemberEntity | undefined = await getRepository(WorkspaceMemberEntity).save({
+        let membership: WorkspaceMemberEntity | undefined = await connection.getRepository(WorkspaceMemberEntity).save({
             user,
             workspace,
             role: WorkspaceMemberRole.ADMIN
         });
         expect(membership.role).equal(WorkspaceMemberRole.ADMIN);
-            await getRepository(WorkspaceMemberEntity).update(
+            await connection.getRepository(WorkspaceMemberEntity).update(
             {
                 user: {
                     id: "user-id",
@@ -46,7 +45,7 @@ describe.only("github issues > #8016 Cannot find alias for relation at user", ()
                 role: WorkspaceMemberRole.MEMBER,
             }
         );
-            membership = await getRepository(WorkspaceMemberEntity).findOne({
+            membership = await connection.getRepository(WorkspaceMemberEntity).findOne({
             user,
             workspace,
         });
