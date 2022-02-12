e42cef51f2b3949909164183a0498099e6b25c48
Dmitry Zotov <dmzt08@gmail.com>
2022-02-12 19:57:10 +0500

    added test for #5898

--- test/github-issues/5898/issue-5898.ts
+++ test/github-issues/5898/issue-5898.ts
@@ -3,13 +3,16 @@ import {expect} from "chai";
 import {Connection, QueryRunner} from "../../../src";
 import {createTestingConnections, closeTestingConnections} from "../../utils/test-utils";
 import {User} from "./entity/User";
+import {Document} from "./entity/Document";
+import {Album} from "./entity/Album";
+import {Photo} from "./entity/Photo";
 
-describe("github issues > #8273 Adding @Generated('uuid') doesn't update column default in PostgreSQL", () => {
+describe("github issues > #5898 Postgres primary key of type uuid: default value migration/sync not working", () => {
     let connections: Connection[];
-    const getColumnDefault = async (queryRunner: QueryRunner, columnName: string): Promise<string|null> => {
-        const query = `SELECT "column_default"`+
-            ` FROM "information_schema"."columns"`+
-            ` WHERE "table_schema" = 'public' AND "table_name" = 'user' AND "column_name" = '${columnName}'`
+    const getColumnDefault = async (queryRunner: QueryRunner, tableName: string, columnName: string): Promise<string|null> => {
+        const query = `SELECT "column_default"` +
+            ` FROM "information_schema"."columns"` +
+            ` WHERE "table_schema" = 'public' AND "table_name" = '${tableName}' AND "column_name" = '${columnName}'`
         const res = await queryRunner.query(query)
         return res.length ? res[0]["column_default"] : null
     }
@@ -17,89 +20,89 @@ describe("github issues > #8273 Adding @Generated('uuid') doesn't update column 
         enabledDrivers: ["postgres"],
         schemaCreate: true,
         dropSchema: true,
-        entities: [User],
+        entities: [User, Document, Album, Photo],
     }));
     after(() => closeTestingConnections(connections));
 
-    it("should add DEFAULT value when @Generated('increment') is added", () => Promise.all(connections.map(async connection => {
+    it("should add DEFAULT value when @PrimaryGeneratedColumn('increment') is added", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
-        let table = await queryRunner.getTable("user");
-        const column = table!.findColumnByName("increment")!;
+        let table = await queryRunner.getTable("photo");
+        const column = table!.findColumnByName("id")!;
         const newColumn = column.clone();
         newColumn.isGenerated = true;
         newColumn.generationStrategy = "increment";
 
         await queryRunner.changeColumn(table!, column, newColumn)
 
-        let columnDefault = await getColumnDefault(queryRunner, "increment")
-        expect(columnDefault).to.equal("nextval('user_increment_seq'::regclass)")
+        let columnDefault = await getColumnDefault(queryRunner, "photo", "id")
+        expect(columnDefault).to.equal("nextval('photo_id_seq'::regclass)")
 
         await queryRunner.executeMemoryDownSql();
 
-        columnDefault = await getColumnDefault(queryRunner, "increment")
+        columnDefault = await getColumnDefault(queryRunner, "photo", "id")
         expect(columnDefault).to.null
 
         await queryRunner.release()
     })));
 
-    it("should remove DEFAULT value when @Generated('increment') is removed", () => Promise.all(connections.map(async connection => {
+    it("should remove DEFAULT value when @PrimaryGeneratedColumn('increment') is removed", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
-        let table = await queryRunner.getTable("user");
-        const column = table!.findColumnByName("incrementWithGenerated")!;
+        let table = await queryRunner.getTable("album");
+        const column = table!.findColumnByName("id")!;
         const newColumn = column.clone();
         newColumn.isGenerated = false;
         newColumn.generationStrategy = undefined;
 
         await queryRunner.changeColumn(table!, column, newColumn)
 
-        let columnDefault = await getColumnDefault(queryRunner, "incrementWithGenerated")
+        let columnDefault = await getColumnDefault(queryRunner, "album", "id")
         expect(columnDefault).to.null
 
         await queryRunner.executeMemoryDownSql();
 
-        columnDefault = await getColumnDefault(queryRunner, "incrementWithGenerated")
-        expect(columnDefault).to.equal(`nextval('"user_incrementWithGenerated_seq"'::regclass)`)
+        columnDefault = await getColumnDefault(queryRunner, "album", "id")
+        expect(columnDefault).to.equal(`nextval('album_id_seq'::regclass)`)
 
         await queryRunner.release()
     })));
 
-    it("should add DEFAULT value when @Generated('uuid') is added", () => Promise.all(connections.map(async connection => {
+    it("should add DEFAULT value when @PrimaryGeneratedColumn('uuid') is added", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
-        let table = await queryRunner.getTable("user");
-        const column = table!.findColumnByName("uuid")!;
+        let table = await queryRunner.getTable("document");
+        const column = table!.findColumnByName("id")!;
         const newColumn = column.clone();
         newColumn.isGenerated = true;
         newColumn.generationStrategy = "uuid";
 
         await queryRunner.changeColumn(table!, column, newColumn)
 
-        let columnDefault = await getColumnDefault(queryRunner, "uuid")
+        let columnDefault = await getColumnDefault(queryRunner, "document", "id")
         expect(columnDefault).to.equal("uuid_generate_v4()")
 
         await queryRunner.executeMemoryDownSql();
 
-        columnDefault = await getColumnDefault(queryRunner, "uuid")
+        columnDefault = await getColumnDefault(queryRunner, "document", "id")
         expect(columnDefault).to.null
 
         await queryRunner.release()
     })));
 
-    it("should remove DEFAULT value when @Generated('uuid') is removed", () => Promise.all(connections.map(async connection => {
+    it("should remove DEFAULT value when @PrimaryGeneratedColumn('uuid') is removed", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
         let table = await queryRunner.getTable("user");
-        const column = table!.findColumnByName("uuidWithGenerated")!;
+        const column = table!.findColumnByName("id")!;
         const newColumn = column.clone();
         newColumn.isGenerated = false;
         newColumn.generationStrategy = undefined;
 
         await queryRunner.changeColumn(table!, column, newColumn)
 
-        let columnDefault = await getColumnDefault(queryRunner, "uuidWithGenerated")
+        let columnDefault = await getColumnDefault(queryRunner, "user", "id")
         expect(columnDefault).to.null
 
         await queryRunner.executeMemoryDownSql();
 
-        columnDefault = await getColumnDefault(queryRunner, "uuidWithGenerated")
+        columnDefault = await getColumnDefault(queryRunner, "user", "id")
         expect(columnDefault).to.equal("uuid_generate_v4()")
 
         await queryRunner.release()
