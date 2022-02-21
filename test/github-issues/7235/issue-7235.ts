a6d560729ced640a7f7ad2f0fbaf171842b0fce2
Nix <nix6839@washnix.com>
2022-02-21 15:22:05 +0900

    test: imporve test #7235

--- test/github-issues/7235/issue-7235.ts
+++ test/github-issues/7235/issue-7235.ts
@@ -3,8 +3,18 @@ import {createTestingConnections, closeTestingConnections, reloadTestingDatabase
 import {Animal} from "./entity/Animal";
 import {Connection} from "../../../src/connection/Connection";
 import {expect} from "chai";
+import {VersionUtils} from "../../../src/util/VersionUtils";
 
 describe('github issues > #7235 Use "INSERT...RETURNING" in MariaDB.', () => {
+    const runOnSpecificVersion = (version: string, fn: Function) =>
+        async () => Promise.all(connections.map(async (connection) => {
+            const result = await connection.query(`SELECT VERSION() AS \`version\``);
+            const dbVersion = result[0]["version"];
+            if (VersionUtils.isGreaterOrEqual(dbVersion, version)) {
+                await fn(connection);
+            }
+        }));
+
     let connections: Connection[];
 
     before(async () => connections = await createTestingConnections({
@@ -16,32 +26,56 @@ describe('github issues > #7235 Use "INSERT...RETURNING" in MariaDB.', () => {
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
 
-    it("should allow `{insert,delete}().returning()` on MariaDB >= 10.5.0", () =>
-        Promise.all(connections.map(async (connection) => {
+    it("should allow `DELETE...RETURNING` on MariaDB >= 10.0.5",
+        runOnSpecificVersion("10.0.5", async (connection: Connection) => {
             const animalRepository = connection.getRepository(Animal);
-            const insertCat = await animalRepository
+
+            await animalRepository
                 .createQueryBuilder()
                 .insert()
-                .values({ name: "Cat" })
-                .returning("name")
+                .values([{ name: "Cat" }, { name: "Wolf" }])
                 .execute();
-            expect(insertCat.raw[0]).to.deep.equal({ name: "Cat" });
 
-            const insertDog = await animalRepository
-                .createQueryBuilder()
-                .insert()
-                .values({ name: "Dog" })
-                .returning(["id", "name"])
-                .execute();
-            expect(insertDog.raw[0]).to.deep.equal({ id: 2, name: "Dog" });
-
-            const deleteDog = await animalRepository
+            const deleteCat = await animalRepository
                 .createQueryBuilder()
                 .delete()
-                .where({ name: "Dog" })
+                .where({ name: "Cat" })
                 .returning(["id", "name"])
                 .execute();
-            expect(deleteDog.raw[0]).to.deep.equal({ id: 2, name: "Dog" });
-        }))
+            expect(deleteCat.raw[0]).to.deep.equal({ id: 1, name: "Cat" });
+            const deleteWolf = await animalRepository
+                .createQueryBuilder()
+                .delete()
+                .where({ name: "Wolf" })
+                .returning("name")
+                .execute();
+            expect(deleteWolf.raw[0]).to.deep.equal({ name: "Wolf" });
+        })
+    );
+
+    it("should allow `INSERT...RETURNING` on MariaDB >= 10.5.0",
+        runOnSpecificVersion("10.5.0", async (connection: Connection) => {
+            const animalRepository = connection.getRepository(Animal);
+            const insertDogFox = await animalRepository
+                .createQueryBuilder()
+                .insert()
+                .values([{ name: "Dog" }, { name: "Fox" }])
+                .returning("name")
+                .execute();
+            expect(insertDogFox.raw).to.deep.equal([
+                { name: "Dog" },
+                { name: "Fox" },
+            ]);
+
+            const insertUnicorn = await animalRepository
+                .createQueryBuilder()
+                .insert()
+                .values({ name: "Unicorn" })
+                .returning(["id", "name"])
+                .execute();
+            expect(insertUnicorn.raw[0]).to.deep.equal(
+                { id: 3, name: "Unicorn" },
+            );
+        })
     );
 });
