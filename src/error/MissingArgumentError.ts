25887aa1a2cbcb6cf8e4db8236ef22f877f9f0fd
cwikla <cwikla@valenciadata.com>
2022-03-12 15:23:14 -0800

    Add Error

---
+++ src/error/MissingArgumentError.ts
@@ -0,0 +1,12 @@
+/**
+ * Thrown when null or undefined are passed into find or update
+*/
+export class MissingArgumentError extends Error {
+
+    constructor() {
+        super();
+        Object.setPrototypeOf(this, MissingArgumentError.prototype);
+        this.message = `You are passing null or undefined into a function that requires one`;
+    }
+
+}
