9b8c43fdb82bc176a0fc7c510bbface7bd7442ca
Umed Khudoiberdiev <pleerock.me@gmail.com>
2022-03-26 20:02:31 +0500

    type simplification

--- src/common/DeepPartial.ts
+++ src/common/DeepPartial.ts
@@ -1,14 +1,12 @@
 /**
  * Same as Partial<T> but goes deeper and makes Partial<T> all its properties and sub-properties.
  */
-export type DeepPartial<T> =
-    T extends Array<infer U> ? DeepPartialArray<U> :
-    T extends Map<infer K, infer V> ? DeepPartialMap<K, V> :
-    T extends Set<infer M> ? DeepPartialSet<M> :
-    T extends object ? DeepPartialObject<T> :
-    T;
-
-export type DeepPartialArray<T> = Array<DeepPartial<T>>;
-export type DeepPartialMap<K, V> = Map<DeepPartial<K>, DeepPartial<V>>;
-export type DeepPartialSet<T> = Set<DeepPartial<T>>;
-export type DeepPartialObject<T> = { [K in keyof T]?: DeepPartial<T[K]> };
+export type DeepPartial<T> = T extends Array<infer U>
+    ? DeepPartial<U>[]
+    : T extends Map<infer K, infer V>
+    ? Map<DeepPartial<K>, DeepPartial<V>>
+    : T extends Set<infer M>
+    ? Set<DeepPartial<M>>
+    : T extends object
+    ? { [K in keyof T]?: DeepPartial<T[K]> }
+    : T;
