c9f6fded15c06784847b0a05912dca5f394c4b80
Felix Gohla <felix.gohla@student.hpi.de>
2022-05-16 15:01:29 +0200

    fix: Create correct children with STI
    This commit fixes the `create` function for EntityManager and Repository
    to create entities of correct type when using Single Table Inheritance.
    Refactors the otherwise repeated code into a new function on
    EntityMetadata.
    
    Related to: #7758

--- src/metadata/EntityMetadata.ts
+++ src/metadata/EntityMetadata.ts
@@ -829,31 +829,54 @@ export class EntityMetadata {
                     relationsAndValues.push([
                         relation,
                         subValue,
-                        this.getInverseEntityMetadata(subValue, relation),
+                        EntityMetadata.getInverseEntityMetadata(
+                            subValue,
+                            relation,
+                        ),
                     ]),
                 )
             } else if (value) {
                 relationsAndValues.push([
                     relation,
                     value,
-                    this.getInverseEntityMetadata(value, relation),
+                    EntityMetadata.getInverseEntityMetadata(value, relation),
                 ])
             }
         })
         return relationsAndValues
     }
 
-    private getInverseEntityMetadata(
+    /**
+     * In the case of SingleTableInheritance, find the correct child metadata
+     * for a given value.
+     *
+     * @param value The value to find the metadata for.
+     * @returns The found metadata for the child entity or the base metadata if no child was found.
+     */
+    findChildMetadata(value: any): EntityMetadata {
+        // Check for single table inheritance and find the correct metadata in that case.
+        // Goal is to use the correct discriminator as we could have a repository
+        // for an (abstract) base class and thus the target would not match.
+        if (
+            this.inheritancePattern === "STI" &&
+            this.childEntityMetadatas.length > 0
+        ) {
+            return this.childEntityMetadatas.find(
+                (meta) => value.constructor === meta.target,
+            ) || this
+        }
+        return this
+    }
+
+    // -------------------------------------------------------------------------
+    // Private Static Methods
+    // -------------------------------------------------------------------------
+
+    private static getInverseEntityMetadata(
         value: any,
         relation: RelationMetadata,
     ): EntityMetadata {
-        const childEntityMetadata =
-            relation.inverseEntityMetadata.childEntityMetadatas.find(
-                (metadata) => metadata.target === value.constructor,
-            )
-        return childEntityMetadata
-            ? childEntityMetadata
-            : relation.inverseEntityMetadata
+        return relation.inverseEntityMetadata.findChildMetadata(value)
     }
 
     // -------------------------------------------------------------------------
