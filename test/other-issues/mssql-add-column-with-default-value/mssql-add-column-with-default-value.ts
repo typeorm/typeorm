026138d2869f0241c2dd8c461306fa583e4fbd8b
Umed Khudoiberdiev <pleerock.me@gmail.com>
2022-06-28 17:54:19 +0500

    fixing compiler errors

--- test/other-issues/mssql-add-column-with-default-value/mssql-add-column-with-default-value.ts
+++ test/other-issues/mssql-add-column-with-default-value/mssql-add-column-with-default-value.ts
@@ -16,7 +16,7 @@ describe("mssql -> add column to existing table", () => {
         await Promise.all(
             connections.map(async (connection) => {
                 await connection.synchronize(true)
-                await connection.getRepository("Post").insert({ title: "test" })
+                await connection.getRepository(Post).insert({ title: "test" })
                 await connection.close()
             }),
         )
