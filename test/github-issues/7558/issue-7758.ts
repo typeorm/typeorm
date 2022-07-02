0a86426d387e6275ec443fec56f86d6a00ead155
Felix Gohla <felix.gohla@student.hpi.de>
2022-07-02 18:16:39 +0200

    test: test saving disciminators with trees in STI
    This commit adds an test for checking whether discriminators are saved
    correctly when saving a tree that also uses Single-Table-Inheritance.
    
    Related to: #7758

--- test/github-issues/7558/issue-7758.ts
+++ test/github-issues/7558/issue-7758.ts
@@ -7,10 +7,13 @@ import {
 import { DataSource } from "../../../src/data-source/index"
 import { expect } from "chai"
 
-import { PersonEntity } from "./entity/Person"
+import { AnimalEntity } from "./entity/Animal"
 import { CatEntity } from "./entity/Cat"
 import { DogEntity } from "./entity/Dog"
-import { AnimalEntity } from "./entity/Animal"
+import { NnaryOperator } from "./entity/NnaryOperator"
+import { NumberEntry } from "./entity/NumberEntry"
+import { OperatorTreeEntry } from "./entity/OperatorTreeEntry"
+import { PersonEntity } from "./entity/Person"
 import { Photo } from "./entity/Photo"
 
 describe("github issues > #7558 Child entities' wrong discriminator value when embedded in parent entity", () => {
@@ -121,6 +124,75 @@ describe("github issues > #7558 Child entities' wrong discriminator value when e
                 expect(catPets[0]).to.include(felix)
                 expect(dogPets.length).to.equal(1)
                 expect(dogPets[0]).to.include(spike)
+            }),
+        ))
+
+    it("should set the correct discriminators for trees in STI settings", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const entityManager = dataSource.createEntityManager()
+                const operatorTreeRepo =
+                    entityManager.getTreeRepository(OperatorTreeEntry)
+                const nnaryOperatorRepo =
+                    entityManager.getTreeRepository(NnaryOperator)
+                const numberEntryRepo = entityManager.getRepository(NumberEntry)
+
+                // Create an operator tree (10.5 * (21 + 42)).
+                const num1 = numberEntryRepo.create({ value: 42 })
+                const num2 = numberEntryRepo.create({ value: 21 })
+                const num3 = numberEntryRepo.create({ value: 10.5 })
+
+                const mulOperator = nnaryOperatorRepo.create({
+                    operator: "*",
+                })
+
+                // Save the tree...
+                // We need to first save the parents step by step.
+                const savedMulOperator = await operatorTreeRepo.save(
+                    mulOperator,
+                )
+                const plusOperator = nnaryOperatorRepo.create({
+                    parent: savedMulOperator,
+                    operator: "+",
+                })
+                num3.parent = savedMulOperator
+
+                await operatorTreeRepo.save(num3)
+                const savedPlusOperator = await operatorTreeRepo.save(
+                    plusOperator,
+                )
+
+                num1.parent = savedPlusOperator
+                num2.parent = savedPlusOperator
+                await operatorTreeRepo.save([num1, num2])
+
+                // ... and fetch it from the database.
+                const fetchedTrees = await operatorTreeRepo.findTrees()
+                expect(fetchedTrees.length).to.eql(1)
+                expect(fetchedTrees[0]).to.be.instanceOf(NnaryOperator)
+                const fetchedOperator = fetchedTrees[0] as NnaryOperator
+                expect(fetchedOperator.operator).to.eql("*")
+
+                expect(fetchedOperator.children.length).to.be.eql(2)
+                // Order is *not* deterministic by default...
+                expect(
+                    fetchedOperator.children.find(
+                        (operator) => operator.id === num3.id,
+                    ),
+                ).to.be.instanceOf(NumberEntry)
+                expect(
+                    fetchedOperator.children.find(
+                        (operator) => operator.id === savedPlusOperator.id,
+                    ),
+                ).to.be.instanceOf(NnaryOperator)
+
+                const subOperator = fetchedOperator.children.find(
+                    (operator) => operator.id === savedPlusOperator.id,
+                ) as NnaryOperator
+                expect(subOperator.operator).to.eql("+")
+                expect(subOperator.children.length).to.be.eql(2)
+                expect(subOperator.children[0]).to.be.instanceOf(NumberEntry)
+                expect(subOperator.children[1]).to.be.instanceOf(NumberEntry)
             }),
         ))
 })
