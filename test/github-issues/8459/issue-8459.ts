1d7a06038a28fae209c034750651e0d4ddd4c442
Gonzalo <rodzalo@tutanota.com>
2022-10-28 11:38:27 -0400

    feat: revamped tests
    Replaced previous unit tests with more significant ones

--- test/github-issues/8459/issue-8459.ts
+++ test/github-issues/8459/issue-8459.ts
@@ -2,23 +2,18 @@ import "reflect-metadata"
 import {
     createTestingConnections,
     closeTestingConnections,
-    reloadTestingDatabases,
 } from "../../utils/test-utils"
 import { DataSource } from "../../../src/data-source/DataSource"
-import { assert } from "chai"
+import { assert, expect } from "chai"
 import { PostgresQueryRunner } from "../../../src/driver/postgres/PostgresQueryRunner"
 import { TableIndex } from "../../../src"
+import { PostCategory } from "./entity/PostCategory"
+import { IndexMetadata } from "../../../src/metadata/IndexMetadata"
 
 describe("github issues > #8459 Can not create indexes of materialized views", () => {
     const tableIndex: TableIndex = new TableIndex({
         columnNames: ["name"],
         name: "name-idx",
-    })
-
-    const uniqueTableIndex: TableIndex = new TableIndex({
-        columnNames: ["categoryName"],
-        name: "catname-idx",
-        isUnique: true,
     })
 
     let dataSources: DataSource[]
@@ -31,57 +26,126 @@ describe("github issues > #8459 Can not create indexes of materialized views", (
                 enabledDrivers: ["postgres"],
             })),
     )
-    beforeEach(() => reloadTestingDatabases(dataSources))
     after(() => closeTestingConnections(dataSources))
 
-    it("should create a view index at runtime", () =>
+    it("should create a materialized view index at runtime", () =>
         Promise.all(
             dataSources.map(async (dataSource) => {
                 const postgresQueryRunner: PostgresQueryRunner = <
                     PostgresQueryRunner
                 >dataSource.createQueryRunner()
-                const views = await postgresQueryRunner.getViews([])
-                assert.deepEqual(views[0].indices[0], tableIndex)
+                const view = await postgresQueryRunner.getView("post_category")
+                assert.deepEqual(view!.indices[0], tableIndex)
             }),
         ))
 
-    it("should delete a view index", () =>
+    it("should rename a materialized view unique index", () =>
         Promise.all(
             dataSources.map(async (dataSource) => {
+                const postCatMetadata = dataSource.getMetadata(PostCategory)
+                postCatMetadata.indices[0].name = "renamed-ix"
+
+                await dataSource.synchronize()
+
                 const postgresQueryRunner: PostgresQueryRunner = <
                     PostgresQueryRunner
                 >dataSource.createQueryRunner()
-                let views = await postgresQueryRunner.getViews([])
-                postgresQueryRunner.dropViewIndex(views[0], tableIndex)
-                views = await postgresQueryRunner.getViews([])
-                assert.isEmpty(views[0].indices)
+                const view = await postgresQueryRunner.getView("post_category")
+                await postgresQueryRunner.release()
+
+                const index = view!.indices.find((i) => i.name === "renamed-ix")
+
+                expect(index).not.be.undefined
             }),
         ))
 
-    it("should create a view index", () =>
+    it("should delete a materialized view index", () =>
         Promise.all(
             dataSources.map(async (dataSource) => {
+                const postCatMetadata = dataSource.getMetadata(PostCategory)
+                postCatMetadata.indices.splice(0, 1)
+
+                await dataSource.synchronize()
+
                 const postgresQueryRunner: PostgresQueryRunner = <
                     PostgresQueryRunner
                 >dataSource.createQueryRunner()
-                let views = await postgresQueryRunner.getViews([])
-                postgresQueryRunner.createViewIndex(views[0], tableIndex)
-                views = await postgresQueryRunner.getViews([])
-                assert.deepEqual(views[0].indices[0], tableIndex)
+                const view = await postgresQueryRunner.getView("post_category")
+                await postgresQueryRunner.release()
+                view!.indices.length.should.be.equal(0)
             }),
         ))
 
-    it("should create a view unique index", () =>
+    it("should create a materialized view index", () =>
         Promise.all(
             dataSources.map(async (dataSource) => {
+                const postCatMetadata = dataSource.getMetadata(PostCategory)
+                const nameColumn =
+                    postCatMetadata.findColumnWithPropertyName("name")!
+                const indexMetadata = new IndexMetadata({
+                    entityMetadata: postCatMetadata,
+                    columns: [nameColumn],
+                    args: {
+                        target: PostCategory,
+                        synchronize: true,
+                        name: "name-ix",
+                    },
+                })
+                indexMetadata.build(dataSource.namingStrategy)
+                postCatMetadata.indices.push(indexMetadata)
+
+                await dataSource.synchronize()
+
                 const postgresQueryRunner: PostgresQueryRunner = <
                     PostgresQueryRunner
                 >dataSource.createQueryRunner()
-                let views = await postgresQueryRunner.getViews([])
-                postgresQueryRunner.dropViewIndex(views[0], tableIndex)
-                postgresQueryRunner.createViewIndex(views[0], uniqueTableIndex)
-                views = await postgresQueryRunner.getViews([])
-                assert.deepEqual(views[0].indices[0], uniqueTableIndex)
+                const view = await postgresQueryRunner.getView("post_category")
+                await postgresQueryRunner.release()
+
+                view!.indices.length.should.be.equal(1)
+
+                postCatMetadata.indices.splice(
+                    postCatMetadata.indices.indexOf(indexMetadata),
+                    1,
+                )
+            }),
+        ))
+
+    it("should create a materialized view unique index", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const postCatMetadata = dataSource.getMetadata(PostCategory)
+                const nameColumn =
+                    postCatMetadata.findColumnWithPropertyName("name")!
+                const indexMetadata = new IndexMetadata({
+                    entityMetadata: postCatMetadata,
+                    columns: [nameColumn],
+                    args: {
+                        target: PostCategory,
+                        synchronize: true,
+                        name: "name-ix",
+                        unique: true,
+                    },
+                })
+                indexMetadata.build(dataSource.namingStrategy)
+                postCatMetadata.indices.push(indexMetadata)
+
+                await dataSource.synchronize()
+
+                const postgresQueryRunner: PostgresQueryRunner = <
+                    PostgresQueryRunner
+                >dataSource.createQueryRunner()
+                const view = await postgresQueryRunner.getView("post_category")
+                await postgresQueryRunner.release()
+
+                const index = view!.indices.find((i) => i.name === "name-ix")
+
+                expect(index?.isUnique).not.be.false
+
+                postCatMetadata.indices.splice(
+                    postCatMetadata.indices.indexOf(indexMetadata),
+                    1,
+                )
             }),
         ))
 })
