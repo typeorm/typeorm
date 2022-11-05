be1eb3f6bf71bab3cd08e00d438fa959f0c67339
AlexMesser <dmzt08@gmail.com>
2022-11-05 13:55:46 +0500

    fix typo

--- test/functional/driver/abstract-sqlite/abstract-sqlite-escape-query-parameters.ts
+++ test/functional/driver/abstract-sqlite/abstract-sqlite-escape-query-parameters.ts
@@ -17,7 +17,7 @@ describe("escape sqlite query parameters", () => {
     beforeEach(() => reloadTestingDatabases(connections))
     after(() => closeTestingConnections(connections))
 
-    it("should transform booelan parameters with value `true` into `1`", () =>
+    it("should transform boolean parameters with value `true` into `1`", () =>
         Promise.all(
             connections.map((connection) => {
                 const [_, parameters] =
@@ -31,7 +31,7 @@ describe("escape sqlite query parameters", () => {
             }),
         ))
 
-    it("should transform booelan parameters with value `false` into `0`", () =>
+    it("should transform boolean parameters with value `false` into `0`", () =>
         Promise.all(
             connections.map((connection) => {
                 const [_, parameters] =
@@ -45,7 +45,7 @@ describe("escape sqlite query parameters", () => {
             }),
         ))
 
-    it("should transform booelan nativeParameters with value `true` into `1`", () =>
+    it("should transform boolean nativeParameters with value `true` into `1`", () =>
         Promise.all(
             connections.map((connection) => {
                 const [_, parameters] =
@@ -59,7 +59,7 @@ describe("escape sqlite query parameters", () => {
             }),
         ))
 
-    it("should transform booelan nativeParameters with value `false` into 0", () =>
+    it("should transform boolean nativeParameters with value `false` into 0", () =>
         Promise.all(
             connections.map((connection) => {
                 const [_, parameters] =
