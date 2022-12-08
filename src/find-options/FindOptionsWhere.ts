9bd9a751d24153b1ed78c1d7ae86b356df5fd186
Toshinori Tsugita <tsugita.dev@gmail.com>
2022-12-08 22:36:43 +0900

    fix: allow FindOptionsWhere to accept LessThan with Union

--- src/find-options/FindOptionsWhere.ts
+++ src/find-options/FindOptionsWhere.ts
@@ -5,34 +5,37 @@ import { EqualOperator } from "./EqualOperator"
 /**
  * A single property handler for FindOptionsWhere.
  */
-export type FindOptionsWhereProperty<Property> = Property extends Promise<
-    infer I
->
+export type FindOptionsWhereProperty<
+    Property,
+    Original = Property,
+> = Property extends Promise<infer I>
     ? FindOptionsWhereProperty<NonNullable<I>>
     : Property extends Array<infer I>
     ? FindOptionsWhereProperty<NonNullable<I>>
     : Property extends Function
     ? never
     : Property extends Buffer
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends Date
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends ObjectID
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends string
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends number
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends boolean
-    ? Property | FindOperator<Property>
+    ? Property | FindOperator<Property> | FindOperator<Original>
     : Property extends object
     ?
           | FindOptionsWhere<Property>
+          | FindOptionsWhere<Original>
           | FindOptionsWhere<Property>[]
           | EqualOperator<Property>
+          | EqualOperator<Original>
           | FindOperator<any>
           | boolean
-    : Property | FindOperator<Property>
+    : Property | FindOperator<Property> | FindOperator<Original>
 
 /**
  * Used for find operations.
