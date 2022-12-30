478b433573ad336848ecb586b191af9993e81be8
mustafaabasaran <mustafabasaran93@gmail.com>
2022-12-30 14:31:41 +0300

    Update ColumnMetadata.ts
    format code.

--- src/metadata/ColumnMetadata.ts
+++ src/metadata/ColumnMetadata.ts
@@ -476,7 +476,7 @@ export class ColumnMetadata {
             this.isInsert = false
             this.isUpdate = false
         }
-        if (options.args.options.primary){
+        if (options.args.options.primary) {
             this.isUpdate = false
         }
         if (options.args.options.transformer)
