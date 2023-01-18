f3c0b26c84706e09c9d03734da1732db7bdd04d7
Nadzeya Shedava <84352147+nadzeya-astrumu@users.noreply.github.com>
2023-01-18 13:56:37 -0800

    Update StringUtils.ts

--- src/util/StringUtils.ts
+++ src/util/StringUtils.ts
@@ -9,7 +9,7 @@ export function camelCase(str: string, firstCapital: boolean = false): string {
     return str.replace(
         /^([A-Z])|[\s-_](\w)/g,
         function (match, p1, p2, offset) {
-            if (firstCapital === true && offset === 0) return p1
+            if (firstCapital === true && offset === 0) return p1.toUpperCase()
             if (p2) return p2.toUpperCase()
             return p1.toLowerCase()
         },
