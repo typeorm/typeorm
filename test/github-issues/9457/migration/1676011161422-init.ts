a82fe2203d2fc099fe4e1f2dfa80c4e5e216cf0a
ke <ke@sbs.co.at>
2023-02-10 08:19:13 +0100

    fix: resolve issues for mssql migration when simple-enum was changed
    - Changes are now detected
    - Incorrect update Statement was split into a DROP CONSTRAINT and ADD CONSTRAINT Statement
    
    Closes: #7785 #9457

---
+++ test/github-issues/9457/migration/1676011161422-init.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "../../../../src"
+
+export class init1676011161422 implements MigrationInterface {
+    name = "init1676011161422"
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TABLE "example_entity"
+             (
+                 "id"         int NOT NULL IDENTITY(1,1),
+                 "enumcolumn" nvarchar(255) CONSTRAINT CHK_be8ed063b3976da24df4213baf_ENUM CHECK (enumcolumn IN ('enumvalue1','enumvalue2','enumvalue3')) NOT NULL,
+                 CONSTRAINT "PK_fccd73330168066a434dbac114f" PRIMARY KEY ("id")
+             )`,
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "example_entity"`)
+    }
+}
