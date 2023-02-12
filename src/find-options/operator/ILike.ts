efa6ae2768ac697841a5b3146ca0fbb627beea7c
Irfanullah Jan <irfanullah.jan@outlook.com>
2023-02-12 11:13:18 +0500

    more info about ILike

--- src/find-options/operator/ILike.ts
+++ src/find-options/operator/ILike.ts
@@ -2,6 +2,7 @@ import { FindOperator } from "../FindOperator"
 
 /**
  * Find Options Operator.
+ * Performs a case-insensitive match.
  * Example: { someField: ILike("%SOME string%") }
  */
 export function ILike<T>(value: T | FindOperator<T>) {
