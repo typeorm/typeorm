5d5f1ecab7338c8a96359b7597ceac53b986ebb5
gmmkmtgk <keshavmaheshwari800@gmail.com>
2023-03-01 19:51:42 +0530

    Formated code

--- src/common/DeepPartial.ts
+++ src/common/DeepPartial.ts
@@ -7,14 +7,14 @@ export type DeepPartial<T> =
     // If T is an array, apply DeepPartial to its element type and return an array of those.
     | (T extends Array<infer U>
           ? DeepPartial<U>[]
-          // If T is a Map, apply DeepPartial to its key and value types and return a new Map.
-          : T extends Map<infer K, infer V>
+          : // If T is a Map, apply DeepPartial to its key and value types and return a new Map.
+          T extends Map<infer K, infer V>
           ? Map<DeepPartial<K>, DeepPartial<V>>
-          // If T is a Set, apply DeepPartial to its element type and return a new Set.
-          : T extends Set<infer M>
+          : // If T is a Set, apply DeepPartial to its element type and return a new Set.
+          T extends Set<infer M>
           ? Set<DeepPartial<M>>
-          // If T is an object, recursively apply DeepPartial to all of its properties.
-          : T extends object
+          : // If T is an object, recursively apply DeepPartial to all of its properties.
+          T extends object
           ? { [K in keyof T]?: DeepPartial<T[K]> }
-          // If T is anything else (e.g. a function), return it as is.
-          : T)
+          : // If T is anything else (e.g. a function), return it as is.
+            T)
