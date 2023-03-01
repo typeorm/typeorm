11f9eb0681a530a49ee6a88b7be4b310b01c63c6
Keshav Maheshwari <51493190+gmmkmtgk@users.noreply.github.com>
2023-03-01 19:23:09 +0530

    Added comments to EntityTarget.ts

--- src/common/EntityTarget.ts
+++ src/common/EntityTarget.ts
@@ -1,11 +1,19 @@
-import { ObjectType } from "./ObjectType"
-import { EntitySchema } from ".."
+import { ObjectType } from "./ObjectType";
+import { EntitySchema } from "..";
 
 /**
  * Entity target.
+ * 
+ * A type that describes the target of a database query or transaction involving an entity.
+ * 
+ * @typeParam Entity - The type of the entity.
  */
 export type EntityTarget<Entity> =
-    | ObjectType<Entity>
-    | EntitySchema<Entity>
-    | string
-    | { type: Entity; name: string }
+  // A reference to an ObjectType that represents the entity.
+  | ObjectType<Entity>
+  // A reference to an EntitySchema that represents the entity.
+  | EntitySchema<Entity>
+  // A string that represents the name of the entity.
+  | string
+  // An object that specifies both the type and name of the entity.
+  | { type: Entity; name: string };
