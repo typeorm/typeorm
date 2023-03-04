34ef5e999ded90f6e4849461e8db2eef76d6a3ca
Keshav Maheshwari <51493190+gmmkmtgk@users.noreply.github.com>
2023-03-05 00:34:39 +0530

    Added comments to CacheClearCommand
    Added necessary comments so that one can understand what is happening

--- src/commands/CacheClearCommand.ts
+++ src/commands/CacheClearCommand.ts
@@ -10,9 +10,11 @@ import { CommandUtils } from "./CommandUtils"
  * Clear cache command.
  */
 export class CacheClearCommand implements yargs.CommandModule {
+    // Define the command name and usage description for this command
     command = "cache:clear"
     describe = "Clears all data stored in query runner cache."
 
+    // Define the command line options that this command supports
     builder(args: yargs.Argv) {
         return args.option("dataSource", {
             alias: "d",
@@ -22,12 +24,16 @@ export class CacheClearCommand implements yargs.CommandModule {
         })
     }
 
+    // Define the logic that runs when this command is executed
     async handler(args: yargs.Arguments) {
         let dataSource: DataSource | undefined = undefined
         try {
+            // Load the DataSource instance from the specified file path
             dataSource = await CommandUtils.loadDataSource(
                 path.resolve(process.cwd(), args.dataSource as string),
             )
+
+            // Disable all options that may affect the cache
             dataSource.setOptions({
                 subscribers: [],
                 synchronize: false,
@@ -35,20 +41,27 @@ export class CacheClearCommand implements yargs.CommandModule {
                 dropSchema: false,
                 logging: ["schema"],
             })
+
+            // Initialize the DataSource instance
             await dataSource.initialize()
 
+            // Check if the cache is enabled
             if (!dataSource.queryResultCache) {
+                // If not, log an error message and exit the command
                 PlatformTools.logCmdErr(
                     "Cache is not enabled. To use cache enable it in connection configuration.",
                 )
                 return
             }
 
+            // Clear the cache
             await dataSource.queryResultCache.clear()
             console.log(chalk.green("Cache was successfully cleared"))
 
+            // Destroy the DataSource instance
             await dataSource.destroy()
         } catch (err) {
+            // If an error occurred, log an error message and exit the command with code 1
             PlatformTools.logCmdErr("Error during cache clear.", err)
 
             if (dataSource && dataSource.isInitialized)
