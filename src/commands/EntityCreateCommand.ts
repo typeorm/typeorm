65a698d13e7f08e73c4a5ee94e817c5b8610a88a
Keshav Maheshwari <51493190+gmmkmtgk@users.noreply.github.com>
2023-03-05 00:51:16 +0530

    Update EntityCreateCommand.ts

--- src/commands/EntityCreateCommand.ts
+++ src/commands/EntityCreateCommand.ts
@@ -8,20 +8,27 @@ import path from "path"
  * Generates a new entity.
  */
 export class EntityCreateCommand implements yargs.CommandModule {
+    // Command description and syntax definition
     command = "entity:create <path>"
     describe = "Generates a new entity."
 
+    // Command handler
     async handler(args: yargs.Arguments) {
         try {
+            // Determine the full path to the file to be created
             const fullPath = (args.path as string).startsWith("/")
                 ? (args.path as string)
                 : path.resolve(process.cwd(), args.path as string)
+            // Extract the name of the entity from the file path
             const filename = path.basename(fullPath)
+            // Generate the contents of the entity file using the template
             const fileContent = EntityCreateCommand.getTemplate(filename)
+            // Check if the file already exists
             const fileExists = await CommandUtils.fileExists(fullPath + ".ts")
             if (fileExists) {
                 throw `File ${chalk.blue(fullPath + ".ts")} already exists`
             }
+            // Create the entity file
             await CommandUtils.createFile(fullPath + ".ts", fileContent)
             console.log(
                 chalk.green(
@@ -31,6 +38,7 @@ export class EntityCreateCommand implements yargs.CommandModule {
                 ),
             )
         } catch (err) {
+            // Log any errors that occur during entity creation
             PlatformTools.logCmdErr("Error during entity creation:", err)
             process.exit(1)
         }
@@ -42,6 +50,8 @@ export class EntityCreateCommand implements yargs.CommandModule {
 
     /**
      * Gets contents of the entity file.
+     * Uses a template string to generate the contents of the new entity file.
+     * The name of the entity is passed in as a parameter.
      */
     protected static getTemplate(name: string): string {
         return `import { Entity } from "typeorm"
