73c409ead72a432294696734dfce73f28c0738ac
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2023-03-23 15:03:47 +0100

    change test

--- test/functional/query-builder/select/query-builder-select.ts
+++ test/functional/query-builder/select/query-builder-select.ts
@@ -5,12 +5,7 @@ import {
     reloadTestingDatabases,
 } from "../../../utils/test-utils"
 import { expect } from "chai"
-import {
-    DataSource,
-    In,
-    IsNull,
-    Raw,
-} from "../../../../src"
+import { DataSource, In, IsNull, Raw } from "../../../../src"
 import { Category } from "./entity/Category"
 import { Post } from "./entity/Post"
 import { Tag } from "./entity/Tag"
