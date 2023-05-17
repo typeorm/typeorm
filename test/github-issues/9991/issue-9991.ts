4c95e2d5e1dd4b597dde6a75f50b6ea2c03d9bf9
sinkhaha <1468709606@qq.com>
2023-05-17 17:10:59 +0800

    add test

--- test/github-issues/9991/issue-9991.ts
+++ test/github-issues/9991/issue-9991.ts
@@ -32,4 +32,51 @@ describe("github issues > #9991", () => {
             }),
         )
     })
+
+    it("should correctly change table comment", async () => {
+        await Promise.all(
+            dataSources.map(async (dataSource) => {
+                const queryRunner = dataSource.createQueryRunner()
+                let table = await queryRunner.getTable("example")
+
+                await queryRunner.changeTableComment(
+                    table!,
+                    "this is new table comment",
+                )
+
+                const sql =
+                    'SELECT table_comment FROM information_schema.tables WHERE table_name = "example"'
+                let rst = await dataSource.manager.query(sql)
+
+                expect(rst[0] && rst[0].TABLE_COMMENT).to.be.eq(
+                    "this is new table comment",
+                )
+
+                await queryRunner.release()
+            }),
+        )
+    })
+
+    it("should correctly synchronize when table comment change", async () => {
+        await Promise.all(
+            dataSources.map(async (dataSource) => {
+                const queryRunner = dataSource.createQueryRunner()
+
+                const exampleMetadata = dataSource.getMetadata(ExampleEntity)
+                exampleMetadata.comment = "change table comment"
+
+                await dataSource.synchronize()
+
+                const sql =
+                    'SELECT table_comment FROM information_schema.tables WHERE table_name = "example"'
+                let rst = await dataSource.manager.query(sql)
+
+                expect(rst[0] && rst[0].TABLE_COMMENT).to.be.eq(
+                    "change table comment",
+                )
+
+                await queryRunner.release()
+            }),
+        )
+    })
 })
