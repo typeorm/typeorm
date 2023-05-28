f22d5e8feeed3d6006663df134e41ad52de4d546
Deep <36783904+deepkhurana1999@users.noreply.github.com>
2023-05-28 23:46:29 +0530

    feat: implement or operator
    Renamed Or operator file name.
    
    Closes: #10054

--- src/find-options/operator/OR.ts
+++
@@ -1,5 +0,0 @@
-import { FindOperator } from "../FindOperator"
-
-export function OR<T>(...values: FindOperator<T>[]): FindOperator<T> {
-    return new FindOperator("or", values as any, true, true)
-}
