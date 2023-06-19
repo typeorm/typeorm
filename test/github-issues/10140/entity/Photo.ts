00c4fb1f82d3c57b53ad25256d6e7bda6409569f
bejewelkyoungminkim <kyoungminkim@bejewel.kr>
2023-06-19 11:48:57 +0900

    fix: resolve issues sqlite can use timestamp type column
    Closes: #10140

---
+++ test/github-issues/10140/entity/Photo.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "../../../../src";
+
+@Entity()
+export class Photo {
+    @PrimaryGeneratedColumn({
+        type: 'int',
+    })
+    id: number
+
+    @Column('varchar')
+    name: string;
+
+    @Column('timestamp')
+    publishedAt: Date
+}
