a236bdf03460bb22686ce058ceed43e4d711ff7a
Lang Minh Nguyên <3langn@gmail.com>
2023-07-27 23:07:36 +0700

    test: added test for issue-3302

---
+++ test/github-issues/3302/entity/Post.ts
@@ -0,0 +1,15 @@
+import { Column } from "../../../../src/decorator/columns/Column"
+import { PrimaryColumn } from "../../../../src/decorator/columns/PrimaryColumn"
+import { Entity } from "../../../../src/decorator/entity/Entity"
+
+@Entity()
+export class Post {
+    @PrimaryColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    beforeQuery: boolean = false
+    afterQuery: boolean = false
+}
