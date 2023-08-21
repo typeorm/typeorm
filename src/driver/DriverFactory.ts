d07bb62acec6bf001bbef312f91142be8438b352
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-08-21 12:08:03 +0200

    migrate from `require` to `import`

--- src/driver/DriverFactory.ts
+++ src/driver/DriverFactory.ts
@@ -1,6 +1,81 @@
 import type { Driver, DriverConstructor } from "./Driver"
 import type { DataSource } from "../data-source/DataSource"
 
+const getDriver = async (
+    type: DataSource["options"]["type"],
+): Promise<DriverConstructor> => {
+    switch (type) {
+        case "mysql":
+        case "mariadb":
+            return (await import("./mysql/MysqlDriver")).MysqlDriver
+        case "postgres":
+            return (await import("./postgres/PostgresDriver")).PostgresDriver
+        case "cockroachdb":
+            return (await import("./cockroachdb/CockroachDriver"))
+                .CockroachDriver
+        case "sap":
+            return (await import("./sap/SapDriver")).SapDriver
+        case "sqlite":
+            return (await import("./sqlite/SqliteDriver")).SqliteDriver
+        case "better-sqlite3":
+            return (await import("./better-sqlite3/BetterSqlite3Driver"))
+                .BetterSqlite3Driver
+        case "cordova":
+            return (await import("./cordova/CordovaDriver")).CordovaDriver
+        case "nativescript":
+            return (await import("./nativescript/NativescriptDriver"))
+                .NativescriptDriver
+        case "react-native":
+            return (await import("./react-native/ReactNativeDriver"))
+                .ReactNativeDriver
+        case "sqljs":
+            return (await import("./sqljs/SqljsDriver")).SqljsDriver
+        case "oracle":
+            return (await import("./oracle/OracleDriver")).OracleDriver
+        case "mssql":
+            return (await import("./sqlserver/SqlServerDriver")).SqlServerDriver
+        case "mongodb":
+            return (await import("./mongodb/MongoDriver")).MongoDriver
+        case "expo":
+            return (await import("./expo/ExpoDriver")).ExpoDriver
+        case "aurora-mysql":
+            return (await import("./aurora-mysql/AuroraMysqlDriver"))
+                .AuroraMysqlDriver
+        case "aurora-postgres":
+            return (await import("./aurora-postgres/AuroraPostgresDriver"))
+                .AuroraPostgresDriver
+        case "capacitor":
+            return (await import("./capacitor/CapacitorDriver")).CapacitorDriver
+        case "spanner":
+            return (await import("./spanner/SpannerDriver")).SpannerDriver
+        default:
+            const { MissingDriverError } = await import(
+                "../error/MissingDriverError"
+            )
+            throw new MissingDriverError(type, [
+                "aurora-mysql",
+                "aurora-postgres",
+                "better-sqlite3",
+                "capacitor",
+                "cockroachdb",
+                "cordova",
+                "expo",
+                "mariadb",
+                "mongodb",
+                "mssql",
+                "mysql",
+                "nativescript",
+                "oracle",
+                "postgres",
+                "react-native",
+                "sap",
+                "sqlite",
+                "sqljs",
+                "spanner",
+            ])
+    }
+}
+
 /**
  * Helps to create drivers.
  */
@@ -8,80 +83,8 @@ export class DriverFactory {
     /**
      * Creates a new driver depend on a given connection's driver type.
      */
-    create(connection: DataSource): Driver {
+    static async create(connection: DataSource): Promise<Driver> {
         const { type } = connection.options
-        return new (this.getDriver(type))(connection)
-    }
-
-    private getDriver(type: DataSource["options"]["type"]): DriverConstructor {
-        switch (type) {
-            case "mysql":
-            case "mariadb":
-                return require("./mysql/MysqlDriver").MysqlDriver
-            case "postgres":
-                return require("./postgres/PostgresDriver").PostgresDriver
-            case "cockroachdb":
-                return require("./cockroachdb/CockroachDriver").CockroachDriver
-            case "sap":
-                return require("./sap/SapDriver").SapDriver
-            case "sqlite":
-                return require("./sqlite/SqliteDriver").SqliteDriver
-            case "better-sqlite3":
-                return require("./better-sqlite3/BetterSqlite3Driver")
-                    .BetterSqlite3Driver
-            case "cordova":
-                return require("./cordova/CordovaDriver").CordovaDriver
-            case "nativescript":
-                return require("./nativescript/NativescriptDriver")
-                    .NativescriptDriver
-            case "react-native":
-                return require("./react-native/ReactNativeDriver")
-                    .ReactNativeDriver
-            case "sqljs":
-                return require("./sqljs/SqljsDriver").SqljsDriver
-            case "oracle":
-                return require("./oracle/OracleDriver").OracleDriver
-            case "mssql":
-                return require("./sqlserver/SqlServerDriver").SqlServerDriver
-            case "mongodb":
-                return require("./mongodb/MongoDriver").MongoDriver
-            case "expo":
-                return require("./expo/ExpoDriver").ExpoDriver
-            case "aurora-mysql":
-                return require("./aurora-mysql/AuroraMysqlDriver")
-                    .AuroraMysqlDriver
-            case "aurora-postgres":
-                return require("./aurora-postgres/AuroraPostgresDriver")
-                    .AuroraPostgresDriver
-            case "capacitor":
-                return require("./capacitor/CapacitorDriver").CapacitorDriver
-            case "spanner":
-                return require("./spanner/SpannerDriver").SpannerDriver
-            default:
-                const {
-                    MissingDriverError,
-                } = require("../error/MissingDriverError")
-                throw new MissingDriverError(type, [
-                    "aurora-mysql",
-                    "aurora-postgres",
-                    "better-sqlite3",
-                    "capacitor",
-                    "cockroachdb",
-                    "cordova",
-                    "expo",
-                    "mariadb",
-                    "mongodb",
-                    "mssql",
-                    "mysql",
-                    "nativescript",
-                    "oracle",
-                    "postgres",
-                    "react-native",
-                    "sap",
-                    "sqlite",
-                    "sqljs",
-                    "spanner",
-                ])
-        }
+        return new (await getDriver(type))(connection)
     }
 }
