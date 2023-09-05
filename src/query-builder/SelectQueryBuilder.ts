2bae6aef00e350a0b99ed28a7b21ee85253e9ac1
ales albert KILBERGR <ales.albert@kilbergr.eu>
2023-09-05 17:59:49 +0200

    fix: serialize bigint when building a query id

--- src/query-builder/SelectQueryBuilder.ts
+++ src/query-builder/SelectQueryBuilder.ts
@@ -3743,7 +3743,10 @@ export class SelectQueryBuilder<Entity extends ObjectLiteral>
      */
     protected async loadRawResults(queryRunner: QueryRunner) {
         const [sql, parameters] = this.getQueryAndParameters()
-        const queryId = sql + " -- PARAMETERS: " + JSON.stringify(parameters)
+        const queryId = sql + " -- PARAMETERS: " + JSON.stringify(
+            parameters,
+            (_, value) => typeof value === "bigint" ? value.toString() : value
+        )
         const cacheOptions =
             typeof this.connection.options.cache === "object"
                 ? this.connection.options.cache
