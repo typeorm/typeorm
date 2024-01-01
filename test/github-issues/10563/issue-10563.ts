dce2b03819cf6e19807d796fce9e8153a82f2db1
alenap93 <ilpugliese93@gmail.com>
2024-01-01 18:59:26 +0100

    fix: test 10563 multiple foreach

--- test/github-issues/10563/issue-10563.ts
+++ test/github-issues/10563/issue-10563.ts
@@ -8,36 +8,33 @@ import { DataSource } from "../../../src/data-source/DataSource"
 import { assert } from "chai"
 import { Dog } from "./entity/family"
 
-describe("github issues > #10653 Default value in child table/entity column decorator for multiple table inheritance is ignored for inherited columns", () => {
+describe.only("github issues > #10653 Default value in child table/entity column decorator for multiple table inheritance is ignored for inherited columns", () => {
     let dataSources: DataSource[]
     before(
         async () =>
-            (dataSources = await createTestingConnections({
-                entities: [__dirname + "/entity/*{.js,.ts}"],
-                schemaCreate: true,
-                dropSchema: true,
-            })),
+        (dataSources = await createTestingConnections({
+            entities: [__dirname + "/entity/*{.js,.ts}"],
+            schemaCreate: true,
+            dropSchema: true,
+            logging: true,
+        })),
     )
     beforeEach(() => reloadTestingDatabases(dataSources))
     after(() => closeTestingConnections(dataSources))
 
-    it("should honor distinct default value configured on inherited column of child entity", () =>
-        Promise.all(
+    it("should honor distinct default value configured on inherited column of child entity", async () =>
+        await Promise.all(
             dataSources.map(async (dataSource) => {
-                await Promise.all(
-                    dataSources.map(async (dataSource) => {
-                        const manager = dataSource.manager
-                        let dog: Dog = new Dog()
-                        dog.name = "Fifi"
-                        await manager.save(dog)
-                        let fifi = await manager.findOneBy(Dog, {
-                            name: "Fifi",
-                        })
-                        assert(
-                            fifi instanceof Dog && fifi["type"] == "PET",
-                            `Fifi=${JSON.stringify(fifi)}`,
-                        )
-                    }),
+                const manager = dataSource.manager
+                let dog: Dog = new Dog()
+                dog.name = "Fifi"
+                await manager.save(dog)
+                let fifi = await manager.findOneBy(Dog, {
+                    name: "Fifi",
+                })
+                assert(
+                    fifi instanceof Dog && fifi["type"] == "PET",
+                    `Fifi=${JSON.stringify(fifi)}`,
                 )
             }),
         ))
