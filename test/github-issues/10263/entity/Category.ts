6b7c02d077897c1f482533dd2c56f057466ff8df
Andreas Mattes <andreas.mattes@bab.gv.at>
2024-02-28 10:46:54 +0100

    feat: Postgres @Tree specify schema for closure tables #10263
    Adds the option 'closureTableSchema' to define the schema of the closure table.
    
    closes: #10263

---
+++ test/github-issues/10263/entity/Category.ts
@@ -0,0 +1,27 @@
+import { PrimaryGeneratedColumn } from "../../../../src/decorator/columns/PrimaryGeneratedColumn"
+import { Column } from "../../../../src/decorator/columns/Column"
+import { TreeParent } from "../../../../src/decorator/tree/TreeParent"
+import { TreeChildren } from "../../../../src/decorator/tree/TreeChildren"
+import { Entity } from "../../../../src/decorator/entity/Entity"
+import { Tree } from "../../../../src/decorator/tree/Tree"
+
+@Entity({ schema: "test_schema" })
+@Tree("closure-table", {
+    closureTableName: "category_xyz",
+    closureTableSchema: "test_schema",
+    ancestorColumnName: (column) => "ancestor_xyz_" + column.propertyName,
+    descendantColumnName: (column) => "descendant_xyz_" + column.propertyName,
+})
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @TreeParent()
+    parentCategory: Category
+
+    @TreeChildren({ cascade: true })
+    childCategories: Category[]
+}
