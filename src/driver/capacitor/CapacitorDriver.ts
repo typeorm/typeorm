typeorm/typeorm

commit 080528b11716cc786b00c4890160f6ccf3375925

fix: resolve circular dependency when using Vite (#10273)
* resolve circular dependency when using vite

src/driver/capacitor/CapacitorDriver.ts
@@ -80,7 +80,7 @@ export class CapacitorDriver extends AbstractSqliteDriver {
 
         // we need to enable foreign keys in sqlite to make sure all foreign key related features
         // working properly. this also makes onDelete to work with sqlite.
-        await connection.query(`PRAGMA foreign_keys = ON`)
+        await connection.run(`PRAGMA foreign_keys = ON`)
 
         if (
             this.options.journalMode &&
@@ -88,7 +88,7 @@ export class CapacitorDriver extends AbstractSqliteDriver {
                 this.options.journalMode,
             ) !== -1
         ) {
-            await connection.query(
+            await connection.run(
                 `PRAGMA journal_mode = ${this.options.journalMode}`,
             )
         }

src/driver/capacitor/CapacitorQueryRunner.ts
@@ -62,7 +62,10 @@ export class CapacitorQueryRunner extends AbstractSqliteQueryRunner {
 
         this.driver.connection.logger.logQuery(query, parameters, this)
 
-        const command = query.substr(0, query.indexOf(" "))
+        const command = query.substring(
+            0,
+            query.indexOf(" ") !== -1 ? query.indexOf(" ") : undefined,
+        )
 
         try {
             let raw: any
@@ -78,7 +81,9 @@ export class CapacitorQueryRunner extends AbstractSqliteQueryRunner {
                 ].indexOf(command) !== -1
             ) {
                 raw = await databaseConnection.execute(query, false)
-            } else if (["INSERT", "UPDATE", "DELETE"].indexOf(command) !== -1) {
+            } else if (
+                ["INSERT", "UPDATE", "DELETE", "PRAGMA"].indexOf(command) !== -1
+            ) {
                 raw = await databaseConnection.run(query, parameters, false)
             } else {
                 raw = await databaseConnection.query(query, parameters || [])

src/data-source/DataSource.ts
@@ -1,4 +1,5 @@
 import { Driver } from "../driver/Driver"
+import { registerQueryBuilders } from "../query-builder"
 import { Repository } from "../repository/Repository"
 import { EntitySubscriberInterface } from "../subscriber/EntitySubscriberInterface"
 import { EntityTarget } from "../common/EntityTarget"
@@ -40,7 +41,6 @@ import { RelationIdLoader } from "../query-builder/RelationIdLoader"
 import { DriverUtils } from "../driver/DriverUtils"
 import { InstanceChecker } from "../util/InstanceChecker"
 import { ObjectLiteral } from "../common/ObjectLiteral"
-import { registerQueryBuilders } from "../query-builder"
 
 registerQueryBuilders()
 
@@ -139,6 +139,7 @@ export class DataSource {
     // -------------------------------------------------------------------------
 
     constructor(options: DataSourceOptions) {
+        registerQueryBuilders()
         this.name = options.name || "default"
         this.options = options
         this.logger = new LoggerFactory().create(

src/platform/BrowserPlatformTools.template
@@ -7,6 +7,8 @@ import { Buffer } from "buffer";
  * For node.js environment this class is not getting packaged.
  * Don't use methods of this class in the code, use PlatformTools methods instead.
  */
+ import { Buffer } from "buffer";
+
 export class PlatformTools {
 
     /**

src/query-builder/QueryBuilder.ts
@@ -117,6 +117,10 @@ export abstract class QueryBuilder<Entity extends ObjectLiteral> {
         }
     }
 
+    static registerQueryBuilderClass(name: string, factory: any) {
+        QueryBuilder.queryBuilderRegistry[name] = factory
+    }
+
     // -------------------------------------------------------------------------
     // Abstract Methods
     // -------------------------------------------------------------------------
@@ -1671,9 +1675,5 @@ export abstract class QueryBuilder<Entity extends ObjectLiteral> {
 
     protected hasCommonTableExpressions(): boolean {
         return this.expressionMap.commonTableExpressions.length > 0
-    }
-
-    static registerQueryBuilderClass(name: string, factory: any) {
-        QueryBuilder.queryBuilderRegistry[name] = factory
     }
 }

commit 66749ba1fffd780ef7004c06d18bdebd6bb6f924

#10687 Fixing regression caused by #10273 by reverting back to .run() instead of .query() for all PRAGMA calls.

src/driver/capacitor/CapacitorDriver.ts
@@ -80,7 +80,7 @@ export class CapacitorDriver extends AbstractSqliteDriver {
 
         // we need to enable foreign keys in sqlite to make sure all foreign key related features
         // working properly. this also makes onDelete to work with sqlite.
-        await connection.run(`PRAGMA foreign_keys = ON`)
+        await connection.query(`PRAGMA foreign_keys = ON`)
 
         if (
             this.options.journalMode &&
@@ -88,7 +88,7 @@ export class CapacitorDriver extends AbstractSqliteDriver {
                 this.options.journalMode,
             ) !== -1
         ) {
-            await connection.run(
+            await connection.query(
                 `PRAGMA journal_mode = ${this.options.journalMode}`,
             )
         }

src/driver/capacitor/CapacitorQueryRunner.ts
@@ -82,7 +82,7 @@ export class CapacitorQueryRunner extends AbstractSqliteQueryRunner {
             ) {
                 raw = await databaseConnection.execute(query, false)
             } else if (
-                ["INSERT", "UPDATE", "DELETE", "PRAGMA"].indexOf(command) !== -1
+                ["INSERT", "UPDATE", "DELETE"].indexOf(command) !== -1
             ) {
                 raw = await databaseConnection.run(query, parameters, false)
             } else {