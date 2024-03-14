9322f866eab9323a0e0a202dbf14649700366b4b
Tolga Paksoy <tolga@paramander.com>
2024-03-14 22:43:23 +0100

    fix: virtual columns not being selected correctly
    This reverts commit b53e410e5abe930ab489ff4c8c16f62306910f6a.

--- src/query-builder/SelectQueryBuilder.ts
+++ src/query-builder/SelectQueryBuilder.ts
@@ -2882,11 +2882,6 @@ export class SelectQueryBuilder<Entity extends ObjectLiteral>
                     })
                 })
             } else {
-                if (column.isVirtualProperty) {
-                    // Do not add unselected virtual properties to final select
-                    return
-                }
-
                 finalSelects.push({
                     selection: selectionPath,
                     aliasName: DriverUtils.buildAlias(
