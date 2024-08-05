f64170e27ac7c378f27a45ee587ed5994b36bd62
Mattias Siø Fjellvang <mattias@doubble.dk>
2024-08-05 17:03:31 +0200

    feat: don't complain if dataSource is passed in Migration Create command (to keep migration:* scope consistent)

--- src/commands/MigrationCreateCommand.ts
+++ src/commands/MigrationCreateCommand.ts
@@ -15,6 +15,14 @@ export class MigrationCreateCommand implements yargs.CommandModule {
 
     builder(args: yargs.Argv) {
         return new DefaultCliArgumentsBuilder(args)
+            .addDataSourceOption({
+                // This command is the only one that does not use a data source
+                // However, it's added here to create a similar experience to the
+                // rest of the migration commands, so that the user will not be
+                // interrupted should they add a data source option by mistake.
+                // it enhances the developer experience.
+                demandOption: false,
+            })
             .builder()
             .positional("path", {
                 type: "string",
