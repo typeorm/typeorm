b7295a0263eda242eeb6f24f144bb743324df732
Arthur Frederico Neves <arthurfred.neves@gmail.com>
2024-11-15 02:38:39 -0400

    [UPDATE]: change test to execute databases in order

--- test/github-issues/9006/issue-9006.ts
+++ test/github-issues/9006/issue-9006.ts
@@ -14,9 +14,6 @@ import { Comment } from "./entity/Comment"
 
 describe("github issues > #9006 Eager relations do not respect relationLoadStrategy", () => {
     let dataSources: DataSource[]
-    let getManySpy: sinon.SinonSpy
-    let getOneSpy: sinon.SinonSpy
-    let getRawManySpy: sinon.SinonSpy
 
     before(async () => {
         dataSources = await createTestingConnections({
@@ -28,173 +25,172 @@ describe("github issues > #9006 Eager relations do not respect relationLoadStrat
     beforeEach(() => reloadTestingDatabases(dataSources))
     after(() => closeTestingConnections(dataSources))
 
-    it("it should load eager relations using relationLoadStrategy as main strategy", () =>
-        Promise.all(
-            dataSources.map(async (dataSource) => {
-                const authorRepository = dataSource.getRepository(Author)
-                const bookRepository = dataSource.getRepository(Book)
-                const commentRepository = dataSource.getRepository(Comment)
+    it("it should load eager relations using relationLoadStrategy as main strategy", async () => {
+        for (const dataSource of dataSources) {
+            const authorRepository = dataSource.getRepository(Author)
+            const bookRepository = dataSource.getRepository(Book)
+            const commentRepository = dataSource.getRepository(Comment)
 
-                const authorModel = await authorRepository.create({
-                    name: "author",
-                })
-                const author = await authorRepository.save(authorModel)
+            const authorModel = await authorRepository.create({
+                name: "author",
+            })
+            const author = await authorRepository.save(authorModel)
 
-                const bookModels = await bookRepository.create([
+            const bookModels = await bookRepository.create([
+                {
+                    title: "book1",
+                    text: "text1",
+                    author: [author],
+                },
+                {
+                    title: "book2",
+                    text: "text2",
+                    author: [author],
+                },
+                {
+                    title: "book3",
+                    text: "text3",
+                    author: [author],
+                },
+            ])
+            const books = await bookRepository.save(bookModels)
+
+            for (const book of books) {
+                for (let index = 0; index < 3; index++) {
+                    const comment = await commentRepository.create({
+                        text: `${book.title}: comment${index}`,
+                        bookId: book.id,
+                        authorId: author.id,
+                    })
+                    await commentRepository.save(comment)
+                }
+            }
+
+            const getManySpy = await sinon.spy(
+                SelectQueryBuilder.prototype,
+                "getMany",
+            )
+            const getOneSpy = await sinon.spy(
+                SelectQueryBuilder.prototype,
+                "getOne",
+            )
+            const getRawManySpy = await sinon.spy(
+                SelectQueryBuilder.prototype,
+                "getRawMany",
+            )
+
+            const eageredAuthor = await authorRepository.findOne({
+                where: { id: author.id },
+                relationLoadStrategy: "query",
+            })
+
+            const [manyToManyCall] = getManySpy.getCalls()
+
+            expect(
+                manyToManyCall.thisValue.expressionMap.joinAttributes,
+            ).to.be.an("array")
+            expect(
+                manyToManyCall.thisValue.expressionMap.joinAttributes,
+            ).to.have.length(1) // Only join attribute allowed, in many to many relations, to be more performatic
+
+            getOneSpy.getCalls().forEach((call) => {
+                expect(call.thisValue.expressionMap.joinAttributes).to.be.an(
+                    "array",
+                )
+                expect(
+                    call.thisValue.expressionMap.joinAttributes,
+                ).to.have.length(0) // No Join Attributes in relationLoadStrategy: "query",
+            })
+
+            expect(getRawManySpy.callCount).to.be.equal(1)
+            expect(getManySpy.callCount).to.be.equal(2)
+            expect(getOneSpy.callCount).to.be.equal(1)
+
+            expect(eageredAuthor).to.deep.equal({
+                id: 1,
+                name: "author",
+                books: [
                     {
+                        id: 1,
                         title: "book1",
                         text: "text1",
-                        author: [author],
+                        comments: [
+                            {
+                                id: 1,
+                                text: "book1: comment0",
+                                bookId: 1,
+                                authorId: 1,
+                            },
+                            {
+                                id: 2,
+                                text: "book1: comment1",
+                                bookId: 1,
+                                authorId: 1,
+                            },
+                            {
+                                id: 3,
+                                text: "book1: comment2",
+                                bookId: 1,
+                                authorId: 1,
+                            },
+                        ],
                     },
                     {
+                        id: 2,
                         title: "book2",
                         text: "text2",
-                        author: [author],
+                        comments: [
+                            {
+                                id: 4,
+                                text: "book2: comment0",
+                                bookId: 2,
+                                authorId: 1,
+                            },
+                            {
+                                id: 5,
+                                text: "book2: comment1",
+                                bookId: 2,
+                                authorId: 1,
+                            },
+                            {
+                                id: 6,
+                                text: "book2: comment2",
+                                bookId: 2,
+                                authorId: 1,
+                            },
+                        ],
                     },
                     {
+                        id: 3,
                         title: "book3",
                         text: "text3",
-                        author: [author],
+                        comments: [
+                            {
+                                id: 7,
+                                text: "book3: comment0",
+                                bookId: 3,
+                                authorId: 1,
+                            },
+                            {
+                                id: 8,
+                                text: "book3: comment1",
+                                bookId: 3,
+                                authorId: 1,
+                            },
+                            {
+                                id: 9,
+                                text: "book3: comment2",
+                                bookId: 3,
+                                authorId: 1,
+                            },
+                        ],
                     },
-                ])
-                const books = await bookRepository.save(bookModels)
+                ],
+            })
 
-                for (const book of books) {
-                    for (let index = 0; index < 3; index++) {
-                        const comment = await commentRepository.create({
-                            text: `${book.title}: comment${index}`,
-                            bookId: book.id,
-                            authorId: author.id,
-                        })
-                        await commentRepository.save(comment)
-                    }
-                }
-
-                getManySpy = await sinon.spy(
-                    SelectQueryBuilder.prototype,
-                    "getMany",
-                )
-                getOneSpy = await sinon.spy(
-                    SelectQueryBuilder.prototype,
-                    "getOne",
-                )
-                getRawManySpy = await sinon.spy(
-                    SelectQueryBuilder.prototype,
-                    "getRawMany",
-                )
-
-                const eageredAuthor = await authorRepository.findOne({
-                    where: { id: author.id },
-                    relationLoadStrategy: "query",
-                })
-
-                const [manyToManyCall] = getManySpy.getCalls()
-
-                expect(
-                    manyToManyCall.thisValue.expressionMap.joinAttributes,
-                ).to.be.an("array")
-                expect(
-                    manyToManyCall.thisValue.expressionMap.joinAttributes,
-                ).to.have.length(1) // Only join attribute allowed, in many to many relations, to be more performatic
-
-                getOneSpy.getCalls().forEach((call) => {
-                    expect(
-                        call.thisValue.expressionMap.joinAttributes,
-                    ).to.be.an("array")
-                    expect(
-                        call.thisValue.expressionMap.joinAttributes,
-                    ).to.have.length(0) // No Join Attributes in relationLoadStrategy: "query",
-                })
-
-                expect(getRawManySpy.callCount).to.be.equal(1)
-                expect(getManySpy.callCount).to.be.equal(2)
-                expect(getOneSpy.callCount).to.be.equal(1)
-
-                expect(eageredAuthor).to.deep.equal({
-                    id: 1,
-                    name: "author",
-                    books: [
-                        {
-                            id: 1,
-                            title: "book1",
-                            text: "text1",
-                            comments: [
-                                {
-                                    id: 1,
-                                    text: "book1: comment0",
-                                    bookId: 1,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 2,
-                                    text: "book1: comment1",
-                                    bookId: 1,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 3,
-                                    text: "book1: comment2",
-                                    bookId: 1,
-                                    authorId: 1,
-                                },
-                            ],
-                        },
-                        {
-                            id: 2,
-                            title: "book2",
-                            text: "text2",
-                            comments: [
-                                {
-                                    id: 4,
-                                    text: "book2: comment0",
-                                    bookId: 2,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 5,
-                                    text: "book2: comment1",
-                                    bookId: 2,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 6,
-                                    text: "book2: comment2",
-                                    bookId: 2,
-                                    authorId: 1,
-                                },
-                            ],
-                        },
-                        {
-                            id: 3,
-                            title: "book3",
-                            text: "text3",
-                            comments: [
-                                {
-                                    id: 7,
-                                    text: "book3: comment0",
-                                    bookId: 3,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 8,
-                                    text: "book3: comment1",
-                                    bookId: 3,
-                                    authorId: 1,
-                                },
-                                {
-                                    id: 9,
-                                    text: "book3: comment2",
-                                    bookId: 3,
-                                    authorId: 1,
-                                },
-                            ],
-                        },
-                    ],
-                })
-
-                getManySpy.restore()
-                getOneSpy.restore()
-                getRawManySpy.restore()
-            }),
-        ))
+            getManySpy.restore()
+            getOneSpy.restore()
+            getRawManySpy.restore()
+        }
+    })
 })
