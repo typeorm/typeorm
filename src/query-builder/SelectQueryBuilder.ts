c416faecf8b4b86135c56243b86f1fbfcbf8c0d6
Gil Tichon <gil.tichon@lemonade.com>
2024-12-10 15:37:30 +0200

    Remove unused import

--- src/query-builder/SelectQueryBuilder.ts
+++ src/query-builder/SelectQueryBuilder.ts
@@ -43,7 +43,6 @@ import { OrmUtils } from "../util/OrmUtils"
 import { EntityPropertyNotFoundError } from "../error/EntityPropertyNotFoundError"
 import { AuroraMysqlDriver } from "../driver/aurora-mysql/AuroraMysqlDriver"
 import { InstanceChecker } from "../util/InstanceChecker"
-import { FindOperator } from "../find-options/FindOperator"
 import { ApplyValueTransformers } from "../util/ApplyValueTransformers"
 
 /**
