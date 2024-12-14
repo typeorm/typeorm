b36fbd3c4bf8267ef47ce912c7ac7f73812f5b7f
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-12-14 18:19:06 +0500

    apply formatting

--- test/functional/repository/find-options-locking/find-options-locking.ts
+++ test/functional/repository/find-options-locking/find-options-locking.ts
@@ -638,14 +638,17 @@ describe("repository > find options > locking", () => {
                                     tables: ["post"],
                                 },
                             }),
-                            entityManager.getRepository(Post).findOne({
-                                where: { id: 1 },
-                                relations: { author: true },
-                                lock: { mode: "pessimistic_write" },
-                            })
-                            .should.be.rejectedWith(
-                                "FOR UPDATE cannot be applied to the nullable side of an outer join",
-                            ),,
+                            entityManager
+                                .getRepository(Post)
+                                .findOne({
+                                    where: { id: 1 },
+                                    relations: { author: true },
+                                    lock: { mode: "pessimistic_write" },
+                                })
+                                .should.be.rejectedWith(
+                                    "FOR UPDATE cannot be applied to the nullable side of an outer join",
+                                ),
+                            ,
                         ])
                     })
                 }
