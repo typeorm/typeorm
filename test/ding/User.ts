82911f89118d01aeccf838ddd6f69762e13f01f7
cx01 <hd@cxehr.com>
2025-03-12 16:45:38 +0800

    test: improve

--- test/ding/User.ts
+++ test/ding/User.ts
@@ -2,20 +2,20 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "../../src"
 
 @Entity({ comment: "用户表" })
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column({ comment: "用户名" })
-    name: string
+  @Column({ type: String, length: 80, comment: "用户名" })
+  name: string
 
-    @Column({ type: "simple-json", comment: "JSON数据", nullable: true })
-    json: Record<string, any> | null
+  @Column({ type: "simple-json", comment: "JSON数据", nullable: true })
+  json: Record<string, any> | null
 
-    @Column({
-        type: "bool",
-        comment: "是否在职",
-        nullable: true,
-        default: false,
-    })
-    onJob: boolean
+  @Column({
+    type: "bool",
+    comment: "是否在职",
+    nullable: true,
+    default: false,
+  })
+  onJob: boolean
 }
