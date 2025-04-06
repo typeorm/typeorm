5738e8211ccf577ded16f5a0280bb2842a87ae10
Naor Peled <thehecticbyte@gmail.com>
2025-04-07 01:16:57 +0300

    chore(new-docs): move to docs folder

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        "https://github.com/typeorm/typeorm/tree/master/new_doc/docs/",
+                        "https://github.com/typeorm/typeorm/tree/master/docs/docs/",
                 },
                 theme: {
                     customCss: "./src/css/custom.css",
