5d66c2b3d51c2b8bc41c69fd414af46130ae2558
Gil Tichon <gil.tichon@lemonade.com>
2025-04-08 14:04:43 +0300

    format

--- test/github-issues/10397/entity/test.entity.ts
+++ test/github-issues/10397/entity/test.entity.ts
@@ -7,23 +7,27 @@ export interface Pair {
 
 export class PairTransformer {
     to(value: Pair[]): string[] {
-        return value.map(pair => `${pair.key}:${pair.value}`)
+        return value.map((pair) => `${pair.key}:${pair.value}`)
     }
 
     from(value: string[]): Pair[] {
-        return value.map(pair => {
+        return value.map((pair) => {
             const [key, value] = pair.split(":")
             return { key, value }
         })
     }
 }
 
-
 @Entity()
 export class TestEntity {
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column({type: "varchar", array: true, default: [], transformer: new PairTransformer()})
+    @Column({
+        type: "varchar",
+        array: true,
+        default: [],
+        transformer: new PairTransformer(),
+    })
     pairs: Pair[]
 }
