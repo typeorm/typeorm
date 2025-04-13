81c0fc4937e6769c9935e52e0f0814a42165270e
Naor Peled <thehecticbyte@gmail.com>
2025-04-13 19:02:10 +0300

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config([
     },
     {
         files: ["**/*.ts"],
+        ignores: ["docs/**"],
         languageOptions: {
             parser: tseslint.parser,
             parserOptions: {
