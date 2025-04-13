7e04d3fa0c28eb7dd56a66954d185d5bfb3cce6a
Jovana Đurić <63621526+jovanadjuric@users.noreply.github.com>
2025-04-13 23:52:30 +0200

    feat(new-docs): add redirects to support legacy paths (#11410)

---
+++ docs/redirects.ts
@@ -0,0 +1,58 @@
+export const redirects = [
+  { from: '/entities', to: '/docs/Entity/entities' },
+  { from: '/embedded-entities', to: '/docs/Entity/embedded-entities' },
+  { from: '/entity-inheritance', to: '/docs/Entity/entity-inheritance' },
+  { from: '/tree-entities', to: '/docs/Entity/tree-entities' },
+  { from: '/view-entities', to: '/docs/Entity/view-entities' },
+  { from: '/separating-entity-definition', to: '/docs/Entity/separating-entity-definition' },
+
+  { from: '/relations', to: '/docs/Relations/relations' },
+  { from: '/one-to-one-relations', to: '/docs/Relations/one-to-one-relations' },
+  { from: '/many-to-one-one-to-many-relations', to: '/docs/Relations/many-to-one-one-to-many-relations' },
+  { from: '/many-to-many-relations', to: '/docs/Relations/many-to-many-relations' },
+  { from: '/eager-and-lazy-relations', to: '/docs/Relations/eager-and-lazy-relations' },
+  { from: '/relations-faq', to: '/docs/Relations/relations-faq' },
+
+  { from: '/working-with-entity-manager', to: '/docs/Working with Entity Manager/working-with-entity-manager' },
+  { from: '/working-with-repository', to: '/docs/Working with Entity Manager/working-with-repository' },
+  { from: '/find-options', to: '/docs/Working with Entity Manager/find-options' },
+  { from: '/custom-repository', to: '/docs/Working with Entity Manager/custom-repository' },
+  { from: '/entity-manager-api', to: '/docs/Working with Entity Manager/entity-manager-api' },
+  { from: '/repository-api', to: '/docs/Working with Entity Manager/repository-api' },
+
+  { from: '/select-query-builder', to: '/docs/Query Builder/select-query-builder' },
+  { from: '/insert-query-builder', to: '/docs/Query Builder/insert-query-builder' },
+  { from: '/update-query-builder', to: '/docs/Query Builder/update-query-builder' },
+  { from: '/delete-query-builder', to: '/docs/Query Builder/delete-query-builder' },
+  { from: '/relational-query-builder', to: '/docs/Query Builder/relational-query-builder' },
+  { from: '/caching', to: '/docs/Query Builder/caching' },
+
+  { from: '/active-record-data-mapper', to: '/docs/Guides/active-record-data-mapper' },
+  { from: '/mongodb', to: '/docs/Guides/mongodb' },
+  { from: '/validation', to: '/docs/Guides/validation' },
+  { from: '/example-with-express', to: '/docs/Guides/example-with-express' },
+  { from: '/usage-with-javascript', to: '/docs/Guides/usage-with-javascript' },
+  { from: '/sequelize-migration', to: '/docs/Guides/sequelize-migration' },
+
+  { from: '/data-source', to: '/docs/Data Source/data-source' },
+  { from: '/data-source-options', to: '/docs/Data Source/data-source-options' },
+  { from: '/multiple-data-sources', to: '/docs/Data Source/multiple-data-sources' },
+  { from: '/data-source-api', to: '/docs/Data Source/data-source-api' },
+
+  { from: '/migrations', to: '/docs/Advanced Topics/migrations' },
+  { from: '/transactions', to: '/docs/Advanced Topics/transactions' },
+  { from: '/indices', to: '/docs/Advanced Topics/indices' },
+  { from: '/listeners-and-subscribers', to: '/docs/Advanced Topics/listeners-and-subscribers' },
+  { from: '/logging', to: '/docs/Advanced Topics/logging' },
+  { from: '/using-cli', to: '/docs/Advanced Topics/using-cli' },
+  { from: '/performance-optimizing', to: '/docs/Advanced Topics/performance-optimizing' },
+
+  { from: '/faq', to: '/docs/Help/faq' },
+  { from: '/supported-platforms', to: '/docs/Help/supported-platforms' },
+  { from: '/decorator-reference', to: '/docs/Help/decorator-reference' },
+  { from: '/support', to: '/docs/Help/support' },
+
+  { from: '/getting-started', to: '/docs/Getting Started' },
+  { from: '/future-of-typeorm', to: '/docs/future-of-typeorm' },
+  { from: '/query-runner', to: '/docs/query-runner' },
+];
