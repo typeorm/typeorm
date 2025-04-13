57377c12b41c84840605e8df3a652e014df69735
jovanadjuric <mat.jovana@gmail.com>
2025-04-13 23:47:05 +0200

    feat(new-docs): add redirects to support legacy paths

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer"
 import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
+import { redirects } from "./redirects"
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -167,6 +168,14 @@ const config: Config = {
             contextualSearch: true,
         },
     } satisfies Preset.ThemeConfig,
+    plugins: [
+        [
+            "@docusaurus/plugin-client-redirects",
+            {
+                redirects,
+            },
+        ],
+    ],
 }
 
 export default config
