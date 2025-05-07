aa66f94779e245b76f86e644d8d157db1119afe1
mmarifat <mma.rifat66@gmail.com>
2025-05-07 21:08:24 +0100

    fix: resolve regression in upsert and orUpdate for PostgreSQL driver
    This fix addresses a regression introduced in
    pull request #11082 by ensuring correct handling of alias names and table names when they are equal or distinct when entities use schema
    
    Closes: #11440

---
+++ test/github-issues/11440/issue-11440.ts
@@ -0,0 +1,105 @@
+import { expect } from "chai"
+import "reflect-metadata";
+
+import { DataSource } from "../../../src"
+import {
+    closeTestingConnections,
+    createTestingConnections,
+    reloadTestingDatabases,
+} from "../../utils/test-utils"
+import { Post } from "./entity/Post"
+
+describe("github issues > #11440", () => {
+    let dataSource: DataSource
+    before(async () => {
+        [dataSource] = await createTestingConnections({
+            entities: [Post],
+            enabledDrivers: ["postgres"], // since the issue is for postgres only
+        })
+    })
+    beforeEach(() => reloadTestingDatabases([dataSource]))
+    after(() => closeTestingConnections([dataSource]))
+
+    it("should use table or alias name during upsert or doUpdate when both schema name and skipUpdateIfNoValuesChanged supplied", async () => {
+        const repository = dataSource.getRepository(Post)
+        await repository.save([
+            {
+                id: 1,
+                title: "First Post",
+            },
+            {
+                id: 2,
+                title: "Second Post",
+            },
+            {
+                id: 3,
+                title: "Third Post",
+            },
+        ])
+
+        // upsert does not cast alias as class name
+        await repository.upsert(
+            [
+                {
+                    id: 1,
+                    title: "First Post",
+                },
+                {
+                    id: 2,
+                    title: "Second Post UPSERTED",
+                },
+            ],
+            {
+                conflictPaths: ["id"],
+                upsertType: "on-conflict-do-update",
+                skipUpdateIfNoValuesChanged: true,
+            },
+        )
+
+        const query = repository
+            .createQueryBuilder()
+            .insert()
+            .values([
+                {
+                    id: 1,
+                    title: "First Post",
+                },
+                {
+                    id: 3,
+                    title: "Third Post OR_UPDATED",
+                },
+            ])
+            .orUpdate(["title"], ["id"], {
+                skipUpdateIfNoValuesChanged: true,
+            })
+
+        // orUpdate cast alias as class name
+        expect(query.getSql()).to.equal(
+            `INSERT INTO "typeorm_test"."post" AS "Post"("id", "title") ` +
+                `VALUES ($1, $2), ($3, $4) ` +
+                `ON CONFLICT ( "id" ) DO UPDATE ` +
+                `SET "title" = EXCLUDED."title" ` +
+                `WHERE ("Post"."title" IS DISTINCT FROM EXCLUDED."title")`,
+        )
+        expect(await query.execute()).not.to.throw
+
+        const posts = await repository.find({
+            order: { id: "ASC" },
+        })
+
+        expect(posts).to.deep.equal([
+            {
+                id: 1,
+                title: "First Post",
+            },
+            {
+                id: 2,
+                title: "Second Post UPSERTED",
+            },
+            {
+                id: 3,
+                title: "Third Post OR_UPDATED",
+            },
+        ])
+    })
+})
