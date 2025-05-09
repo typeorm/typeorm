c1ed8a8fa7b8c81c46bbca9b7f8d7aeed305338b
Simon Garner <simon@equalogic.com>
2025-05-09 22:22:16 +1200

    Add test for Repository.delete() with criteria

--- test/functional/repository/delete-by-id-and-ids/repository-remove-by-id-and-ids.ts
+++ test/functional/repository/delete-by-id-and-ids/repository-remove-by-id-and-ids.ts
@@ -8,7 +8,7 @@ import {
     reloadTestingDatabases,
 } from "../../../utils/test-utils"
 
-describe("repository > deleteById methods", function () {
+describe("repository > delete methods", function () {
     // -------------------------------------------------------------------------
     // Configuration
     // -------------------------------------------------------------------------
@@ -27,7 +27,7 @@ describe("repository > deleteById methods", function () {
     // Specifications
     // -------------------------------------------------------------------------
 
-    it("remove using deleteById method should delete successfully", () =>
+    it("remove using delete method should delete successfully", () =>
         Promise.all(
             connections.map(async (connection) => {
                 const postRepository = connection.getRepository(Post)
@@ -62,7 +62,7 @@ describe("repository > deleteById methods", function () {
             }),
         ))
 
-    it("remove using removeByIds method should delete successfully", () =>
+    it("remove multiple rows using delete method should delete successfully", () =>
         Promise.all(
             connections.map(async (connection) => {
                 const postRepository = connection.getRepository(Post)
@@ -94,6 +94,45 @@ describe("repository > deleteById methods", function () {
                 expect(loadedPosts.find((p) => p.id === 2)).to.be.undefined
                 expect(loadedPosts.find((p) => p.id === 3)).to.be.undefined
                 expect(loadedPosts.find((p) => p.id === 4)).not.to.be.undefined
+            }),
+        ))
+
+    it("remove row using delete method with partial criteria should delete successfully", () =>
+        Promise.all(
+            connections.map(async (connection) => {
+                const postRepository = connection.getRepository(Post)
+
+                // save a new posts
+                const newPost1 = postRepository.create()
+                newPost1.title = "Super post #1"
+                const newPost2 = postRepository.create()
+                newPost2.title = "Super post #2"
+                const newPost3 = postRepository.create()
+                newPost3.title = "Super post #3"
+                const newPost4 = postRepository.create()
+                newPost4.title = "Super post #4"
+
+                await postRepository.save(newPost1)
+                await postRepository.save(newPost2)
+                await postRepository.save(newPost3)
+                await postRepository.save(newPost4)
+
+                // remove with criteria
+                await postRepository.delete({ title: "Super post #3" })
+
+                // load to check
+                const loadedPosts = await postRepository.find()
+
+                // assert
+                loadedPosts.length.should.be.equal(3)
+                expect(loadedPosts.find((p) => p.title === "Super post #1")).not
+                    .to.be.undefined
+                expect(loadedPosts.find((p) => p.title === "Super post #2")).not
+                    .to.be.undefined
+                expect(loadedPosts.find((p) => p.title === "Super post #3")).to
+                    .be.undefined
+                expect(loadedPosts.find((p) => p.title === "Super post #4")).not
+                    .to.be.undefined
             }),
         ))
 })
