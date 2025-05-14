4c14681302dd2d26ef2df2af4d3969557369cdb6
mmarifat <mma.rifat66@gmail.com>
2025-05-14 12:56:11 +0100

    chore: run tests by running through all the drivers

--- test/github-issues/11440/issue-11440.ts
+++ test/github-issues/11440/issue-11440.ts
@@ -10,96 +10,100 @@ import {
 import { Post } from "./entity/Post"
 
 describe("github issues > #11440", () => {
-    let dataSource: DataSource
+    let dataSources: DataSource[]
+
     before(async () => {
-        ;[dataSource] = await createTestingConnections({
+        dataSources = await createTestingConnections({
             entities: [Post],
-            enabledDrivers: ["postgres"], // since the issue is for postgres only
         })
     })
-    beforeEach(() => reloadTestingDatabases([dataSource]))
-    after(() => closeTestingConnections([dataSource]))
+    beforeEach(() => reloadTestingDatabases(dataSources))
+    after(() => closeTestingConnections(dataSources))
 
     it("should use table or alias name during upsert or doUpdate when both schema name and skipUpdateIfNoValuesChanged supplied", async () => {
-        const repository = dataSource.getRepository(Post)
-        await repository.save([
-            {
-                id: 1,
-                title: "First Post",
-            },
-            {
-                id: 2,
-                title: "Second Post",
-            },
-            {
-                id: 3,
-                title: "Third Post",
-            },
-        ])
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const repository = dataSource.getRepository(Post)
+                await repository.save([
+                    {
+                        id: 1,
+                        title: "First Post",
+                    },
+                    {
+                        id: 2,
+                        title: "Second Post",
+                    },
+                    {
+                        id: 3,
+                        title: "Third Post",
+                    },
+                ])
 
-        // upsert does not cast alias as class name
-        await repository.upsert(
-            [
-                {
-                    id: 1,
-                    title: "First Post",
-                },
-                {
-                    id: 2,
-                    title: "Second Post UPSERTED",
-                },
-            ],
-            {
-                conflictPaths: ["id"],
-                upsertType: "on-conflict-do-update",
-                skipUpdateIfNoValuesChanged: true,
-            },
+                // upsert does not cast alias as class name
+                await repository.upsert(
+                    [
+                        {
+                            id: 1,
+                            title: "First Post",
+                        },
+                        {
+                            id: 2,
+                            title: "Second Post UPSERTED",
+                        },
+                    ],
+                    {
+                        conflictPaths: ["id"],
+                        upsertType: "on-conflict-do-update",
+                        skipUpdateIfNoValuesChanged: true,
+                    },
+                )
+
+                const query = repository
+                    .createQueryBuilder()
+                    .insert()
+                    .values([
+                        {
+                            id: 1,
+                            title: "First Post",
+                        },
+                        {
+                            id: 3,
+                            title: "Third Post OR_UPDATED",
+                        },
+                    ])
+                    .orUpdate(["title"], ["id"], {
+                        skipUpdateIfNoValuesChanged: true,
+                    })
+
+                // orUpdate cast alias as class name
+                expect(query.getSql()).to.equal(
+                    `INSERT INTO "typeorm_test"."post" AS "Post"("id", "title") ` +
+                        `VALUES ($1, $2), ($3, $4) ` +
+                        `ON CONFLICT ( "id" ) DO UPDATE ` +
+                        `SET "title" = EXCLUDED."title" ` +
+                        `WHERE ("Post"."title" IS DISTINCT FROM EXCLUDED."title")`,
+                )
+                await query.execute()
+
+                const posts = await repository.find({
+                    order: { id: "ASC" },
+                })
+
+                expect(posts).to.deep.equal([
+                    {
+                        id: 1,
+                        title: "First Post",
+                    },
+                    {
+                        id: 2,
+                        title: "Second Post UPSERTED",
+                    },
+                    {
+                        id: 3,
+                        title: "Third Post OR_UPDATED",
+                    },
+                ])
+            }),
         )
-
-        const query = repository
-            .createQueryBuilder()
-            .insert()
-            .values([
-                {
-                    id: 1,
-                    title: "First Post",
-                },
-                {
-                    id: 3,
-                    title: "Third Post OR_UPDATED",
-                },
-            ])
-            .orUpdate(["title"], ["id"], {
-                skipUpdateIfNoValuesChanged: true,
-            })
-
-        // orUpdate cast alias as class name
-        expect(query.getSql()).to.equal(
-            `INSERT INTO "typeorm_test"."post" AS "Post"("id", "title") ` +
-                `VALUES ($1, $2), ($3, $4) ` +
-                `ON CONFLICT ( "id" ) DO UPDATE ` +
-                `SET "title" = EXCLUDED."title" ` +
-                `WHERE ("Post"."title" IS DISTINCT FROM EXCLUDED."title")`,
-        )
-        expect(await query.execute()).not.to.throw
-
-        const posts = await repository.find({
-            order: { id: "ASC" },
-        })
-
-        expect(posts).to.deep.equal([
-            {
-                id: 1,
-                title: "First Post",
-            },
-            {
-                id: 2,
-                title: "Second Post UPSERTED",
-            },
-            {
-                id: 3,
-                title: "Third Post OR_UPDATED",
-            },
-        ])
     })
 })
