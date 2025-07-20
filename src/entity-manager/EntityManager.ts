0b66267e0e1561a465ee5b3fe11c49c5b2a82774
Lyndsey Boonekamp <lboonekamp@temperzone.com>
2025-07-21 09:48:42 +1200

    feat: Add useStructuredResult and recordsets
    -Add useStructuredResult to EntityManager.query() and QueryRunner.query()

--- src/entity-manager/EntityManager.ts
+++ src/entity-manager/EntityManager.ts
@@ -174,8 +174,17 @@ export class EntityManager {
      *
      * @see [Official docs](https://typeorm.io/docs/Working%20with%20Entity%20Manager/entity-manager-api/) for examples.
      */
-    async query<T = any>(query: string, parameters?: any[]): Promise<T> {
-        return this.connection.query(query, parameters, this.queryRunner)
+    async query<T = any>(
+        query: string,
+        parameters?: any[],
+        useStructuredResult?: boolean,
+    ): Promise<T> {
+        return this.connection.query(
+            query,
+            parameters,
+            this.queryRunner,
+            useStructuredResult,
+        )
     }
 
     /**
