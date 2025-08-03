852f4e30d3c8cc4b0320939ded9cab0a0ceef11e
Eliya Cohen <co.eliya2@gmail.com>
2025-08-03 14:43:57 +0300

    feat: introduce OmitRelations type

--- src/common/RelationType.ts
+++ src/common/RelationType.ts
@@ -1,3 +1,5 @@
+declare const relationSymbol: unique symbol
+
 /**
  * Wrapper type for relation type definitions in entities.
  * Used to circumvent ESM modules circular dependency issue caused by reflection metadata saving the type of the property.
@@ -11,4 +13,31 @@
  *
  * }
  */
-export type Relation<T> = T
+export type Relation<T> = T & { [relationSymbol]?: true }
+
+type IsRelation<T> = T extends { [relationSymbol]?: true } ? true : false
+type GetEntityRelationKeys<T> = {
+    [K in keyof T]: IsRelation<T[K]> extends true ? K : never
+}[keyof T]
+
+/**
+ * Omit relations from an object.
+ * This is useful when you want to create a new object without the relations.
+ *
+ * Usage example:
+ *
+ * ```ts
+ * \@Entity()
+ * export default class User {
+ *   // ... other properties
+ *
+ *   \@OneToOne(() => Profile, profile => profile.user)
+ *   profile: Relation<Profile>;
+ *
+ *   constructor(user: OmitRelations<User>) {
+ *     Object.assign(this, user);
+ *   }
+ * }
+ * ```
+ */
+export type OmitRelations<T> = Omit<T, GetEntityRelationKeys<T>>
