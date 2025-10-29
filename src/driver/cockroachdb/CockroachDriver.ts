2b2bede07b4507ff83cc080518ff85ba9bc0b421
CHOIJEWON <alsrn6040@kakao.com>
2025-10-29 14:28:20 +0900

    feat: apply utc flag to date columns across all drivers

--- src/driver/cockroachdb/CockroachDriver.ts
+++ src/driver/cockroachdb/CockroachDriver.ts
@@ -385,7 +385,7 @@ export class CockroachDriver implements Driver {
         if (columnMetadata.type === Boolean) {
             return value === true ? 1 : 0
         } else if (columnMetadata.type === "date") {
-            return DateUtils.mixedDateToDateString(value)
+            return DateUtils.mixedDateToDateString(value, columnMetadata.utc)
         } else if (columnMetadata.type === "time") {
             return DateUtils.mixedDateToTimeString(value)
         } else if (
@@ -445,7 +445,7 @@ export class CockroachDriver implements Driver {
         ) {
             value = DateUtils.normalizeHydratedDate(value)
         } else if (columnMetadata.type === "date") {
-            value = DateUtils.mixedDateToDateString(value)
+            value = DateUtils.mixedDateToDateString(value, columnMetadata.utc)
         } else if (columnMetadata.type === "time") {
             value = DateUtils.mixedTimeToString(value)
         } else if (columnMetadata.type === "simple-array") {
