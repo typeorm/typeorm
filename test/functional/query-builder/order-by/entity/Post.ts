5e718d2af077d9e4c3bea4d134fa29ad86ed4047
alukan <64636633+alukan@users.noreply.github.com>
2025-11-01 21:19:46 +0100

    move tests from issues to functional

--- test/functional/query-builder/order-by/entity/Post.ts
+++ test/functional/query-builder/order-by/entity/Post.ts
@@ -1,6 +1,8 @@
 import { Entity } from "../../../../../src/decorator/entity/Entity"
 import { PrimaryGeneratedColumn } from "../../../../../src/decorator/columns/PrimaryGeneratedColumn"
 import { Column } from "../../../../../src/decorator/columns/Column"
+import { OneToMany } from "../../../../../src/decorator/relations/OneToMany"
+import { Comment } from "./Comment"
 
 @Entity({
     orderBy: {
@@ -19,4 +21,7 @@ export class Post {
 
     @Column()
     num2: number = 1
+
+    @OneToMany(() => Comment, comment => comment.post)
+    comments: Comment[]
 }
--- test/functional/query-builder/order-by/query-builder-order-by.ts
+++ test/functional/query-builder/order-by/query-builder-order-by.ts
@@ -8,6 +8,7 @@ import { DataSource } from "../../../../src/data-source/DataSource"
 import { expect } from "chai"
 import { Post } from "./entity/Post"
 import { DriverUtils } from "../../../../src/driver/DriverUtils"
+import { Comment } from "./entity/Comment"
 
 describe("query builder > order-by", () => {
     let connections: DataSource[]
@@ -148,6 +149,96 @@ describe("query builder > order-by", () => {
 
                 expect(loadedPost2!.num1).to.be.equal(10)
                 expect(loadedPost2!.num2).to.be.equal(2)
+            }),
+        ))
+
+    it("should not throw when using leftJoin + orderBy with expression + take + getRawAndEntities", () =>
+        Promise.all(
+            connections.map(async (connection) => {
+                // Create test data
+                const post1 = new Post()
+                post1.myOrder = 1
+                post1.num1 = 10
+                post1.num2 = 5
+
+                const post2 = new Post()
+                post2.myOrder = 2
+                post2.num1 = 15
+                post2.num2 = 3
+
+                const post3 = new Post()
+                post3.myOrder = 3
+                post3.num1 = 20
+                post3.num2 = 4
+
+                await connection.manager.save([post1, post2, post3])
+
+                // Create some comments
+                const comment1 = new Comment()
+                comment1.text = "Comment 1"
+                comment1.postId = post1.id
+
+                const comment2 = new Comment()
+                comment2.text = "Comment 2"
+                comment2.postId = post2.id
+
+                await connection.manager.save([comment1, comment2])
+
+                // Create query builder with leftJoin, orderBy expression and take
+                const queryBuilder = connection.manager
+                    .createQueryBuilder(Post, "post")
+                    .leftJoin("post.comments", "comment")
+                    .orderBy("post.num1 + post.num2", "ASC")
+                    .take(2)
+
+                // This should not throw an error
+                const result = await queryBuilder.getRawAndEntities()
+
+                // Verify we get results
+                expect(result).to.be.an("object")
+                expect(result.entities).to.be.an("array")
+                expect(result.raw).to.be.an("array")
+                expect(result.entities.length).to.be.lessThanOrEqual(2)
+            }),
+        ))
+
+    it("should work with different orderBy expressions and getRawAndEntities with leftJoin", () =>
+        Promise.all(
+            connections.map(async (connection) => {
+                // Create test data
+                const post1 = new Post()
+                post1.myOrder = 1
+                post1.num1 = 10
+                post1.num2 = 5
+
+                await connection.manager.save(post1)
+
+                const comment1 = new Comment()
+                comment1.text = "Test comment"
+                comment1.postId = post1.id
+
+                await connection.manager.save(comment1)
+
+                // Test with different expression types
+                const expressions = [
+                    "post.num1 + post.num2",
+                    "post.num1 * post.num2",
+                    "post.num1 - post.num2",
+                    "post.myOrder + post.num1",
+                ]
+
+                for (const expression of expressions) {
+                    const queryBuilder = connection.manager
+                        .createQueryBuilder(Post, "post")
+                        .leftJoin("post.comments", "comment")
+                        .orderBy(expression, "DESC")
+                        .take(1)
+
+                    // Should not throw error for any expression
+                    const result = await queryBuilder.getRawAndEntities()
+                    expect(result.entities).to.be.an("array")
+                    expect(result.raw).to.be.an("array")
+                }
             }),
         ))
 })
