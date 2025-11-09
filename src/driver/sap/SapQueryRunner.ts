6cfda26fb5e7674ad436f8968d11a339ab1bfe77
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2025-11-09 09:55:53 +0100

    Update src/driver/sap/SapQueryRunner.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/driver/sap/SapQueryRunner.ts
+++ src/driver/sap/SapQueryRunner.ts
@@ -2145,7 +2145,7 @@ export class SapQueryRunner extends BaseQueryRunner implements QueryRunner {
     }
 
     /**
-     * Drops a unique constraints.
+     * Drops unique constraints.
      */
     async dropUniqueConstraints(
         tableOrName: Table | string,
