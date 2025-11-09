2c8050110ce3c884036df54483ffba6c244a4bc2
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2025-11-09 13:06:01 +0100

    more

--- test/functional/migrations/show-command/migration/1530542855524-ExampleMigration.ts
+++ test/functional/migrations/show-command/migration/1530542855524-ExampleMigration.ts
@@ -1,5 +1,4 @@
 import { MigrationInterface } from "../../../../../src/migration/MigrationInterface"
-import { QueryRunner } from "../../../../../src/query-runner/QueryRunner"
 
 export class ExampleMigration1530542855524 implements MigrationInterface {
     public async up() {}
