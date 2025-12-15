4cd62a8e3a6b61124012de2e2f198df328fb4fd6
Aaron <aaron@cute.engineer>
2025-12-15 11:24:04 +1100

    test: adding test for named placeholder in mysql

---
+++ test/github-issues/8828/issue-8828.test.ts
@@ -0,0 +1,48 @@
+import { DataSource } from "../../../src"
+import {
+    closeTestingConnections,
+    createTestingConnections,
+    reloadTestingDatabases,
+} from "../../utils/test-utils"
+import { ExampleEntity } from "./entity/ExampleEntity"
+import { expect } from "chai"
+
+describe.only("github issues > #8828 Widen query parameters type to support named placeholders?", () => {
+    let dataSources: DataSource[]
+
+    before(async () => {
+        dataSources = await createTestingConnections({
+            entities: [ExampleEntity],
+            enabledDrivers: ["mysql"],
+            driverSpecific: {
+                extra: {
+                    namedPlaceholders: true,
+                },
+            },
+        })
+    })
+
+    beforeEach(() => reloadTestingDatabases(dataSources))
+    after(() => closeTestingConnections(dataSources))
+
+    it("can use named parameters in a sql query", async () => {
+        await Promise.all(
+            dataSources.map(async (dataSource) => {
+                const foo = new ExampleEntity()
+                foo.name = "foo"
+                await dataSource.manager.save(foo)
+
+                const bar = new ExampleEntity()
+                bar.name = "bar"
+                await dataSource.manager.save(bar)
+
+                const examples = await dataSource.manager.query(
+                    `SELECT * FROM example_entity WHERE name = :name`,
+                    { name: "bar" },
+                )
+
+                expect(examples[0].name).to.equal("bar")
+            }),
+        )
+    })
+})
