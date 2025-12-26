106f54d675bcc6959719e6ae24184a7ae4dd085b
Prakhar Chhalotre <chhalotreprakhar00@gmail.com>
2025-12-26 15:48:51 +0530

    test: add tests with cascade false

--- test/functional/repository/clear-cascade/postgres/clear-cascade-postgres.test.ts
+++ test/functional/repository/clear-cascade/postgres/clear-cascade-postgres.test.ts
@@ -21,26 +21,66 @@ describe("repository > clear cascade (postgres)", () => {
     beforeEach(() => reloadTestingDatabases(dataSources))
     after(() => closeTestingConnections(dataSources))
 
-    it("clear({ cascade: true }) truncates dependent tables", () =>
-        Promise.all(
-            dataSources.map(async (dataSource) => {
-                const parentRepo = dataSource.getRepository(Parent)
-                const childRepo = dataSource.getRepository(Child)
+    describe("clear({ cascade: true })", () => {
+        it("truncates dependent tables", () =>
+            Promise.all(
+                dataSources.map(async (dataSource) => {
+                    const parentRepo = dataSource.getRepository(Parent)
+                    const childRepo = dataSource.getRepository(Child)
 
-                const parent = await parentRepo.save({ name: "p1" })
-                await childRepo.save({ value: "c1", parent })
+                    const parent = await parentRepo.save({ name: "p1" })
+                    await childRepo.save({ value: "c1", parent })
 
-                let parentCount = await parentRepo.count()
-                let childCount = await childRepo.count()
-                expect(parentCount).to.equal(1)
-                expect(childCount).to.equal(1)
+                    let parentCount = await parentRepo.count()
+                    let childCount = await childRepo.count()
+                    expect(parentCount).to.equal(1)
+                    expect(childCount).to.equal(1)
 
-                await parentRepo.clear({ cascade: true })
+                    await parentRepo.clear({ cascade: true })
 
-                parentCount = await parentRepo.count()
-                childCount = await childRepo.count()
-                expect(parentCount).to.equal(0)
-                expect(childCount).to.equal(0)
-            }),
-        ))
+                    parentCount = await parentRepo.count()
+                    childCount = await childRepo.count()
+                    expect(parentCount).to.equal(0)
+                    expect(childCount).to.equal(0)
+                }),
+            ))
+    })
+
+    describe("clear({ cascade: false })", () => {
+        it("fails with dependent tables", () =>
+            Promise.all(
+                dataSources.map(async (dataSource) => {
+                    const parentRepo = dataSource.getRepository(Parent)
+                    const childRepo = dataSource.getRepository(Child)
+
+                    const parent = await parentRepo.save({ name: "p1" })
+                    await childRepo.save({ value: "c1", parent })
+
+                    const parentCount = await parentRepo.count()
+                    const childCount = await childRepo.count()
+                    expect(parentCount).to.equal(1)
+                    expect(childCount).to.equal(1)
+
+                    await expect(parentRepo.clear({ cascade: false })).to.be
+                        .rejected
+                }),
+            ))
+
+        it("truncates independent table", () =>
+            Promise.all(
+                dataSources.map(async (dataSource) => {
+                    const childRepo = dataSource.getRepository(Child)
+
+                    await childRepo.save({ value: "c1" })
+
+                    const childCount = await childRepo.count()
+                    expect(childCount).to.equal(1)
+
+                    await childRepo.clear({ cascade: false })
+
+                    const newChildCount = await childRepo.count()
+                    expect(newChildCount).to.equal(0)
+                }),
+            ))
+    })
 })
