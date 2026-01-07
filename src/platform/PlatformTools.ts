dd2cb3c2f6bc7331a501f1095bbbce246f2ab444
mag123c <diehreo@gmail.com>
2026-01-07 21:03:23 +0900

    chore: revert PlatformTools.ts changes

--- src/platform/PlatformTools.ts
+++ src/platform/PlatformTools.ts
@@ -45,6 +45,7 @@ export class PlatformTools {
     /**
      * Loads ("require"-s) given file or package.
      * This operation only supports on node platform
+     * @param name
      */
     static load(name: string): any {
         // if name is not absolute or relative, then try to load package from the node_modules of the directory we are currently in
@@ -156,6 +157,7 @@ export class PlatformTools {
 
     /**
      * Normalizes given path. Does "path.normalize" and replaces backslashes with forward slashes on Windows.
+     * @param pathStr
      */
     static pathNormalize(pathStr: string): string {
         let normalizedPath = path.normalize(pathStr)
@@ -166,6 +168,7 @@ export class PlatformTools {
 
     /**
      * Gets file extension. Does "path.extname".
+     * @param pathStr
      */
     static pathExtname(pathStr: string): string {
         return path.extname(pathStr)
@@ -173,6 +176,7 @@ export class PlatformTools {
 
     /**
      * Resolved given path. Does "path.resolve".
+     * @param pathStr
      */
     static pathResolve(pathStr: string): string {
         return path.resolve(pathStr)
@@ -180,6 +184,7 @@ export class PlatformTools {
 
     /**
      * Synchronously checks if file exist. Does "fs.existsSync".
+     * @param pathStr
      */
     static fileExist(pathStr: string): boolean {
         return fs.existsSync(pathStr)
@@ -199,8 +204,8 @@ export class PlatformTools {
 
     /**
      * Loads a dotenv file into the environment variables.
-     *
      * @param path The file to load as a dotenv configuration
+     * @param pathStr
      */
     static dotenv(pathStr: string): void {
         dotenv.config({ path: pathStr })
@@ -208,6 +213,7 @@ export class PlatformTools {
 
     /**
      * Gets environment variable.
+     * @param name
      */
     static getEnvVariable(name: string): any {
         return process.env[name]
@@ -215,6 +221,7 @@ export class PlatformTools {
 
     /**
      * Highlights sql string to be printed in the console.
+     * @param sql
      */
     static highlightSql(sql: string) {
         return highlight(sql, {
@@ -234,6 +241,8 @@ export class PlatformTools {
 
     /**
      * Pretty-print sql string to be print in the console.
+     * @param sql
+     * @param dataSourceType
      */
     static formatSql(sql: string, dataSourceType?: DatabaseType): string {
         const databaseLanguageMap: Record<
