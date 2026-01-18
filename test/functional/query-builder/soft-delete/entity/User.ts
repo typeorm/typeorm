31cc389b8e1a07f9ba479e50bcfc5eb68d549691
gioboa <giorgiob.boa@gmail.com>
2026-01-18 20:27:53 +0100

    test: move tests into functional folder

--- test/functional/query-builder/soft-delete/entity/User.ts
+++ test/functional/query-builder/soft-delete/entity/User.ts
@@ -3,7 +3,13 @@ import { PrimaryGeneratedColumn } from "../../../../../src/decorator/columns/Pri
 import { DeleteDateColumn } from "../../../../../src/decorator/columns/DeleteDateColumn"
 import { Column } from "../../../../../src/decorator/columns/Column"
 import { Photo } from "./Photo"
-import { JoinColumn, OneToOne } from "../../../../../src"
+import {
+    JoinColumn,
+    OneToOne,
+    CreateDateColumn,
+    UpdateDateColumn,
+    VersionColumn,
+} from "../../../../../src"
 
 @Entity()
 export class User {
@@ -13,12 +19,24 @@ export class User {
     @Column()
     name: string
 
+    @Column({ nullable: true })
+    company: string
+
     @Column()
     likesCount: number = 0
 
     @OneToOne(() => Photo)
     @JoinColumn()
     picture: Photo
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @VersionColumn()
+    version: number
 
     @DeleteDateColumn()
     deletedAt: Date
