19fff0b6a050f9a88a1ce9f0259bd9b15be4795e
gioboa <giorgiob.boa@gmail.com>
2026-01-19 21:59:46 +0100

    chore: run format 🍉

--- test/github-issues/9544/issue-9544.test.ts
+++ test/github-issues/9544/issue-9544.test.ts
@@ -1,9 +1,8 @@
-import { expect } from "chai";
+import { expect } from "chai"
 import { shorten } from "../../../src/util/StringUtils"
 
 describe("github issues > #9544 shorten long camel case alias correctly", () => {
-    const testCases = 
-    [
+    const testCases = [
         {
             input: "SupplierStockInTransit__supplierStock__productVariant_customFields_tryonFrameAsset",
             expected: "SuStInTr__suSt__prVacuFitrFrAs",
@@ -15,21 +14,21 @@ describe("github issues > #9544 shorten long camel case alias correctly", () => 
         {
             input: "camelCase_alias",
             expected: "caCaal",
-        }
-    ];
-    
+        },
+    ]
+
     testCases.map((testCase, i) => {
-        it(`should produce correct results for camel case aliases, case #${i+1}`, () => {
-            const {input, expected} = testCase;
-            expect(shorten(input)).to.equal(expected);
+        it(`should produce correct results for camel case aliases, case #${i + 1}`, () => {
+            const { input, expected } = testCase
+            expect(shorten(input)).to.equal(expected)
         })
     })
 
     it("should produce unique results for different inputs", () => {
-        const uniqueInputs = new Set(testCases.map(x => x.input));
-        const uniqueExpects = new Set(testCases.map(x => x.expected));
+        const uniqueInputs = new Set(testCases.map((x) => x.input))
+        const uniqueExpects = new Set(testCases.map((x) => x.expected))
 
-        expect(uniqueInputs.size).to.equal(testCases.length);
-        expect(uniqueExpects.size).to.equal(testCases.length);
+        expect(uniqueInputs.size).to.equal(testCases.length)
+        expect(uniqueExpects.size).to.equal(testCases.length)
     })
 })
