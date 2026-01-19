6d438d53c8392bb631d38aaef934a9862b7e5009
Prakhar Chhalotre <chhalotreprakhar00@gmail.com>
2026-01-19 16:41:44 +0530

    fix: disable global order for aggregate functions

--- test/functional/repository/aggregate-methods/entity/Post.ts
+++ test/functional/repository/aggregate-methods/entity/Post.ts
@@ -2,7 +2,7 @@ import { Entity } from "../../../../../src/decorator/entity/Entity"
 import { Column } from "../../../../../src/decorator/columns/Column"
 import { PrimaryColumn } from "../../../../../src/decorator/columns/PrimaryColumn"
 
-@Entity()
+@Entity({ orderBy: { counter: "DESC" } })
 export class Post {
     @PrimaryColumn()
     id: number
