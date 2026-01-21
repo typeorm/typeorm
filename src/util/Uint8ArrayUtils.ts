392bbfbd009b898351094366dfde365b6b5ed221
Julian Pufler <julian@pufler.dev>
2026-01-21 23:15:57 +0100

    refactor: uint8array utils performance refactor

--- src/util/Uint8ArrayUtils.ts
+++ src/util/Uint8ArrayUtils.ts
@@ -5,17 +5,26 @@ export const areUint8ArraysEqual = (
     left: Uint8Array,
     right: Uint8Array,
 ): boolean => {
-    if (left.byteLength !== right.byteLength) return false
-    for (let index = 0; index < left.byteLength; index++) {
-        if (left[index] !== right[index]) return false
+    if (left.byteLength !== right.byteLength) {
+        return false
     }
+
+    for (let index = 0; index < left.byteLength; index++) {
+        if (left[index] !== right[index]) {
+            return false
+        }
+    }
+
     return true
 }
 
-export const uint8ArrayToHex = (value: Uint8Array): string => {
+const HEX_LOOKUP_TABLE = Array.from({ length: 256 }, (_, i) =>
+    i.toString(16).padStart(2, "0"),
+)
+export const uint8ArrayToHex = (value: Uint8Array) => {
     let hex = ""
     for (let index = 0; index < value.length; index++) {
-        hex += value[index].toString(16).padStart(2, "0")
+        hex += HEX_LOOKUP_TABLE[value[index]]
     }
     return hex
 }
