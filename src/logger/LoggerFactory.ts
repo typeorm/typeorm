1d1533050d3182e0102a2531f432dbb97c32d20d
gioboa <giorgiob.boa@gmail.com>
2026-02-24 16:03:15 +0100

    fix: should invoke custom logger

--- src/logger/LoggerFactory.ts
+++ src/logger/LoggerFactory.ts
@@ -27,7 +27,7 @@ export class LoggerFactory {
     ): Logger {
         if (ObjectUtils.isObject(logger)) {
             if (
-                options !== undefined &&
+                options &&
                 typeof (logger as Logger).setOptions === "function"
             ) {
                 ;(logger as Logger).setOptions!(options)
