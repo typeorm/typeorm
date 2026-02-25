29c5cc219f77f0602d0d022a8c7db1f2058ee3bd
gioboa <giorgiob.boa@gmail.com>
2026-02-25 10:58:42 +0100

    fix: should invoke custom logger

--- src/logger/LoggerFactory.ts
+++ src/logger/LoggerFactory.ts
@@ -6,7 +6,7 @@ import { FileLogger } from "./FileLogger"
 import { DebugLogger } from "./DebugLogger"
 import { ObjectUtils } from "../util/ObjectUtils"
 import { FormattedConsoleLogger } from "./FormattedConsoleLogger"
-import { AbstractLogger } from "./AbstractLogger"
+
 /**
  * Helps to create logger instances.
  */
