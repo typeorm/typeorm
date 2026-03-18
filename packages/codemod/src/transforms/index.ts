f928df746716464ff278eff8b599ff62b337659f
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-03-18 01:04:59 +0100

    refactor: simplify transforms barrel to import * as v1

--- packages/codemod/src/transforms/index.ts
+++ packages/codemod/src/transforms/index.ts
@@ -1,9 +1,9 @@
-import { description as v1Description } from "./v1"
+import * as v1 from "./v1"
 
 export interface VersionInfo {
     description: string
 }
 
 export const versions: Record<string, VersionInfo> = {
-    v1: { description: v1Description },
+    v1,
 }
