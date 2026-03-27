77fee2e0a675b645ba232819bf8929aa0f3540be
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-03-27 00:14:54 +0000

    fix(codemod): remove silent error swallowing in package.json scanner

--- packages/codemod/src/dependencies/find-package-json.ts
+++ packages/codemod/src/dependencies/find-package-json.ts
@@ -10,23 +10,19 @@ export const findPackageJsonFiles = (paths: string[]): string[] => {
     const results: string[] = []
 
     for (const p of paths) {
-        try {
-            const stat = fs.statSync(p, { throwIfNoEntry: false })
-            if (!stat) continue
+        const stat = fs.statSync(p, { throwIfNoEntry: false })
+        if (!stat) continue
 
-            if (stat.isFile() && path.basename(p) === "package.json") {
-                results.push(p)
-            } else if (stat.isDirectory()) {
-                results.push(
-                    ...globSync("**/package.json", {
-                        cwd: p,
-                        ignore: ["**/node_modules/**"],
-                        absolute: true,
-                    }),
-                )
-            }
-        } catch {
-            // Skip unreadable paths (permission errors, broken symlinks, etc.)
+        if (stat.isFile() && path.basename(p) === "package.json") {
+            results.push(p)
+        } else if (stat.isDirectory()) {
+            results.push(
+                ...globSync("**/package.json", {
+                    cwd: p,
+                    ignore: ["**/node_modules/**"],
+                    absolute: true,
+                }),
+            )
         }
     }
 
