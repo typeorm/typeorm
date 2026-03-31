482eda7428000478c263b0772c9733533c45753b
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-03-31 13:25:03 +0000

    refactor(test): convert custom-column-names test to standard multi-driver pattern

--- test/functional/persistence/custom-column-names/persistence-custom-column-names.test.ts
+++ test/functional/persistence/custom-column-names/persistence-custom-column-names.test.ts
@@ -1,168 +1,154 @@
 import { expect } from "chai"
 import "reflect-metadata"
-import { DataSource } from "../../../../src/data-source/DataSource"
-import { setupSingleTestingConnection } from "../../../utils/test-utils"
+import type { DataSource } from "../../../../src/data-source/DataSource"
+import {
+    closeTestingConnections,
+    createTestingConnections,
+    reloadTestingDatabases,
+} from "../../../utils/test-utils"
 import { Category } from "./entity/Category"
 import { CategoryMetadata } from "./entity/CategoryMetadata"
 import { Post } from "./entity/Post"
 
-describe("persistence > custom-column-names", function () {
-    let dataSource: DataSource
+describe("persistence > custom-column-names", () => {
+    let dataSources: DataSource[]
     before(async () => {
-        const options = setupSingleTestingConnection("mysql", {
+        dataSources = await createTestingConnections({
             entities: [Post, Category, CategoryMetadata],
         })
-        if (!options) return
-
-        dataSource = new DataSource(options)
     })
-    after(() => dataSource?.destroy())
+    beforeEach(() => reloadTestingDatabases(dataSources))
+    after(() => closeTestingConnections(dataSources))
 
-    function reloadDatabase() {
-        if (!dataSource) return
-        return dataSource.synchronize(true)
-    }
+    it("should attach exist entity to exist entity with many-to-one relation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const newCategory = dataSource.manager.create(Category)
+                newCategory.name = "Animals"
+                await dataSource.manager.save(newCategory)
 
-    it("should attach exist entity to exist entity with many-to-one relation", async function () {
-        if (!dataSource) return
-        await reloadDatabase()
+                const newPost = dataSource.manager.create(Post)
+                newPost.title = "All about animals"
+                await dataSource.manager.save(newPost)
 
-        const categoryRepository = dataSource.getRepository(Category)
-        const postRepository = dataSource.getRepository(Post)
+                newPost.category = newCategory
+                await dataSource.manager.save(newPost)
 
-        const newCategory = categoryRepository.create()
-        newCategory.name = "Animals"
-        await categoryRepository.save(newCategory)
+                const loadedPost = await dataSource.manager.findOneOrFail(
+                    Post,
+                    { where: { id: 1 }, relations: { category: true } },
+                )
 
-        const newPost = postRepository.create()
-        newPost.title = "All about animals"
-        await postRepository.save(newPost)
+                expect(loadedPost.category).not.to.be.undefined
+                expect(loadedPost.categoryId).not.to.be.undefined
+            }),
+        ))
 
-        newPost.category = newCategory
-        await postRepository.save(newPost)
+    it("should attach new entity to exist entity with many-to-one relation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const newCategory = dataSource.manager.create(Category)
+                newCategory.name = "Animals"
+                await dataSource.manager.save(newCategory)
 
-        const loadedPost = await postRepository.findOneOrFail({
-            where: { id: 1 },
-            relations: { category: true },
-        })
+                const newPost = dataSource.manager.create(Post)
+                newPost.title = "All about animals"
+                newPost.category = newCategory
+                await dataSource.manager.save(newPost)
 
-        expect(loadedPost.category).not.to.be.undefined
-        expect(loadedPost.categoryId).not.to.be.undefined
-    })
+                const loadedPost = await dataSource.manager.findOneOrFail(
+                    Post,
+                    { where: { id: 1 }, relations: { category: true } },
+                )
 
-    it("should attach new entity to exist entity with many-to-one relation", async function () {
-        if (!dataSource) return
-        await reloadDatabase()
+                expect(loadedPost.category).not.to.be.undefined
+                expect(loadedPost.categoryId).not.to.be.undefined
+            }),
+        ))
 
-        const categoryRepository = dataSource.getRepository(Category)
-        const postRepository = dataSource.getRepository(Post)
+    it("should attach new entity to new entity with many-to-one relation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const newCategory = dataSource.manager.create(Category)
+                newCategory.name = "Animals"
+                const newPost = dataSource.manager.create(Post)
+                newPost.title = "All about animals"
+                newPost.category = newCategory
+                await dataSource.manager.save(newPost)
 
-        const newCategory = categoryRepository.create()
-        newCategory.name = "Animals"
-        await categoryRepository.save(newCategory)
+                const loadedPost = await dataSource.manager.findOneOrFail(
+                    Post,
+                    { where: { id: 1 }, relations: { category: true } },
+                )
 
-        const newPost = postRepository.create()
-        newPost.title = "All about animals"
-        newPost.category = newCategory
-        await postRepository.save(newPost)
+                expect(loadedPost.category).not.to.be.undefined
+                expect(loadedPost.categoryId).not.to.be.undefined
+            }),
+        ))
 
-        const loadedPost = await postRepository.findOneOrFail({
-            where: { id: 1 },
-            relations: { category: true },
-        })
+    it("should attach exist entity to exist entity with one-to-one relation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const newPost = dataSource.manager.create(Post)
+                newPost.title = "All about animals"
+                await dataSource.manager.save(newPost)
 
-        expect(loadedPost.category).not.to.be.undefined
-        expect(loadedPost.categoryId).not.to.be.undefined
-    })
+                const newCategory = dataSource.manager.create(Category)
+                newCategory.name = "Animals"
+                await dataSource.manager.save(newCategory)
 
-    it("should attach new entity to new entity with many-to-one relation", async function () {
-        if (!dataSource) return
-        await reloadDatabase()
+                const newMetadata = dataSource.manager.create(CategoryMetadata)
+                newMetadata.keyword = "animals"
+                await dataSource.manager.save(newMetadata)
 
-        const categoryRepository = dataSource.getRepository(Category)
-        const postRepository = dataSource.getRepository(Post)
+                newCategory.metadata = newMetadata
+                newPost.category = newCategory
+                await dataSource.manager.save(newPost)
 
-        const newCategory = categoryRepository.create()
-        newCategory.name = "Animals"
-        const newPost = postRepository.create()
-        newPost.title = "All about animals"
-        newPost.category = newCategory
-        await postRepository.save(newPost)
+                const loadedPost = await dataSource.manager.findOneOrFail(
+                    Post,
+                    {
+                        where: { id: 1 },
+                        relations: { category: { metadata: true } },
+                    },
+                )
 
-        const loadedPost = await postRepository.findOneOrFail({
-            where: { id: 1 },
-            relations: { category: true },
-        })
+                expect(loadedPost.category).not.to.be.undefined
+                expect(loadedPost.categoryId).not.to.be.undefined
+                expect(loadedPost.category.metadata).not.to.be.undefined
+                expect(loadedPost.category.metadataId).not.to.be.undefined
+            }),
+        ))
 
-        expect(loadedPost.category).not.to.be.undefined
-        expect(loadedPost.categoryId).not.to.be.undefined
-    })
+    it("should attach new entity to exist entity with one-to-one relation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const newPost = dataSource.manager.create(Post)
+                newPost.title = "All about animals"
+                await dataSource.manager.save(newPost)
 
-    it("should attach exist entity to exist entity with one-to-one relation", async function () {
-        if (!dataSource) return
-        await reloadDatabase()
+                const newMetadata = dataSource.manager.create(CategoryMetadata)
+                newMetadata.keyword = "animals"
+                const newCategory = dataSource.manager.create(Category)
+                newCategory.name = "Animals"
+                newCategory.metadata = newMetadata
+                await dataSource.manager.save(newCategory)
 
-        const categoryRepository = dataSource.getRepository(Category)
-        const postRepository = dataSource.getRepository(Post)
-        const metadataRepository = dataSource.getRepository(CategoryMetadata)
+                newPost.category = newCategory
+                await dataSource.manager.save(newPost)
 
-        const newPost = postRepository.create()
-        newPost.title = "All about animals"
-        await postRepository.save(newPost)
+                const loadedPost = await dataSource.manager.findOneOrFail(
+                    Post,
+                    {
+                        where: { id: 1 },
+                        relations: { category: { metadata: true } },
+                    },
+                )
 
-        const newCategory = categoryRepository.create()
-        newCategory.name = "Animals"
-        await categoryRepository.save(newCategory)
-
-        const newMetadata = metadataRepository.create()
-        newMetadata.keyword = "animals"
-        await metadataRepository.save(newMetadata)
-
-        newCategory.metadata = newMetadata
-        newPost.category = newCategory
-        await postRepository.save(newPost)
-
-        const loadedPost = await postRepository.findOneOrFail({
-            where: { id: 1 },
-            relations: { category: { metadata: true } },
-        })
-
-        expect(loadedPost.category).not.to.be.undefined
-        expect(loadedPost.categoryId).not.to.be.undefined
-        expect(loadedPost.category.metadata).not.to.be.undefined
-        expect(loadedPost.category.metadataId).not.to.be.undefined
-    })
-
-    it("should attach new entity to exist entity with one-to-one relation", async function () {
-        if (!dataSource) return
-        await reloadDatabase()
-
-        const categoryRepository = dataSource.getRepository(Category)
-        const postRepository = dataSource.getRepository(Post)
-        const metadataRepository = dataSource.getRepository(CategoryMetadata)
-
-        const newPost = postRepository.create()
-        newPost.title = "All about animals"
-        await postRepository.save(newPost)
-
-        const newMetadata = metadataRepository.create()
-        newMetadata.keyword = "animals"
-        const newCategory = categoryRepository.create()
-        newCategory.name = "Animals"
-        newCategory.metadata = newMetadata
-        await categoryRepository.save(newCategory)
-
-        newPost.category = newCategory
-        await postRepository.save(newPost)
-
-        const loadedPost = await postRepository.findOneOrFail({
-            where: { id: 1 },
-            relations: { category: { metadata: true } },
-        })
-
-        expect(loadedPost.category).not.to.be.undefined
-        expect(loadedPost.categoryId).not.to.be.undefined
-        expect(loadedPost.category.metadata).not.to.be.undefined
-        expect(loadedPost.category.metadataId).not.to.be.undefined
-    })
+                expect(loadedPost.category).not.to.be.undefined
+                expect(loadedPost.categoryId).not.to.be.undefined
+                expect(loadedPost.category.metadata).not.to.be.undefined
+                expect(loadedPost.category.metadataId).not.to.be.undefined
+            }),
+        ))
 })
