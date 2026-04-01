dc39d0c77301a04d092384febef727eb13672fa5
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-04-01 23:53:40 +0000

    test(cascades): unskip cascade-remove test and remove driver restriction
    The cascade-remove test was skipped because CascadesSubjectBuilder
    did not traverse isCascadeRemove relations. Now that the traversal
    is gated on operationType, cascade remove works correctly.
    
    Remove MySQL-only restriction, use loadedUser for remove (ensures
    relations are populated for cascade traversal).

--- test/functional/cascades/cascade-remove/cascade-remove.test.ts
+++ test/functional/cascades/cascade-remove/cascade-remove.test.ts
@@ -9,13 +9,11 @@ import type { DataSource } from "../../../../src/data-source/DataSource"
 import { Photo } from "./entity/Photo"
 import { User } from "./entity/User"
 
-// todo: fix later
-describe.skip("cascades > remove", () => {
+describe("cascades > remove", () => {
     let dataSources: DataSource[]
     before(async () => {
         dataSources = await createTestingConnections({
             __dirname,
-            enabledDrivers: ["mysql"],
         })
     })
     beforeEach(() => reloadTestingDatabases(dataSources))
@@ -71,7 +69,7 @@ describe.skip("cascades > remove", () => {
                 expect(manyToManyPhotoNames).to.deep.include("many-to-many #2")
                 expect(manyToManyPhotoNames).to.deep.include("many-to-many #3")
 
-                await dataSource.manager.remove(user)
+                await dataSource.manager.remove(loadedUser!)
 
                 const allPhotos = await dataSource.manager.find(Photo)
                 expect(allPhotos.length).to.equal(1)
