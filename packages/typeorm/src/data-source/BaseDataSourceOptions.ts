ab69ea907d17440b3f97e154000e08ce1b4b400f
CretanBull <salvinodsa30@gmail.com>
2026-04-03 12:46:06 +0530

    feat(caching): implement in-memory cache with lru cache

--- packages/typeorm/src/data-source/BaseDataSourceOptions.ts
+++ packages/typeorm/src/data-source/BaseDataSourceOptions.ts
@@ -173,11 +173,16 @@ export interface BaseDataSourceOptions {
               /**
                * Type of caching.
                *
-               * - "database" means cached values will be stored in the separate table in database. This is default value.
+               * - "database" means cached values will be stored in the separate table in database. This is the default value.
+               * - "in-memory" means cached values will be stored in application memory using an LRU cache mechanism.
                * - "redis" means cached values will be stored inside redis. You must provide redis connection options.
                */
               readonly type?:
-                  "database" | "redis" | "ioredis" | "ioredis/cluster" // todo: add mongodb and other cache providers as well in the future
+                  | "database"
+                  | "in-memory"
+                  | "redis"
+                  | "ioredis"
+                  | "ioredis/cluster" // todo: add mongodb and other cache providers as well in the future
 
               /**
                * Factory function for custom cache providers that implement QueryResultCache.
@@ -191,7 +196,7 @@ export interface BaseDataSourceOptions {
               readonly tableName?: string
 
               /**
-               * Used to provide redis connection options.
+               * Used to provide redis/in-memory cache connection options.
                */
               readonly options?: any
 
