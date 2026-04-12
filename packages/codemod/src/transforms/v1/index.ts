407d1addbb86b5bfb407e2a52d5cc575f45eaedb
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-04-12 00:18:19 +0000

    feat(codemod): add transform for orphanedRowAction on @ManyToOne

--- packages/codemod/src/transforms/v1/index.ts
+++ packages/codemod/src/transforms/v1/index.ts
@@ -24,6 +24,7 @@ import * as queryBuilderReplacePropertyNames from "./query-builder-replace-prope
 import * as queryBuilderWhereExpression from "./query-builder-where-expression"
 import * as queryRunnerLoadedTablesViews from "./query-runner-loaded-tables-views"
 import * as relationCount from "./relation-count"
+import * as relationOrphanedRowAction from "./relation-orphaned-row-action"
 import * as repositoryAbstract from "./repository-abstract"
 import * as repositoryExist from "./repository-exist"
 import * as repositoryFindByIds from "./repository-find-by-ids"
@@ -71,6 +72,7 @@ export const transforms = [
     queryBuilderReplacePropertyNames,
     findOptionsStringSelect,
     findOptionsStringRelations,
+    relationOrphanedRowAction,
 ]
 
 export default transformer(transforms)
