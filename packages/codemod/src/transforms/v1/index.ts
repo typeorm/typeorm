f38201f32c447fcee359c520429584da44d9a4f7
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2026-04-18 20:01:51 +0000

    refactor(codemod): drop datasource-expo transform — dep auto-upgrade suffices

--- packages/codemod/src/transforms/v1/index.ts
+++ packages/codemod/src/transforms/v1/index.ts
@@ -3,7 +3,6 @@ import * as columnUnsignedNumeric from "./column-unsigned-numeric"
 import * as columnWidthZerofill from "./column-width-zerofill"
 import * as connectionManager from "./connection-manager"
 import * as connectionToDataSource from "./connection-to-datasource"
-import * as datasourceExpo from "./datasource-expo"
 import * as datasourceMongodb from "./datasource-mongodb"
 import * as datasourceMssql from "./datasource-mssql"
 import * as datasourceMysqlConnector from "./datasource-mysql-connector"
@@ -75,7 +74,6 @@ export const transforms = [
     findOptionsStringSelect,
     findOptionsStringRelations,
     connectionManager,
-    datasourceExpo,
     fileLogger,
 ]
 
