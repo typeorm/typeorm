50969ff29d6b67adcdf9605b4c03da602a6fde89
dukhyun <ideook@gmail.com>
2026-05-11 11:06:49 +0900

    fix: preserve postgres type modifiers with collation changes

--- test/github-issues/3357/issue-3357.test.ts
+++ test/github-issues/3357/issue-3357.test.ts
@@ -25,41 +25,100 @@ describe("github issues > #3357 postgres varchar length changes", () => {
             dataSources.map(async (dataSource) => {
                 const queryRunner = dataSource.createQueryRunner()
 
-                await queryRunner.createTable(
-                    new Table({
-                        name: "bug3357",
-                        columns: [
-                            {
-                                name: "example",
-                                type: "character varying",
-                                length: "50",
-                            },
-                        ],
-                    }),
-                    true,
-                )
+                try {
+                    await queryRunner.createTable(
+                        new Table({
+                            name: "bug3357",
+                            columns: [
+                                {
+                                    name: "example",
+                                    type: "character varying",
+                                    length: "50",
+                                },
+                            ],
+                        }),
+                        true,
+                    )
 
-                const table = await queryRunner.getTable("bug3357")
-                const column = table!.findColumnByName("example")!
-                const changedColumn = column.clone()
-                changedColumn.length = "51"
+                    const table = await queryRunner.getTable("bug3357")
+                    const column = table!.findColumnByName("example")!
+                    const changedColumn = column.clone()
+                    changedColumn.length = "51"
 
-                queryRunner.enableSqlMemory()
-                await queryRunner.changeColumn(table!, column, changedColumn)
-                const sqlInMemory = queryRunner.getMemorySql()
+                    queryRunner.enableSqlMemory()
+                    await queryRunner.changeColumn(
+                        table!,
+                        column,
+                        changedColumn,
+                    )
+                    const sqlInMemory = queryRunner.getMemorySql()
 
-                expect(
-                    sqlInMemory.upQueries.map((query) => query.query),
-                ).to.eql([
-                    'ALTER TABLE "bug3357" ALTER COLUMN "example" TYPE character varying(51)',
-                ])
-                expect(
-                    sqlInMemory.downQueries.map((query) => query.query),
-                ).to.eql([
-                    'ALTER TABLE "bug3357" ALTER COLUMN "example" TYPE character varying(50)',
-                ])
+                    expect(
+                        sqlInMemory.upQueries.map((query) => query.query),
+                    ).to.eql([
+                        'ALTER TABLE "bug3357" ALTER COLUMN "example" TYPE character varying(51)',
+                    ])
+                    expect(
+                        sqlInMemory.downQueries.map((query) => query.query),
+                    ).to.eql([
+                        'ALTER TABLE "bug3357" ALTER COLUMN "example" TYPE character varying(50)',
+                    ])
+                } finally {
+                    await queryRunner.release()
+                }
+            }),
+        ))
 
-                await queryRunner.release()
+    it("should preserve varchar length when changing collation", () =>
+        Promise.all(
+            dataSources.map(async (dataSource) => {
+                const queryRunner = dataSource.createQueryRunner()
+
+                try {
+                    await queryRunner.createTable(
+                        new Table({
+                            name: "bug3357_collation",
+                            columns: [
+                                {
+                                    name: "example",
+                                    type: "character varying",
+                                    length: "50",
+                                },
+                            ],
+                        }),
+                        true,
+                    )
+
+                    const table =
+                        await queryRunner.getTable("bug3357_collation")
+                    const column = table!.findColumnByName("example")!
+                    const changedColumn = column.clone()
+                    changedColumn.length = "51"
+                    changedColumn.collation = "C"
+
+                    queryRunner.enableSqlMemory()
+                    await queryRunner.changeColumn(
+                        table!,
+                        column,
+                        changedColumn,
+                    )
+                    const sqlInMemory = queryRunner.getMemorySql()
+
+                    expect(
+                        sqlInMemory.upQueries.map((query) => query.query),
+                    ).to.eql([
+                        'ALTER TABLE "bug3357_collation" ALTER COLUMN "example" TYPE character varying(51)',
+                        'ALTER TABLE "bug3357_collation" ALTER COLUMN "example" TYPE character varying(51) COLLATE "C"',
+                    ])
+                    expect(
+                        sqlInMemory.downQueries.map((query) => query.query),
+                    ).to.eql([
+                        'ALTER TABLE "bug3357_collation" ALTER COLUMN "example" TYPE character varying(50)',
+                        'ALTER TABLE "bug3357_collation" ALTER COLUMN "example" TYPE character varying(50) COLLATE pg_catalog."default"',
+                    ])
+                } finally {
+                    await queryRunner.release()
+                }
             }),
         ))
 })
