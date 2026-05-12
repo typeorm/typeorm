6520461182975dcecaf14a88217b1e87bde108c7
ZainKazmiii <111486944+ZainKazmiii@users.noreply.github.com>
2026-05-11 22:08:25 -0400

    test: cover postgres varchar length alterations

--- test/functional/database-schema/column-length/postgres/column-length-postgres.test.ts
+++ test/functional/database-schema/column-length/postgres/column-length-postgres.test.ts
@@ -44,6 +44,19 @@ describe("database schema > column length > postgres", () => {
     it("all types should update their size", () =>
         Promise.all(
             dataSources.map(async (dataSource) => {
+                const characterVaryingValue = "a".repeat(50)
+                const varcharValue = "b".repeat(50)
+                const characterValue = "c".repeat(50)
+                const charValue = "d".repeat(50)
+
+                await dataSource.getRepository(Post).save({
+                    id: 1,
+                    characterVarying: characterVaryingValue,
+                    varchar: varcharValue,
+                    character: characterValue,
+                    char: charValue,
+                })
+
                 const metadata = dataSource.getMetadata(Post)
                 metadata.findColumnWithPropertyName(
                     "characterVarying",
@@ -51,6 +64,33 @@ describe("database schema > column length > postgres", () => {
                 metadata.findColumnWithPropertyName("varchar")!.length = "100"
                 metadata.findColumnWithPropertyName("character")!.length = "100"
                 metadata.findColumnWithPropertyName("char")!.length = "100"
+
+                const sqlInMemory = await dataSource.driver
+                    .createSchemaBuilder()
+                    .log()
+                const upQueries = sqlInMemory.upQueries.map(
+                    (query) => query.query,
+                )
+
+                expect(upQueries).to.include(
+                    'ALTER TABLE "post" ALTER COLUMN "characterVarying" TYPE character varying(100)',
+                )
+                expect(upQueries).to.include(
+                    'ALTER TABLE "post" ALTER COLUMN "varchar" TYPE character varying(100)',
+                )
+                expect(upQueries).to.include(
+                    'ALTER TABLE "post" ALTER COLUMN "character" TYPE character(100)',
+                )
+                expect(upQueries).to.include(
+                    'ALTER TABLE "post" ALTER COLUMN "char" TYPE character(100)',
+                )
+                expect(upQueries.some((query) => query.includes("DROP COLUMN")))
+                    .to.be.false
+                expect(
+                    upQueries.some((query) =>
+                        query.includes('ADD "characterVarying"'),
+                    ),
+                ).to.be.false
 
                 await dataSource.synchronize(false)
 
@@ -70,6 +110,14 @@ describe("database schema > column length > postgres", () => {
                 expect(table!.findColumnByName("char")!.length).to.be.equal(
                     "100",
                 )
+
+                const post = await dataSource
+                    .getRepository(Post)
+                    .findOneByOrFail({ id: 1 })
+                expect(post.characterVarying).to.be.equal(characterVaryingValue)
+                expect(post.varchar).to.be.equal(varcharValue)
+                expect(post.character).to.be.equal(characterValue)
+                expect(post.char).to.be.equal(charValue)
             }),
         ))
 })
