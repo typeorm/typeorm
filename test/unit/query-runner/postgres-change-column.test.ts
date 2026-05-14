e0c6078430abce30e368e733a0a67fc7f3e4df3d
Gimyoonsoo <Gimyoonsoo@users.noreply.github.com>
2026-05-14 17:23:56 +0900

    Update Postgres column cache after type alters

---
+++ test/unit/query-runner/postgres-change-column.test.ts
@@ -0,0 +1,101 @@
+import { expect } from "chai"
+import type { DataSource } from "../../../src/data-source/DataSource"
+import type { PostgresDataSourceOptions } from "../../../src/driver/postgres/PostgresDataSourceOptions"
+import { PostgresDriver } from "../../../src/driver/postgres/PostgresDriver"
+import { PostgresQueryRunner } from "../../../src/driver/postgres/PostgresQueryRunner"
+import { Table } from "../../../src/schema-builder/table/Table"
+import { TableColumn } from "../../../src/schema-builder/table/TableColumn"
+
+describe("query runner > postgres > change column", () => {
+    function createQueryRunner() {
+        const driver = Object.create(PostgresDriver.prototype) as PostgresDriver
+
+        driver.database = "typeorm_test"
+        driver.schema = "public"
+        driver.searchSchema = "public"
+        driver.spatialTypes = []
+        driver.options = {
+            type: "postgres",
+        } as PostgresDataSourceOptions
+        driver.dataSource = {
+            driver,
+            metadataTableName: "typeorm_metadata",
+        } as unknown as DataSource
+
+        const queryRunner = new PostgresQueryRunner(driver, "master")
+        queryRunner.enableSqlMemory()
+
+        return queryRunner
+    }
+
+    it("changes varchar length without dropping the column and updates the cached table", async () => {
+        const queryRunner = createQueryRunner()
+        const table = new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "name",
+                    type: "character varying",
+                    length: "50",
+                },
+            ],
+        })
+        const newColumn = new TableColumn({
+            name: "name",
+            type: "character varying",
+            length: "51",
+        })
+
+        queryRunner.loadedTables = [table]
+
+        await queryRunner.changeColumn(table, table.columns[0], newColumn)
+
+        const upQueries = queryRunner
+            .getMemorySql()
+            .upQueries.map((query) => query.query)
+
+        expect(upQueries).to.deep.equal([
+            'ALTER TABLE "users" ALTER COLUMN "name" TYPE character varying(51)',
+        ])
+        expect(upQueries.join("\n")).not.to.contain("DROP COLUMN")
+        expect(upQueries.join("\n")).not.to.contain(" ADD ")
+        expect(queryRunner.loadedTables[0].columns[0].length).to.equal("51")
+    })
+
+    it("uses the new varchar length when changing length and collation together", async () => {
+        const queryRunner = createQueryRunner()
+        const table = new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "name",
+                    type: "character varying",
+                    length: "50",
+                    collation: "en_US",
+                },
+            ],
+        })
+        const newColumn = new TableColumn({
+            name: "name",
+            type: "character varying",
+            length: "51",
+            collation: "sv_SE",
+        })
+
+        queryRunner.loadedTables = [table]
+
+        await queryRunner.changeColumn(table, table.columns[0], newColumn)
+
+        const upQueries = queryRunner
+            .getMemorySql()
+            .upQueries.map((query) => query.query)
+
+        expect(upQueries).to.deep.equal([
+            'ALTER TABLE "users" ALTER COLUMN "name" TYPE character varying(51) COLLATE "sv_SE"',
+        ])
+        expect(queryRunner.loadedTables[0].columns[0].length).to.equal("51")
+        expect(queryRunner.loadedTables[0].columns[0].collation).to.equal(
+            "sv_SE",
+        )
+    })
+})
