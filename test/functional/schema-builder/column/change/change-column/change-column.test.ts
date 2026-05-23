2f6714e1c3162d71d319cdd37826fcc81c4cb3fd
Ari Morel <ari-agent-spector@agentmail.to>
2026-05-23 17:07:48 +0000

    test: cover postgres varchar length alteration in functional suite

--- test/functional/schema-builder/column/change/change-column/change-column.test.ts
+++ test/functional/schema-builder/column/change/change-column/change-column.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai"
 import "reflect-metadata"
 import type { DataSource } from "../../../../../../src"
+import { Table } from "../../../../../../src"
 import {
     closeTestingConnections,
     createTestingConnections,
@@ -87,6 +88,76 @@ describe("schema builder > change column", () => {
                 textColumn.length = "255"
             }),
         ))
+
+    it("should alter postgres varchar length without dropping existing data", async () => {
+        const postgresDataSources = dataSources.filter(
+            (dataSource) => dataSource.driver.options.type === "postgres",
+        )
+
+        await Promise.all(
+            postgresDataSources.map(async (dataSource) => {
+                const queryRunner = dataSource.createQueryRunner()
+
+                await queryRunner.createTable(
+                    new Table({
+                        name: "change_column_varchar_length",
+                        columns: [
+                            {
+                                name: "id",
+                                type: "int",
+                                isPrimary: true,
+                            },
+                            {
+                                name: "example",
+                                type: "varchar",
+                                length: "50",
+                            },
+                        ],
+                    }),
+                )
+
+                await queryRunner.query(
+                    `INSERT INTO "change_column_varchar_length"("id", "example") VALUES (1, 'value')`,
+                )
+
+                const table = await queryRunner.getTable(
+                    "change_column_varchar_length",
+                )
+                const oldColumn = table!.findColumnByName("example")!
+                const newColumn = oldColumn.clone()
+                newColumn.length = "51"
+
+                queryRunner.enableSqlMemory()
+                await queryRunner.changeColumn(table!, oldColumn, newColumn)
+                const upQueries = queryRunner
+                    .getMemorySql()
+                    .upQueries.map(({ query }) => query)
+                queryRunner.disableSqlMemory()
+
+                expect(upQueries).to.eql([
+                    `ALTER TABLE "change_column_varchar_length" ALTER COLUMN "example" TYPE character varying(51)`,
+                ])
+                expect(upQueries.join("\n")).not.to.contain("DROP COLUMN")
+                expect(upQueries.join("\n")).not.to.contain("ADD")
+
+                await queryRunner.changeColumn(table!, oldColumn, newColumn)
+
+                const rows = await queryRunner.query(
+                    `SELECT "example" FROM "change_column_varchar_length" WHERE "id" = 1`,
+                )
+                expect(rows).to.eql([{ example: "value" }])
+
+                const changedTable = await queryRunner.getTable(
+                    "change_column_varchar_length",
+                )
+                expect(
+                    changedTable!.findColumnByName("example")!.length,
+                ).to.equal("51")
+
+                await queryRunner.release()
+            }),
+        )
+    })
 
     it("should correctly change column type", () =>
         Promise.all(
