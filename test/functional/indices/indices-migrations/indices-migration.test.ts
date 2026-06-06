eba73df301073028dfdfa39040fc8c314646d8f4
Prakhar Chhalotre <chhalotreprakhar00@gmail.com>
2026-06-07 02:14:31 +0530

    chore: run lint and format

--- test/functional/indices/indices-migrations/indices-migration.test.ts
+++ test/functional/indices/indices-migrations/indices-migration.test.ts
@@ -1,20 +1,20 @@
-import { DataSource } from "../../../../src"
+import type { DataSource } from "../../../../src"
 import {
     closeTestingConnections,
     createTestingConnections,
 } from "../../../utils/test-utils"
 
+// GitHub issue #5117: Table indexes needlessly recreated when generating migrations
 describe("indices > indices migrations", () => {
-    describe("detect indices creation (#5117)", () => {
+    describe("detect indices creation", () => {
         let dataSources: DataSource[]
-        before(
-            async () =>
-                (dataSources = await createTestingConnections({
-                    entities: [__dirname + "/entity/*{.js,.ts}"],
-                    schemaCreate: false,
-                    dropSchema: true,
-                })),
-        )
+        before(async () => {
+            dataSources = await createTestingConnections({
+                entities: [__dirname + "/entity/*{.js,.ts}"],
+                schemaCreate: false,
+                dropSchema: true,
+            })
+        })
         after(() => closeTestingConnections(dataSources))
 
         it("should detect indices creation", () =>
@@ -39,16 +39,15 @@ describe("indices > indices migrations", () => {
             ))
     })
 
-    describe("no changes in indices (#5117)", () => {
+    describe("no changes in indices", () => {
         let dataSources: DataSource[]
-        before(
-            async () =>
-                (dataSources = await createTestingConnections({
-                    entities: [__dirname + "/entity/*{.js,.ts}"],
-                    schemaCreate: true,
-                    dropSchema: true,
-                })),
-        )
+        before(async () => {
+            dataSources = await createTestingConnections({
+                entities: [__dirname + "/entity/*{.js,.ts}"],
+                schemaCreate: true,
+                dropSchema: true,
+            })
+        })
         after(() => closeTestingConnections(dataSources))
 
         it("should detect no changes in indices", () =>
