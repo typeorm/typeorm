2b6545704e460fd27268417a1de7e1d515a8158a
kigland <shuaizhicheng336@gmail.com>
2026-06-14 01:02:16 +0800

    fix query order-by dollar aliases

--- packages/typeorm/src/query-builder/SelectQueryBuilder.ts
+++ packages/typeorm/src/query-builder/SelectQueryBuilder.ts
@@ -3863,9 +3863,9 @@ export class SelectQueryBuilder<Entity extends ObjectLiteral>
         }
 
         const qualifiedColumnReferencePattern =
-            /"([^"]+)"\."([^"]+)"|`([^`]+)`\.`([^`]+)`|\[([^\]]+)\]\.\[([^\]]+)\]|(\w+)\.([\w.]+)/g
+            /"([^"]+)"\."([^"]+)"|`([^`]+)`\.`([^`]+)`|\[([^\]]+)\]\.\[([^\]]+)\]|([A-Za-z0-9_$]+)\.([A-Za-z0-9_$.]+)/g
         const singleQualifiedColumnReferencePattern =
-            /^(?:"([^"]+)"\."([^"]+)"|`([^`]+)`\.`([^`]+)`|\[([^\]]+)\]\.\[([^\]]+)\]|(\w+)\.([\w.]+))$/
+            /^(?:"([^"]+)"\."([^"]+)"|`([^`]+)`\.`([^`]+)`|\[([^\]]+)\]\.\[([^\]]+)\]|([A-Za-z0-9_$]+)\.([A-Za-z0-9_$.]+))$/
         const getMatchedAliasAndPropertyPath = (
             doubleQuotedAlias: string | undefined,
             doubleQuotedPropertyPath: string | undefined,
