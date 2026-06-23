2b814ef10c4319643750379a7a050945ff6c7a69
Ahavahdev1 <brunodesidera@mail.com>
2026-06-23 02:00:38 -0300

    fix(test): remove any casting and fix test failure

---
+++ test/functional/util/orm-utils/normalize-where-criteria.spec.ts
@@ -0,0 +1,14 @@
+﻿import { expect } from "chai";
+import { TypeORMError } from "../../../../src/error";
+import { OrmUtils } from "../../../../src/util/OrmUtils";
+
+describe("OrmUtils.normalizeWhereCriteria", () => {
+    it("should throw TypeORMError when options is undefined and value is null", () => {
+        expect(() => OrmUtils.normalizeWhereCriteria({ key: null })).to.throw(TypeORMError);
+    });
+
+    it("should use default behavior when options is an empty object", () => {
+        const result = OrmUtils.normalizeWhereCriteria({ key: null }, {});
+        expect(result).to.deep.equal({});
+    });
+});
