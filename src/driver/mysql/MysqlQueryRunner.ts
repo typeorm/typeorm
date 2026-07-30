32f0da7a24f0e321cf53b3ea7b701f127eda9883
parastejpal987-cmyk <parastejpal987@gmail.com>
2026-07-30 23:18:00 +0530

    feat(mysql): use ALTER MODIFY instead of drop/add for safe column changes

--- src/driver/mysql/MysqlQueryRunner.ts
+++ src/driver/mysql/MysqlQueryRunner.ts
@@ -1137,8 +1137,6 @@ export class MysqlQueryRunner extends BaseQueryRunner implements QueryRunner {
         if (
             (newColumn.isGenerated !== oldColumn.isGenerated &&
                 newColumn.generationStrategy !== "uuid") ||
-            oldColumn.type !== newColumn.type ||
-            oldColumn.length !== newColumn.length ||
             (oldColumn.generatedType &&
                 newColumn.generatedType &&
                 oldColumn.generatedType !== newColumn.generatedType) ||
